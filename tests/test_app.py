22bf2ba55b970ec0d00194739fb70114e071db21
Andreas Kring <andreas@magenta.dk>
2017-05-09 16:19:41 +0200

    Added test for list_organisations

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,41 +1,46 @@
-import os
-import sys
 import unittest
-import tempfile
 import mora.app as mora
-import requests
 import requests_mock
-# from unittest.mock import patch
-
+import json
 from mora import lora
+
 
 class MoraTestCase(unittest.TestCase):
     def setUp(self):
-        # self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
         mora.app.config['TESTING'] = True
         self.app = mora.app.test_client()
-        # with flaskr.app.app_context():
-        #     flaskr.init_db()
-        pass
 
     def tearDown(self):
-        # os.close(self.db_fd)
-        # os.unlink(flaskr.app.config['DATABASE'])
         pass
+
+    # Careful here - test code must not be "complicated"...
+    def _json_to_dict(self, path):
+        """
+        Reads JSON from resources folder and converts to Python dictionary
+        :param path: path to json resource 
+        :return: dictionary corresponding to the resource JSON
+        """
+        with open(path) as f:
+            lines = f.readlines()
+        json_str = ''.join(lines)
+        return json.loads(json_str)
 
     def test_acl(self):
         rv = self.app.get('/acl/')
         assert b'[]\n' == rv.data
 
-    def test_root(self):
-        pass
-
     @requests_mock.mock()
-    def test_list_organisations(self, m):
-        m.get(lora.LORA_URL + 'organisation/organisation?bvn=%25', json={'results': []})
-        self.assertEquals(self.app.get('/o').text, 'data',
-                        'you fool')
-        pass
+    def test_list_organisations(self, mock):
+        lora_response = self._json_to_dict('tests/resources/lora_response_get_organisation_from_uuid.json')
+
+        mock.get(lora.LORA_URL + 'organisation/organisation?bvn=%', json={'results': [['f58a99a5-a34f-4c6a-8fb2-118a2e25eacb']]})
+        mock.get(lora.LORA_URL + 'organisation/organisation?uuid=f58a99a5-a34f-4c6a-8fb2-118a2e25eacb', json=lora_response)
+
+        expected_response = self._json_to_dict('tests/resources/mo_response_list_organisations.json')
+        actual_response = json.loads(self.app.get('/o').data.decode())
+
+        self.assertEquals(actual_response, expected_response, 'JSON does not match for list_organisations')
+
 
 if __name__ == '__main__':
     unittest.main()
