8f7071354bb5202267f92a19d797d2adb7ea309e
Andreas Kring <andreas@magenta.dk>
2017-05-15 10:02:07 +0200

    Rearranged mocking folder structure in test code

--- tests/test_app.py
+++ tests/test_app.py
@@ -47,31 +47,31 @@ class MoraTestCase(unittest.TestCase):
         mock.get(lora.LORA_URL + 'organisation/organisation?uuid=f58a99a5-a34f-4c6a-8fb2-118a2e25eacb',
                  json=lora_response)
 
-        expected_response = self._jsonfile_to_dict('tests/mocking/mo_response_list_organisations.json')
+        expected_response = self._jsonfile_to_dict('tests/mocking/mo/list_organisations.json')
         actual_response = self._request('/o/')
 
         self.assertEqual(actual_response, expected_response, 'JSON does not match for list_organisations')
 
     @requests_mock.mock()
     def test_full_hierarchy_no_treetype(self, mock):
-        lora_org_response = self._jsonfile_to_dict('tests/mocking/lora/lora_response_org_uuid.json')
+        lora_org_response = self._jsonfile_to_dict('tests/mocking/lora/organisation/organisation/get_org_from_uuid.json')
 
-        mock.get(lora.LORA_URL + 'organisation/organisation?uuid=722a3b7b-adb1-4f58-88cd-ecbdefd5014b',
+        mock.get(lora.LORA_URL + 'organisation/organisation?uuid=f58a99a5-a34f-4c6a-8fb2-118a2e25eacb',
                  json=lora_org_response)
-        mock.get(lora.LORA_URL + 'organisation/organisationenhed?tilhoerer=722a3b7b-adb1-4f58-88cd-ecbdefd5014b',
-                 json=self._jsonfile_to_dict('tests/mocking/lora/lora_response_organisation_orgEnhed_tilhoerer.json'))
+        mock.get(lora.LORA_URL + 'organisation/organisationenhed?tilhoerer=f58a99a5-a34f-4c6a-8fb2-118a2e25eacb',
+                 json=self._jsonfile_to_dict('tests/mocking/lora/organisation/organisationenhed/get_orgEnhed_from_tilhoerer.json'))
         params_get_orgUnits = ''.join(['?uuid=43b5dfc7-4d8c-494f-a954-d63379ffa1f3',
                                    '&uuid=1ae72c47-b8f3-41ba-9c54-ed5c496c5bdd',
                                     '&uuid=da9491aa-123f-4218-9551-ff1fcefe4ddb'])
         mock.get(lora.LORA_URL + 'organisation/organisationenhed' + params_get_orgUnits,
-                 json=self._jsonfile_to_dict('tests/mocking/lora/lora_response_organisation_orgEnhed_uuidx3.json'))
-        mock.get(lora.LORA_URL + 'organisation/organisationenhed?overordnet=da9491aa-123f-4218-9551-ff1fcefe4ddb&tilhoerer=722a3b7b-adb1-4f58-88cd-ecbdefd5014b',
+                 json=self._jsonfile_to_dict('tests/mocking/lora/organisation/organisationenhed/get_orgEnhed_from_uuidx3.json'))
+        mock.get(lora.LORA_URL + 'organisation/organisationenhed?overordnet=da9491aa-123f-4218-9551-ff1fcefe4ddb&tilhoerer=f58a99a5-a34f-4c6a-8fb2-118a2e25eacb',
                  json={'results': [[]]})
-        mock.get(lora.LORA_URL + 'organisation/organisationenhed?overordnet=43b5dfc7-4d8c-494f-a954-d63379ffa1f3&tilhoerer=722a3b7b-adb1-4f58-88cd-ecbdefd5014b',
+        mock.get(lora.LORA_URL + 'organisation/organisationenhed?overordnet=43b5dfc7-4d8c-494f-a954-d63379ffa1f3&tilhoerer=f58a99a5-a34f-4c6a-8fb2-118a2e25eacb',
                  json={'results': [[]]})
 
-        expected_response = self._jsonfile_to_dict('tests/mocking/mo_response_full-hierarchy.json')
-        actual_response = self._request('/o/722a3b7b-adb1-4f58-88cd-ecbdefd5014b/full-hierarchy?effective-date=&query=')
+        expected_response = self._jsonfile_to_dict('tests/mocking/mo/full-hierarchy_no_treetype.json')
+        actual_response = self._request('/o/f58a99a5-a34f-4c6a-8fb2-118a2e25eacb/full-hierarchy?effective-date=&query=')
 
         self.assertEqual(actual_response, expected_response, 'JSON does not match for full_hierarchy (no treetype)')
 
