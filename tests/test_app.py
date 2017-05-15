30b99a9407e2d196e36b2c1dd06c9f7d48fea6a9
Andreas Kring <andreas@magenta.dk>
2017-05-15 10:49:54 +0200

    Introduced URL-map in test code to make it more readable

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,6 +11,7 @@ class MoraTestCase(unittest.TestCase):
     def setUp(self):
         mora.app.config['TESTING'] = True
         self.app = mora.app.test_client()
+        self.lora_urls = self._jsonfile_to_dict('tests/mocking/lora/url_map.json')
 
     def tearDown(self):
         pass
@@ -35,6 +36,14 @@ class MoraTestCase(unittest.TestCase):
         """
         return json.loads(self.app.get(url).data.decode())
 
+    def _get_lora_url(self, key):
+        """
+        Return the URL to call in LoRa for the given key
+        :param key: the key used in the url_map.json file
+        :return: URL in LoRa as a string
+        """
+        return lora.LORA_URL + self.lora_urls[key]
+
     def test_acl(self):
         rv = self.app.get('/acl/')
         assert b'[]\n' == rv.data
@@ -42,10 +51,9 @@ class MoraTestCase(unittest.TestCase):
     @requests_mock.mock()
     def test_list_organisations(self, mock):
         lora_response = self._jsonfile_to_dict('tests/mocking/lora/organisation/organisation/get_org_from_uuid.json')
-        mock.get(lora.LORA_URL + 'organisation/organisation?bvn=%',
-                 json={'results': [['f58a99a5-a34f-4c6a-8fb2-118a2e25eacb']]})
-        mock.get(lora.LORA_URL + 'organisation/organisation?uuid=f58a99a5-a34f-4c6a-8fb2-118a2e25eacb',
-                 json=lora_response)
+
+        mock.get(self._get_lora_url('org_org_bvn'), json={'results': [['f58a99a5-a34f-4c6a-8fb2-118a2e25eacb']]})
+        mock.get(self._get_lora_url('org_org_uuid'), json=lora_response)
 
         expected_response = self._jsonfile_to_dict('tests/mocking/mo/list_organisations.json')
         actual_response = self._request('/o/')
@@ -56,29 +64,18 @@ class MoraTestCase(unittest.TestCase):
     def test_full_hierarchy_no_treetype(self, mock):
         lora_org_response = self._jsonfile_to_dict('tests/mocking/lora/organisation/organisation/get_org_from_uuid.json')
 
-        mock.get(lora.LORA_URL + 'organisation/organisation?uuid=f58a99a5-a34f-4c6a-8fb2-118a2e25eacb',
-                 json=lora_org_response)
-        mock.get(lora.LORA_URL + 'organisation/organisationenhed?tilhoerer=f58a99a5-a34f-4c6a-8fb2-118a2e25eacb',
+        mock.get(self._get_lora_url('org_org_uuid'), json=lora_org_response)
+        mock.get(self._get_lora_url('org_orgEnhed_tilhoerer'),
                  json=self._jsonfile_to_dict('tests/mocking/lora/organisation/organisationenhed/get_orgEnhed_from_tilhoerer.json'))
-        params_get_orgUnits = ''.join(['?uuid=43b5dfc7-4d8c-494f-a954-d63379ffa1f3',
-                                   '&uuid=1ae72c47-b8f3-41ba-9c54-ed5c496c5bdd',
-                                    '&uuid=da9491aa-123f-4218-9551-ff1fcefe4ddb'])
-        mock.get(lora.LORA_URL + 'organisation/organisationenhed' + params_get_orgUnits,
+        mock.get(self._get_lora_url('org_orgEnhed_uuidx3'),
                  json=self._jsonfile_to_dict('tests/mocking/lora/organisation/organisationenhed/get_orgEnhed_from_uuidx3.json'))
-        mock.get(lora.LORA_URL + 'organisation/organisationenhed?overordnet=da9491aa-123f-4218-9551-ff1fcefe4ddb&tilhoerer=f58a99a5-a34f-4c6a-8fb2-118a2e25eacb',
-                 json={'results': [[]]})
-        mock.get(lora.LORA_URL + 'organisation/organisationenhed?overordnet=43b5dfc7-4d8c-494f-a954-d63379ffa1f3&tilhoerer=f58a99a5-a34f-4c6a-8fb2-118a2e25eacb',
-                 json={'results': [[]]})
+        mock.get(self._get_lora_url('org_orgEnhed_overordnet_tilhoerer1'), json={'results': [[]]})
+        mock.get(self._get_lora_url('org_orgEnhed_overordnet_tilhoerer2'), json={'results': [[]]})
 
         expected_response = self._jsonfile_to_dict('tests/mocking/mo/full-hierarchy_no_treetype.json')
         actual_response = self._request('/o/f58a99a5-a34f-4c6a-8fb2-118a2e25eacb/full-hierarchy?effective-date=&query=')
 
         self.assertEqual(actual_response, expected_response, 'JSON does not match for full_hierarchy (no treetype)')
-
-
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
