81208ea621b35c50e6b374aa0173998a6c54abb5
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-06-20 09:46:18 +0200

    tests: get rid of expected failures

--- tests/test_app.py
+++ tests/test_app.py
@@ -53,69 +53,6 @@ class MoraTestCase(unittest.TestCase):
                  json=self._jsonfile_to_dict(
                      'tests/mocking/lora/organisation/organisationenhed/get_orgEnhed_from_uuidx3.json'))
 
-    @unittest.expectedFailure
-    @requests_mock.mock()
-    def test_list_organisations(self, mock):
-        self._standard_mock_setup(mock)
-
-        mock.get(self._get_lora_url('org_org_bvn'), json={'results': [['f58a99a5-a34f-4c6a-8fb2-118a2e25eacb']]})
-
-        expected_response = self._jsonfile_to_dict('tests/mocking/mo/list_organisations.json')
-        actual_response = self._request('/o/')
-
-        self.assertEqual(actual_response, expected_response, 'JSON do not match for list_organisations')
-
-    @unittest.expectedFailure
-    @requests_mock.mock()
-    def test_full_hierarchy_treetype_is_null(self, mock):
-        self._standard_mock_setup(mock)
-
-        mock.get(self._get_lora_url('org_orgEnhed_overordnet_tilhoerer1'), json={'results': [[]]})
-        mock.get(self._get_lora_url('org_orgEnhed_overordnet_tilhoerer2'), json={'results': [[]]})
-
-        expected_response = self._jsonfile_to_dict('tests/mocking/mo/full-hierarchy_treetype_null.json')
-        actual_response = self._request('/o/f58a99a5-a34f-4c6a-8fb2-118a2e25eacb/full-hierarchy?effective-date=&query=')
-
-        self.assertEqual(actual_response, expected_response, 'JSON do not match for full_hierarchy (no treetype)')
-
-    @unittest.expectedFailure
-    @requests_mock.mock()
-    def test_full_hierarchy_treetype_is_treetype(self, mock):
-        self._standard_mock_setup(mock)
-
-        expected_response = self._jsonfile_to_dict('tests/mocking/mo/full-hierarchy_treetype_treetype.json')
-        actual_response = self._request(
-            '/o/f58a99a5-a34f-4c6a-8fb2-118a2e25eacb/full-hierarchy?effective-date=&query=&treeType=treeType')
-
-        self.assertEqual(actual_response, expected_response, 'JSON do not match for full_hierarchy (treetype=treetype)')
-
-    @unittest.expectedFailure
-    @requests_mock.mock()
-    def test_full_hierarchy_treetype_is_specific(self, mock):
-        lora_org_response = self._jsonfile_to_dict(
-            'tests/mocking/lora/organisation/organisation/get_org_from_uuid.json')
-        mock.get(self._get_lora_url('org_org_uuid'), json=lora_org_response)
-        mock.get(self._get_lora_url('org_orgEnhed_overordnet_tilhoerer3'), json={
-            'results': [
-                [
-                    '43b5dfc7-4d8c-494f-a954-d63379ffa1f3',
-                    'da9491aa-123f-4218-9551-ff1fcefe4ddb'
-                ]
-            ]
-        }
-                 )
-        lora_response = self._jsonfile_to_dict(
-            'tests/mocking/lora/organisation/organisationenhed/get_orgEnhed_from_uuidx2.json')
-        mock.get(self._get_lora_url('org_orgEnhed_uuidx2'), json=lora_response)
-        mock.get(self._get_lora_url('org_orgEnhed_overordnet_tilhoerer1'), json={'results': [[]]})
-        mock.get(self._get_lora_url('org_orgEnhed_overordnet_tilhoerer2'), json={'results': [[]]})
-
-        expected_response = self._jsonfile_to_dict('tests/mocking/mo/full-hierarchy_treetype_specific.json')
-        actual_response = self._request(
-            '/o/f58a99a5-a34f-4c6a-8fb2-118a2e25eacb/full-hierarchy?effective-date=&query=&treeType=specific&orgUnitId=1ae72c47-b8f3-41ba-9c54-ed5c496c5bdd')
-
-        self.assertEqual(actual_response, expected_response, 'JSON do not match for full_hierarchy (treetype=specific)')
-
     @requests_mock.mock()
     def test_list_classes(self, mock):
         lora_klasse_response = jsonfile_to_dict(
