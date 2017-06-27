6ee9513b4eac94fba6acc42e3d942ceaac41ec20
Andreas Kring <andreas@magenta.dk>
2017-06-27 11:45:22 +0200

    Added integration test for creating org unit with final end-date. However, for some the strange reason there is a problem with this test (see comments in the code)

--- tests/test_integration_writing.py
+++ tests/test_integration_writing.py
@@ -7,9 +7,12 @@
 #
 
 import json
+import freezegun
+import unittest
 from . import util
 from mora import lora
 from tests.util import jsonfile_to_dict
+from pprint import pprint
 
 
 class TestCreateOrgUnit(util.LoRATestCase):
@@ -17,6 +20,7 @@ class TestCreateOrgUnit(util.LoRATestCase):
 
     # TODO: mock time
 
+    @freezegun.freeze_time('2010-06-01 12:00:00', tz_offset=+1)
     def test_should_create_org_unit_with_virkning_to_infinity(self):
         self.load_sample_structures()
 
@@ -71,11 +75,85 @@ class TestCreateOrgUnit(util.LoRATestCase):
         # Get the UUID of the org unit just created
         uuid = json.loads(r.data.decode())['uuid']
 
-        lora_response = lora.get('organisation/organisationenhed', uuid=uuid)
+        lora_response = lora.organisationenhed(
+            uuid=uuid)[0]['registreringer'][-1]
         lora_response.pop('fratidspunkt')
 
         expected_response = jsonfile_to_dict(
             'tests/integration_test_data/create_org_unit_infinity.json')
+        expected_response.pop('fratidspunkt')
+
+        self.assertEqual(expected_response, lora_response)
+
+    # TODO: test below is for some reason failing!?
+    # 'Relationer' is missing from the JSON returned from the LoRa test
+    # instance. The create org unit has been test manually and the data
+    # in LoRa after running app.create_organisation_unit is correct.
+    # It is strange that the test above passes and the one below does not...
+
+    @unittest.expectedFailure
+    @freezegun.freeze_time('2010-06-01 12:00:00', tz_offset=+1)
+    def test_should_create_org_unit_with_virkning_to_2011_01_01(self):
+        self.load_sample_structures()
+
+        root = '2874e1dc-85e6-4269-823a-e1125484dfd3'
+        org = '456362c4-0ee4-4e5e-a72c-751239745e62'
+
+        payload = {
+            "user-key": "NULL",
+            "name": "NyEnhed",
+            "valid-from": "01-01-2010",
+            "valid-to": "01-01-2011",
+            "org": org,
+            "parent": root,
+            "type": {
+                "name": "Afdeling",
+                "userKey": "Afdeling",
+                "uuid": "32547559-cfc1-4d97-94c6-70b192eff825"
+            },
+            "locations": [
+                {
+                    "name": "lnavn",
+                    "primaer": True,
+                    "location": {
+                        "UUID_EnhedsAdresse":
+                            "98001816-a7cc-4115-a9e6-2c5c06c79e5d",
+                        "postdistrikt": "Risskov",
+                        "postnr": "8240",
+                        "vejnavn": "Pilevej 2, 8240 Risskov"
+                    },
+                    "contact-channels": [
+                        {
+                            "contact-info": "12345678",
+                            "visibility": {
+                                "name": "N/A",
+                                "user-key": "N/A",
+                                "uuid": "00000000-0000-0000-0000-000000000000"
+                            },
+                            "type": {
+                                "name": "Phone Number",
+                                "prefix": "urn:magenta.dk:telefon:",
+                                "uuid": "b7ccfb21-f623-4e8f-80ce-89731f726224"
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+        r = self.client.post('/o/' + org + '/org-unit',
+                             data=json.dumps(payload),
+                             content_type='application/json')
+        self.assertEqual(201, r.status_code)
+
+        # Get the UUID of the org unit just created
+        uuid = json.loads(r.data.decode())['uuid']
+
+        lora_response = lora.organisationenhed(
+            uuid=uuid, virkningtil='infinity')[0]['registreringer'][-1]
+        lora_response.pop('fratidspunkt')
+
+        expected_response = jsonfile_to_dict(
+            'tests/integration_test_data/create_org_unit_2011-01-01.json')
         expected_response.pop('fratidspunkt')
 
         self.assertEqual(expected_response, lora_response)
