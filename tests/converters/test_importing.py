c7dbc6a29aa53533d61cc110e5659191e768c75f
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-11-27 19:39:43 +0100

    increase test coverage of import by triggering more cases
    and make failure reporting more graceful by unwrapping ordered dicts,
    since 'unittest' doesn't make line-by-line diffs for them.

--- tests/converters/test_importing.py
+++ tests/converters/test_importing.py
@@ -6,6 +6,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
+import json
 import os
 
 from mora.converters import importing
@@ -20,35 +21,38 @@ class MockTests(util.TestCase):
     def test_load(self, m):
         expected = util.get_fixture('MAGENTA_01.json')
 
-        self.assertEqual(expected, importing.load_data([
+        self.assertEqual(expected, dict(importing.load_data([
             os.path.join(util.FIXTURE_DIR, 'MAGENTA_01.json'),
-        ]))
+        ])))
 
-        self.assertEqual(expected, importing.load_data([
+        self.assertEqual(expected, dict(importing.load_data([
             os.path.join(util.FIXTURE_DIR, 'MAGENTA_01.json'),
-        ], exact=True))
+        ], exact=True)))
 
-        self.assertEqual(expected, importing.load_data([
+        self.assertEqual(expected, dict(importing.load_data([
             os.path.join(util.FIXTURE_DIR, 'MAGENTA_01.xlsx'),
-        ]))
+        ])))
 
     @util.mock()
     def test_convert(self, m):
         data = util.get_fixture('MAGENTA_01.json')
 
-        # JSON converts tuples to lists - convert them back
-        expected = list(map(tuple, util.get_fixture('MAGENTA_01-result.json')))
+        # JSON converts tuples to lists - the map() converts them back
+        expected = sorted(map(tuple,
+                              util.get_fixture('MAGENTA_01-result.json')))
 
-        with open('/tmp/x.json', 'w') as fp:
-            import json
-            json.dump(
-                list(importing.convert([
-                    os.path.join(util.FIXTURE_DIR, 'MAGENTA_01.json'),
-                ])),
-                fp,
-                indent=2, sort_keys=True,
-            )
-
-        self.assertEqual(sorted(expected), sorted(importing.convert([
+        actual = sorted(importing.convert([
             os.path.join(util.FIXTURE_DIR, 'MAGENTA_01.json'),
-        ])))
+        ]))
+
+        # for resetting the test
+        if False:
+            with open('/tmp/x.json', 'w') as fp:
+                json.dump(actual, fp, indent=2, sort_keys=True)
+
+        self.assertEqual(expected, actual)
+
+    @util.mock()
+    def test_unknown_suffix(self, m):
+        gen = importing.read_paths('test.hest')
+        self.assertRaises(ValueError, next, gen)
