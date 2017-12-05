e04fe8ca7fb2371378f28e82a8710e7f643500b8
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-12-05 11:09:58 +0100

    importing: add more tests for address washing

--- tests/converters/test_importing.py
+++ tests/converters/test_importing.py
@@ -65,3 +65,30 @@ class MockTests(util.TestCase):
     def test_unknown_suffix(self, m):
         gen = importing.read_paths('test.hest')
         self.assertRaises(ValueError, next, gen)
+
+    @util.mock('importing-wash.json')
+    def test_addr_wash(self, m):
+        w = importing._wash_address
+
+        w.cache_clear()
+
+        self.assertEqual(w('', None, None),
+                         None)
+
+        self.assertEqual(w('Rådhuspladsen', 8100, 'Århus C'),
+                         '9b9a6a18-ffb7-4ece-a7f1-5368812e4719')
+
+        self.assertEqual(w('Rådhuset', '8100', 'Aarhus C'),
+                         '9b9a6a18-ffb7-4ece-a7f1-5368812e4719')
+
+        self.assertEqual(w('Skejbygårdsvej 14-16', 8240, 'Risskov'),
+                         None)
+
+        self.assertEqual(w('Runevej 107-109', 8210, 'Aarhus V'),
+                         '209b75e7-a662-4224-9e3d-64ef1f16eab0')
+
+        self.assertEqual(w('Jettesvej 2, Hus 1', 8220, 'Brabrand'),
+                         '2240aef2-7636-40b6-8c19-8f3e4dd65710')
+
+        self.assertEqual(w('Grundtvigsvej 14, kld.', 8260, 'Viby J'),
+                         None)
