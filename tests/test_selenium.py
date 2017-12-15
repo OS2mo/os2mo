4fc4168103c526624d0e9cfd4488304aad05a7d2
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-12-15 15:11:23 +0100

    selenium tests: add tests for unit creation workflow

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -30,6 +30,24 @@ CHROME_UBUNTU_DRIVER = '/usr/lib/chromium-browser/chromedriver'
 
 @unittest.skipUnless(selenium, 'selenium not installed')
 class RightsTests(util.LiveLoRATestCase):
+    def assertTableContents(self, expected, element):
+        self.assertEquals(
+            expected,
+            [
+                [
+                    cell.text
+                    for cell in row.find_elements_by_tag_name('td')
+                ]
+                for row in element.find_elements_by_css_selector('tbody > tr')
+            ],
+        )
+
+    def get_tab_visibility(self):
+        return {
+            tab.get_property('innerText').strip(): tab.is_displayed()
+            for tab in self.browser.find_elements_by_tag_name('tab-heading')
+        }
+
     @classmethod
     def setUpClass(cls):
         default_driver = (
@@ -185,5 +203,310 @@ class RightsTests(util.LiveLoRATestCase):
             'Overordnet Enhed',
             self.browser.find_element_by_css_selector(
                 '[ng-bind="organisation.activeName"]',
+            ).text,
+        )
+
+    def test_create_unit_to_infinity_without_contact_channel(self):
+        self.load_sample_structures()
+        self.test_login()
+
+        # hover over the new org. unit button, and click it
+        btn = self.browser.find_element_by_id("elOrgNewBtn")
+
+        hover = AC(self.browser).move_to_element_with_offset(btn, 5, 5).click()
+        hover.perform()
+
+        self.browser.implicitly_wait(5)
+
+        self.browser.find_element_by_id('orgStartDate').send_keys('01-01-2016')
+        self.browser.find_element_by_name('orgUnitName').send_keys('Ny Enhed Uden Slutdato')
+
+        select = ui.Select(self.browser.find_element_by_name('orgUnitType'))
+        select.select_by_visible_text('Afdeling')
+
+        inputel = self.browser.find_element_by_css_selector('[ng-model=valueEnhead]')
+        inputel.click()
+
+        self.browser.implicitly_wait(5)
+
+        top_level = self.browser.find_element_by_css_selector(
+            '.modal-dialog treecontrol [tree-transclude]',
+        )
+
+        self.assertEqual(top_level.text, 'Overordnet Enhed')
+        top_level.click()
+
+        self.assertEqual(inputel.get_property('value'), 'Overordnet Enhed')
+
+        self.browser.find_element_by_name('geoLocal').click()
+
+        # focus the address search field
+        search_field = self.browser.find_element_by_css_selector(
+            '#search-address input[name=query]',
+        )
+        search_field.click()
+        search_field.send_keys('Pilestræde 43, 3')
+
+        dropdown = self.browser.find_element_by_css_selector(
+            '#search-address ul.dropdown-menu',
+        )
+
+        dropdown.find_element_by_css_selector('li').click()
+
+        self.browser.find_element_by_css_selector('[placeholder=Lokationsnavn]').send_keys(
+            'stednavn',
+        )
+
+        ui.Select(self.browser.find_element_by_css_selector(
+            'form[name=contactChannelForm] select[name=type]',
+        )).select_by_visible_text('Phone Number')
+
+        ui.Select(self.browser.find_element_by_css_selector(
+            'form[name=contactChannelForm] select[name=visibility]',
+        )).select_by_visible_text('Hemmeligt')
+
+        self.browser.find_element_by_css_selector(
+            'form[name=contactChannelForm] input[name=contact-info]',
+        ).send_keys('00000000')
+
+        # NB: horrible UI - we're entering text into the contact channel item, but
+        # not actually clicking the "create" button - so we don't get
+        # one
+
+        self.browser.find_element_by_id('elOrgCreateOk').click()
+
+        ui.WebDriverWait(self.browser, 1).until(
+            EC.invisibility_of_element_located((By.ID, 'elOrgCreateOk')),
+        )
+
+        # TODO: check work log
+
+        # we've created the unit, now search for it
+        self.browser.find_element_by_css_selector(
+            '.input-group-btn > input'
+        ).send_keys('Uden Slutdato')
+        self.browser.find_element_by_css_selector(
+            '.input-group-btn > button'
+        ).click()
+
+        self.assertTableContents(
+            [
+                ['Ny Enhed Uden Slutdato', '01-01-2016', 'infinity', ''],
+            ],
+            self.browser.find_element_by_id('organisationList'),
+        )
+
+        found = self.browser.find_elements_by_css_selector(
+            'table#organisationList > tbody > tr'
+        )
+
+        self.assertEqual(1, len(found))
+
+        # show it!
+        found[0].find_element_by_tag_name('a').click()
+
+        self.browser.implicitly_wait(15)
+
+        unit_tabs = self.browser.find_elements_by_id('elOrgorg-unitTab')
+
+        # yes, three items with the same ID :(
+        self.assertEqual(3, len(unit_tabs))
+
+        # Check that present is visible, and the others aren't
+        self.assertEqual(
+            [True, False, False],
+            [v.is_displayed() for v in unit_tabs]
+        )
+        self.assertEqual(
+            {
+                'Association': False,
+                'Contact Channel': False,
+                'Engagement': False,
+                'Enhed': True,
+                'Leder': False,
+                'Lokation': True,
+                'Rolle': False,
+            },
+            self.get_tab_visibility(),
+        )
+
+        self.assertTableContents(
+            [
+                [
+                    'Ny Enhed Uden Slutdato',
+                    'Afdeling',
+                    'Overordnet Enhed',
+                    '01-01-2016',
+                    'infinity'
+                ],
+            ],
+            unit_tabs[0],
+        )
+
+        self.assertEqual(
+            'Ny Enhed Uden Slutdato',
+            self.browser.find_element_by_css_selector(
+                '.org-detail span[ng-bind="organisation.activeName"]',
+            ).text,
+        )
+
+    def test_create_unit_to_2020_with_contact_channel(self):
+        self.load_sample_structures()
+        self.test_login()
+
+        # hover over the new org. unit button, and click it
+        btn = self.browser.find_element_by_id("elOrgNewBtn")
+
+        hover = AC(self.browser).move_to_element_with_offset(btn, 5, 5).click()
+        hover.perform()
+
+        self.browser.implicitly_wait(5)
+
+        self.browser.find_element_by_id('orgStartDate').send_keys('01-01-2016')
+        self.browser.find_element_by_id('orgEndDate').send_keys('01-01-2020')
+        self.browser.find_element_by_name('orgUnitName').send_keys('Ny Enhed Med Slutdato')
+
+        select = ui.Select(self.browser.find_element_by_name('orgUnitType'))
+        select.select_by_visible_text('Afdeling')
+
+        inputel = self.browser.find_element_by_css_selector('[ng-model=valueEnhead]')
+        inputel.click()
+
+        self.browser.implicitly_wait(5)
+
+        top_level = self.browser.find_element_by_css_selector(
+            '.modal-dialog treecontrol [tree-transclude]',
+        )
+
+        self.assertEqual(top_level.text, 'Overordnet Enhed')
+        top_level.click()
+
+        self.assertEqual(inputel.get_property('value'), 'Overordnet Enhed')
+
+        self.browser.find_element_by_name('geoLocal').click()
+
+        # focus the address search field
+        search_field = self.browser.find_element_by_css_selector(
+            '#search-address input[name=query]',
+        )
+        search_field.click()
+        search_field.send_keys('Pilestræde 43, 3')
+
+        dropdown = self.browser.find_element_by_css_selector(
+            '#search-address ul.dropdown-menu',
+        )
+
+        dropdown.find_element_by_css_selector('li').click()
+
+        self.browser.find_element_by_css_selector('[placeholder=Lokationsnavn]').send_keys(
+            'stednavn',
+        )
+
+        ui.Select(self.browser.find_element_by_css_selector(
+            'form[name=contactChannelForm] select[name=type]',
+        )).select_by_visible_text('Phone Number')
+
+        ui.Select(self.browser.find_element_by_css_selector(
+            'form[name=contactChannelForm] select[name=visibility]',
+        )).select_by_visible_text('Hemmeligt')
+
+        self.browser.find_element_by_css_selector(
+            'form[name=contactChannelForm] input[name=contact-info]',
+        ).send_keys('00000000')
+
+        # NB: horrible UI - actually click the button to create the location
+        self.browser.find_element_by_id('elOrgCreateBtn_contact').click()
+
+        self.browser.find_element_by_id('elOrgCreateOk').click()
+
+        ui.WebDriverWait(self.browser, 1).until(
+            EC.invisibility_of_element_located((By.ID, 'elOrgCreateOk')),
+        )
+
+        # TODO: check work log
+
+        # we've created the unit, now search for it - intentionally incorrectly
+        self.browser.find_element_by_css_selector(
+            '.input-group-btn > input'
+        ).send_keys('Uden Slutdato')
+        self.browser.find_element_by_css_selector(
+            '.input-group-btn > button'
+        ).click()
+
+        # oops, we didn't find anything!
+        self.assertTableContents(
+            [],
+            self.browser.find_element_by_id('organisationList'),
+        )
+
+        # we've created the unit, now search for it
+        self.browser.find_element_by_css_selector(
+            '.input-group-btn > input'
+        ).clear()
+        self.browser.find_element_by_css_selector(
+            '.input-group-btn > input'
+        ).send_keys('Med Slutdato')
+        self.browser.find_element_by_css_selector(
+            '.input-group-btn > button'
+        ).click()
+
+        self.assertTableContents(
+            [
+                ['Ny Enhed Med Slutdato', '01-01-2016', '01-01-2020', ''],
+            ],
+            self.browser.find_element_by_id('organisationList'),
+        )
+
+        found = self.browser.find_elements_by_css_selector(
+            'table#organisationList > tbody > tr'
+        )
+
+        self.assertEqual(1, len(found))
+
+        # show it!
+        found[0].find_element_by_tag_name('a').click()
+
+        self.browser.implicitly_wait(15)
+
+        unit_tabs = self.browser.find_elements_by_id('elOrgorg-unitTab')
+
+        # yes, three items with the same ID :(
+        self.assertEqual(3, len(unit_tabs))
+
+        # Check that present is visible, and the others aren't
+        self.assertEqual(
+            [True, False, False],
+            [v.is_displayed() for v in unit_tabs]
+        )
+        self.assertEqual(
+            {
+                'Association': False,
+                'Contact Channel': True,
+                'Engagement': False,
+                'Enhed': True,
+                'Leder': False,
+                'Lokation': True,
+                'Rolle': False,
+            },
+            self.get_tab_visibility(),
+        )
+
+        self.assertTableContents(
+            [
+                [
+                    'Ny Enhed Med Slutdato',
+                    'Afdeling',
+                    'Overordnet Enhed',
+                    '01-01-2016',
+                    '01-01-2020'
+                ],
+            ],
+            unit_tabs[0],
+        )
+
+        self.assertEqual(
+            'Ny Enhed Med Slutdato',
+            self.browser.find_element_by_css_selector(
+                '.org-detail span[ng-bind="organisation.activeName"]',
             ).text,
         )
