4114942d3bf55f8df682a461e7aee771098b1cfb
Anders Jepsen <hi@andersjepsen.com>
2018-01-02 13:08:33 +0100

    added click-outside directive

--- src/components/OrganisationUnitPicker.vue
+++ src/components/OrganisationUnitPicker.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="form-group">
+  <div class="form-group">      
       <label for="">{{ label }}</label>
       <input 
         type="text" 
         class="form-control" 
-        placeholder="Kommer senere..."
+        placeholder="Vælg enhed"
         ref="orgUnitPicker"
         :value="selectedSuperUnit.name"
-        @focus="show"
+        @click.stop="show"
       >
-      <div class="mo-input-group" v-show="showTree">
-        <tree-view v-model="selectedSuperUnit" :orgUuid="org.uuid"/>
+      <div class="mo-input-group" v-show="showTree" v-click-outside="hide">
+        <tree-view 
+          v-model="selectedSuperUnit" 
+          v-click-outside="hide"
+          :orgUuid="org.uuid" 
+        />
       </div>
   </div>
 </template>
 
 <script>
   import Organisation from '../api/Organisation'
+  import ClickOutside from '../directives/ClickOutside'
   import TreeView from '../components/Treeview'
+
   export default {
     components: {
       TreeView
@@ -34,6 +40,9 @@
         selectedSuperUnit: {},
         showTree: false
       }
+    },
+    directives: {
+      ClickOutside
     },
     watch: {
       selectedSuperUnit (newVal, oldVal) {
@@ -75,5 +84,4 @@
     border-radius: 0 0 0.25rem;
     transition: border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s;
   }
-
 </style>
