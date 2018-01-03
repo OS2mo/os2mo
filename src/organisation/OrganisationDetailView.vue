9f018ecbad4507ebb98500efacfa4cd8b64712e3
Anders Jepsen <hi@andersjepsen.com>
2018-01-03 10:01:39 +0100

    added organisation detail view and removed some leftover outputs in unit rename

---
+++ src/organisation/OrganisationDetailView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="headline">
+      Enhed
+    </div>
+    <organisation-detail-unit/>
+    <div class="headline">
+      Lokation
+    </div>
+    <organisation-detail-location/>
+    <div class="headline">
+      Kontaktkanal
+    </div>
+    <organisation-detail-contact/>
+  </div>
+</template>
+
+<script>
+  import OrganisationDetailUnit from './OrganisationDetailUnit.vue'
+  import OrganisationDetailLocation from './OrganisationDetailLocation.vue'
+  import OrganisationDetailContact from './OrganisationDetailContact.vue'
+
+  export default {
+    components: {
+      OrganisationDetailUnit,
+      OrganisationDetailLocation,
+      OrganisationDetailContact
+    },
+    data () {
+      return {
+        details: []
+      }
+    },
+    created: function () {
+      this.getDetails()
+    },
+    methods: {}
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .headline {
+    background-color: #007bff;
+    color: white;
+    padding-left: 1em;
+    font-weight: bold;
+  }
+</style>
