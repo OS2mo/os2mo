57db9b5fda66e8d334da117f9efb760c00627014
Novik <ns@magenta.dk>
2018-01-05 15:06:34 +0100

    Styled login page

--- src/login/LoginPage.vue
+++ src/login/LoginPage.vue
@@ -1,13 +1,47 @@
 <template>
   <div id="login-wrapper" class="col">
-    <button class="btn btn-default">
+    <button class="btn btn-primary btn-bg" v-b-modal.login>
       <icon name="user"/>
       Medarbejder
     </button>
-    <button class="btn btn-default">
+    <button class="btn btn-success btn-bg-2" v-b-modal.login>
       <icon name="share-alt"/>
       Organisation
     </button>
+
+    <b-modal
+      id="login"
+      size="lg"
+      hide-footer
+      title="Log ind">
+
+      <b-form >
+      <b-form-input
+        class="col-5"
+        v-model="text1"
+        type="text"
+        placeholder="Brugernavn">
+      </b-form-input>
+        <br>
+      <b-form-input
+        class="col-5"
+        v-model="text2"
+        type="password"
+        placeholder="Adgangskode">
+      </b-form-input>
+        <br>
+      <b-form-checkbox
+        class="col-4"
+        id="checkbox1"
+        value="accepted">
+        Husk mig
+      </b-form-checkbox>
+        <br>
+      <button class="btn btn-primary col-5 btn-bg" v-b-modal.login>
+        Log ind
+      </button>
+      </b-form>
+    </b-modal>
   </div>
 </template>
 
@@ -24,8 +58,26 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#login-wrapper {
+  #login-wrapper {
   text-align: center;
-}
+  }
+
+  .btn-bg{
+  background-color: #4a5a79;
+    cursor: pointer;
+  }
+
+  .btn-bg-2{
+    cursor: pointer;
+  }
+
+  .btn-bg:hover{
+    background-color: #002f5d;
+  }
+
+  .input-center{
+    vertical-align: middle;
+  }
+
 
 </style>
