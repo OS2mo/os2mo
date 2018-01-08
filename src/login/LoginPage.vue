5b115e4c68dc82265eccbb6e8a8dc82fd74df059
Novik <ns@magenta.dk>
2018-01-08 14:52:02 +0100

    Styled login page (update)

--- src/login/LoginPage.vue
+++ src/login/LoginPage.vue
@@ -1,46 +1,49 @@
 <template>
   <div id="login-wrapper" class="col">
-    <button class="btn btn-primary btn-bg" v-b-modal.login>
-      <icon name="user"/>
-      Medarbejder
+    <button class="btn btn-primary btn-bg col-1" v-b-modal.login>
+      <icon name="user" scale="1.8"/>
+      <span class="btn-text">Medarbejder</span>
     </button>
-    <button class="btn btn-success btn-bg-2" v-b-modal.login>
-      <icon name="share-alt"/>
-      Organisation
+      <br>
+    <button class="btn btn-success btn-bg-2 col-1" v-b-modal.login>
+      <icon name="share-alt" scale="1.8"/>
+      <span class="btn-text">Organisation</span>
     </button>
 
     <b-modal
       id="login"
-      size="lg"
+      size="md"
       hide-footer
       title="Log ind">
 
+      <br>
       <b-form >
       <b-form-input
-        class="col-5"
-        v-model="text1"
+        class="col-8 input-center"
+        name="username"
         type="text"
         placeholder="Brugernavn">
       </b-form-input>
         <br>
       <b-form-input
-        class="col-5"
-        v-model="text2"
+        class="col-8 input-center"
+        name="password"
         type="password"
         placeholder="Adgangskode">
       </b-form-input>
         <br>
       <b-form-checkbox
-        class="col-4"
+        class="checkbox-input"
         id="checkbox1"
         value="accepted">
         Husk mig
       </b-form-checkbox>
         <br>
-      <button class="btn btn-primary col-5 btn-bg" v-b-modal.login>
+      <button class="btn btn-primary col-8 btn-bg" v-b-modal.login>
         Log ind
       </button>
       </b-form>
+      <br>
     </b-modal>
   </div>
 </template>
@@ -58,17 +61,29 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
   #login-wrapper {
-  text-align: center;
+    text-align: center;
+    margin-top: 200px;
+  }
+
+  .btn-text{
+    float: right;
+    margin-right: 20px;
+    margin-top: 5px;
+    vertical-align: middle;
+    font-size: 18px;
   }
 
   .btn-bg{
-  background-color: #4a5a79;
+    background-color: #4a5a79;
     cursor: pointer;
   }
 
   .btn-bg-2{
     cursor: pointer;
+    margin-top: 10px;
+    margin-right: 5px;
   }
 
   .btn-bg:hover{
@@ -76,8 +91,13 @@
   }
 
   .input-center{
-    vertical-align: middle;
+    margin-left: auto;
+    margin-right: auto;
   }
 
+  .checkbox-input{
+    margin-right: 215px;
+    margin-bottom: 20px;
+  }
 
 </style>
