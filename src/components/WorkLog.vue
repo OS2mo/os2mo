c7914f8973b04cd1cf50f8cce3ee4248059e2d9c
Novik <ns@magenta.dk>
2018-01-09 12:56:06 +0100

    Renamed and created new files

---
+++ src/components/WorkLog.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="card">
+  <div class="card-body">
+    <h5>Worklog</h5>
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{ name: 'OrganisationDetailUnit' }">
+            Arbejdslog
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{ name: 'OrganisationDetailLocation' }">
+            Begivenheder
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{ name: 'OrganisationDetailContact' }">
+            Fejl
+          </router-link>
+        </li>
+      </ul>
+  </div>
+</div>
+</template>
+
+<script>
+    export default {
+      name: 'worklog'
+    }
+</script>
+
+<style scoped>
+ 
+</style>
