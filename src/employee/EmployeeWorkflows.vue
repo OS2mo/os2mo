722754d15f69d864f28815227034b910ab634a3b
Anders Jepsen <anders@magenta.dk>
2018-01-10 13:22:51 +0100

    renamed workflow button to button workflow

--- src/employee/EmployeeWorkflows.vue
+++ src/employee/EmployeeWorkflows.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="workflows">
-    <workflow-button label="Ny Medarbejder" icon="user-plus" target="EmployeeCreate"/>
-    <workflow-button label="Orlov" icon="user-md" target="EmployeeLeave"/>
-    <workflow-button label="Flyt engagement" icon="share-square-o" target="EmployeeMove"/>
-    <workflow-button label="Flyt mange engagementer" icon="share-square-o" target="EmployeeMoveMany"/>
-    <workflow-button label="Afslut medarbejder" icon="user-times" target="EmployeeEnd"/>
+    <button-workflow label="Ny Medarbejder" icon="user-plus" target="EmployeeCreate"/>
+    <button-workflow label="Orlov" icon="user-md" target="EmployeeLeave"/>
+    <button-workflow label="Flyt engagement" icon="share-square-o" target="EmployeeMove"/>
+    <button-workflow label="Flyt mange engagementer" icon="share-square-o" target="EmployeeMoveMany"/>
+    <button-workflow label="Afslut medarbejder" icon="user-times" target="EmployeeEnd"/>
   </div>
 </template>
 
 <script>
-  import WorkflowButton from '../components/WorkflowButton'
+  import ButtonWorkflow from '../components/ButtonWorkflow'
 
   export default {
     components: {
-      WorkflowButton
+      ButtonWorkflow
     }
   }
 </script>
