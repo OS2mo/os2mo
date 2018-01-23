e7a824aad1482a08313d612c998feaf26eab815b
Anders Jepsen <anders@magenta.dk>
2018-01-23 09:38:47 +0100

    activated employee create engagement

--- src/employee/EmployeeWorkflows.vue
+++ src/employee/EmployeeWorkflows.vue
@@ -1,19 +1,25 @@
 <template>
-  <div id="workflows">
-    <button-workflow label="Ny Medarbejder" icon="user-plus" target="EmployeeCreate"/>
-    <button-workflow label="Orlov" icon="user-md" target="EmployeeLeave"/>
-    <button-workflow label="Flyt engagement" icon="share-square-o" target="EmployeeMove"/>
-    <button-workflow label="Flyt mange engagementer" icon="share-square-o" target="EmployeeMoveMany"/>
-    <button-workflow label="Afslut medarbejder" icon="user-times" target="EmployeeEnd"/>
+  <div>
+    <div id="workflows">
+      <button-workflow label="Ny Medarbejder" icon="user-plus" v-b-modal.employeeCreate/>
+      <button-workflow label="Orlov" icon="user-md" target="EmployeeLeave"/>
+      <button-workflow label="Flyt engagement" icon="share-square-o" target="EmployeeMove"/>
+      <button-workflow label="Flyt mange engagementer" icon="share-square-o" target="EmployeeMoveMany"/>
+      <button-workflow label="Afslut medarbejder" icon="user-times" target="EmployeeEnd"/>
+    </div>
+
+    <employee-create/>
   </div>
 </template>
 
 <script>
   import ButtonWorkflow from '../components/ButtonWorkflow'
+  import EmployeeCreate from './EmployeeCreate'
 
   export default {
     components: {
-      ButtonWorkflow
+      ButtonWorkflow,
+      EmployeeCreate
     }
   }
 </script>
