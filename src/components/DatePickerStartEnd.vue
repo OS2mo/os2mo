028e993d113e85b8111d8c1f506daa15b11e97e0
Anders Jepsen <anders@magenta.dk>
2018-02-09 11:35:59 +0100

    date picker start end can now be initially hidden

--- src/components/DatePickerStartEnd.vue
+++ src/components/DatePickerStartEnd.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="form-row">
+    <div v-show="hidden">
+      <button class="btn btn-link" @click="hidden=false">
+        Vælg anden validity
+      </button>
+    </div>
     <date-picker 
-      class="col"
       label="Startdato"
       v-model="validFrom"
+      v-show="!hidden"
       :disabled-from="validTo"
-      required
       @input="updateDate()"
+      required
     />
 
     <date-picker 
       label="Slutdato"
       v-model="validTo"
+      v-show="!hidden"
       :disabled-to="validFrom"
       @input="updateDate()"
     />
   </div>
-
 </template>
 
 <script>
@@ -27,13 +32,26 @@
       DatePicker
     },
     props: {
-      value: Object
+      value: Object,
+      initiallyHidden: Boolean
     },
     data () {
       return {
         validFrom: null,
-        validTo: null
+        validTo: null,
+        hidden: false
       }
+    },
+    watch: {
+      value (newVal) {
+        if (this.hidden) {
+          this.validFrom = newVal.from
+          this.validTo = newVal.to
+        }
+      }
+    },
+    created () {
+      this.hidden = this.initiallyHidden
     },
     methods: {
       updateDate () {
