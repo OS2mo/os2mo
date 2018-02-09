a88d9d301a3c04f1ca245687cd1fc360af7bd44c
Novik <ns@magenta.dk>
2018-02-09 14:18:52 +0100

    organisation landing page

--- src/organisation/OrganisationLandingPage.vue
+++ src/organisation/OrganisationLandingPage.vue
@@ -1,42 +1,15 @@
 <template>
-  <div class="card">
-    <div class="card-body">
-      <h4 class="card-title">{{org.name}}</h4>
-      
-      <p>Medarbejdere: {{org.person_count}}</p>
-      <p>Org funker: {{org.employment_count}}</p>
-      <p>Enheder: {{org.unit_count}}</p>
-    </div>
-  </div>
+
+  <organisation-landing-page-stats/>
+
 </template>
 
-
 <script>
-  import Organisation from '../api/Organisation'
-  import { EventBus } from '../EventBus'
+  import OrganisationLandingPageStats from './OrganisationLandingPageStats'
+  
   export default {
-    components: {},
-    data () {
-      return {
-        org: {}
-      }
-    },
-    created () {
-      this.getOrganisationDetails(Organisation.getSelectedOrganisation())
-    },
-    mounted () {
-      EventBus.$on('organisation-changed', newOrg => {
-        this.getOrganisationDetails(newOrg)
-      })
-    },
-    methods: {
-      getOrganisationDetails (newOrg) {
-        let vm = this
-        Organisation.get(newOrg.uuid)
-        .then(response => {
-          vm.org = response
-        })
-      }
+    components: {
+      OrganisationLandingPageStats
     }
   }
 </script>
