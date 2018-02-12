c52eb815fb90de5440fabdc2043a4830730fff95
Anders Jepsen <anders@magenta.dk>
2018-02-12 10:26:59 +0100

    the view now resets to base route when changing organisation

--- src/components/OrganisationPicker.vue
+++ src/components/OrganisationPicker.vue
@@ -23,8 +23,9 @@ import Organisation from '../api/Organisation'
 export default {
   name: 'OrganisationPicker',
   props: {
-    value: null,
-    atDate: Date
+    value: Object,
+    atDate: Date,
+    resetRoute: Boolean
   },
   data () {
     return {
@@ -38,6 +39,9 @@ export default {
   watch: {
     selectedOrganisation (newVal) {
       Organisation.setSelectedOrganisation(newVal)
+      if (this.resetRoute) {
+        this.resetToBaseRoute()
+      }
       this.$emit('input', newVal)
     },
 
@@ -53,12 +57,17 @@ export default {
         vm.orgs = response
         vm.selectedOrganisation = response[0]
       })
+    },
+
+    // resets the route back to base. So if we're viewing an employee, it goes back to the employee list
+    resetToBaseRoute () {
+      if (this.$route.name.indexOf('Organisation') > -1) {
+        this.$router.push({name: 'Organisation'})
+      }
+      if (this.$route.name.indexOf('Employee') > -1) {
+        this.$router.push({name: 'Employee'})
+      }
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
