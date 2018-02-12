84e7921cfca932e6ec2ba7903c2ef08ca3926cf1
Anders Jepsen <anders@magenta.dk>
2018-02-12 09:05:07 +0100

    refactored into a infobox component

--- src/organisation/OrganisationLandingPage.vue
+++ src/organisation/OrganisationLandingPage.vue
@@ -1,20 +1,55 @@
 <template>
-
-  <organisation-landing-page-stats/>
+  <div class="card">
+    <div class="card-body">
+      <loading v-show="isLoading"/>
+      <div v-show="!isLoading">
+      <h4 class="card-title">{{org.name}}</h4>
+      <div class="row justify-content-md-center">
+        <info-box icon="user" label="Medarbejdere" :info="org.person_count"/>
+        <info-box icon="globe" label="Org funker" :info="org.employment_count"/>
+        <info-box icon="users" label="Enheder" :info="org.unit_count"/>
+      </div>
+      </div>
+    </div>
+</div>
 
 </template>
 
 <script>
-  import OrganisationLandingPageStats from './OrganisationLandingPageStats'
+  import Organisation from '../api/Organisation'
+  import { EventBus } from '../EventBus'
+  import InfoBox from '../components/InfoBox'
+  import Loading from '../components/Loading'
   
   export default {
     components: {
-      OrganisationLandingPageStats
+      InfoBox,
+      Loading
+    },
+    data () {
+      return {
+        org: {},
+        isLoading: false
+      }
+    },
+    created () {
+      this.getOrganisationDetails(Organisation.getSelectedOrganisation())
+    },
+    mounted () {
+      EventBus.$on('organisation-changed', newOrg => {
+        this.getOrganisationDetails(newOrg)
+      })
+    },
+    methods: {
+      getOrganisationDetails (newOrg) {
+        let vm = this
+        vm.isLoading = true
+        Organisation.get(newOrg.uuid)
+        .then(response => {
+          vm.org = response
+          vm.isLoading = false
+        })
+      }
     }
   }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
