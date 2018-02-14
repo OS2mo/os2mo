6c2d01a6d1cdc7b8f878f36a24c237a26c0b8056
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2018-02-14 18:09:29 +0100

    common: add a header so that it appears on the documentation site

--- mora/service/common.py
+++ mora/service/common.py
@@ -5,6 +5,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
+
+'''Common utilities
+----------------
+
+'''
+
 import abc
 import collections
 import copy
