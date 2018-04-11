05d6af600fba23a05b96de8bc784940785f47ad5
Anders Jepsen <anders@magenta.dk>
2018-04-11 11:38:15 +0200

    dates now show correctly

--- src/filters/Date.js
+++ src/filters/Date.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 
 const moment = require('moment')
-// require('moment/locale/da')
 
-// Vue.use(require('vue-moment'), {
-//   moment
-// })
 Vue.filter('date', function (value) {
   if (value === null || value === undefined) return
   return moment(value).format('DD-MM-YYYY')
