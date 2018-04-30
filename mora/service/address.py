bd2545fd0a3284f6f1f9f9323413dac0248c7535
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2018-04-30 13:18:57 +0200

    docs: fix link to address module
    Imports should come _after_ the docstring.

--- mora/service/address.py
+++ mora/service/address.py
@@ -6,8 +6,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-import json
-
 '''Addresses
 ---------
 
@@ -150,6 +148,7 @@ API
 '''
 
 import itertools
+import json
 import re
 
 import flask
