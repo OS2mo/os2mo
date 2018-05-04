7e14f02337e3646e846c3cffafb8b55b2a220eb4
Anders Jepsen <anders@magenta.dk>
2018-05-04 08:26:49 +0200

    returns an error if trying to close org unit with children

--- src/api/OrganisationUnit.js
+++ src/api/OrganisationUnit.js
@@ -198,7 +198,8 @@ export default {
         return response.data
       })
       .catch(error => {
-        store.commit('log/newError', {type: 'ERROR', value: error.response})
+        store.commit('log/newError', {type: 'ERROR', value: error.response.data})
+        return error.response.data
       })
   }
 }
