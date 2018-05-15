ab924a790c7a2d6fd8f847eef07e1ebc02adab3a
Novik <ns@magenta.dk>
2018-05-15 13:46:23 +0200

    Added confirm checkbox for move engagement

--- src/employee/MoEmployeeWorkflows/MoEmployeeMove.vue
+++ src/employee/MoEmployeeWorkflows/MoEmployeeMove.vue
@@ -9,29 +9,37 @@
     no-close-on-backdrop
     @hidden="resetData"
   >
-    <form @submit.stop.prevent="moveEmployee">
-      <mo-employee-picker v-model="employee" required/>
+  <form @submit.stop.prevent="moveEmployee">
+    <mo-employee-picker v-model="employee" required/>
 
-      <div class="form-row">
-        <mo-date-picker :label="$t('input_fields.move_date')" class="col" v-model="move.data.validity.from" required/>
-      </div>
-      
-      <div class="form-row">
-        <mo-engagement-picker v-model="original" :employee="employee" required/>
-      </div>
+    <div class="form-row">
+      <mo-date-picker :label="$t('input_fields.move_date')" class="col" v-model="move.data.validity.from" required/>
+    </div>
+    
+    <div class="form-row">
+      <mo-engagement-picker v-model="original" :employee="employee" required/>
+    </div>
 
-      <div class="form-row">
-        <mo-organisation-unit-search
-          :label="$t('input_fields.move_to')" 
-          class="col" 
-          v-model="move.data.org_unit"
-          required
-        />       
-      </div>
+    <div class="form-row">
+      <mo-organisation-unit-search
+        :label="$t('input_fields.move_to')" 
+        class="col" 
+        v-model="move.data.org_unit"
+        required
+      />       
+    </div>
 
     <div class="float-right">
       <button-submit :is-loading="isLoading"/>
     </div>
+
+    <mo-confirm-checkbox
+      :entry-date="move.data.validity.from"
+      :entry-name="original.engagement_type.name"
+      :entry-org-name="original.org_unit.name"
+      v-if="this.dateConflict" 
+      required
+    />
   </form>
 </b-modal>
 </template>
@@ -43,6 +51,7 @@
   import MoEngagementPicker from '@/components/MoPicker/MoEngagementPicker'
   import MoEmployeePicker from '@/components/MoPicker/MoEmployeePicker'
   import ButtonSubmit from '@/components/ButtonSubmit'
+  import MoConfirmCheckbox from '@/components/MoConfirmCheckbox'
 
   export default {
     $_veeValidate: {
@@ -53,13 +62,19 @@
       MoOrganisationUnitSearch,
       MoEngagementPicker,
       MoEmployeePicker,
-      ButtonSubmit
+      ButtonSubmit,
+      MoConfirmCheckbox
+    },
+    props: {
+      entryName: String,
+      entryDate: Date,
+      entryOrgName: String
     },
     data () {
       return {
         employee: {},
         isLoading: false,
-        original: {},
+        original: null,
         move: {
           type: 'engagement',
           data: {
@@ -74,6 +89,16 @@
         return Object.keys(this.fields).every(field => {
           return this.fields[field] && this.fields[field].valid
         })
+      },
+
+      dateConflict () {
+        if (this.move.data.validity.from && this.original) {
+          if (this.original.validity.to == null) return true
+          this.move.data.validity.from = new Date(this.move.data.validity.from)
+          this.original.validity.to = new Date(this.original.validity.to)
+          if (this.move.data.validity.from <= this.original.validity.to) return true
+        }
+        return false
       }
     },
     methods: {
