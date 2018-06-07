1f4c9ce62a06de30719ebdd0a95a5a9060fa8531
Novik <ns@magenta.dk>
2018-06-07 10:14:59 +0200

    Added testCafe and first test

---
+++ e2e-tests/MyFirstTest.js
@@ -0,0 +1,10 @@
+fixture('Getting Started')
+  .page('http://localhost:8080/login/')
+
+test('My first test', async t => {
+  await t
+      .click('.btn-text')
+      .typeText('input[name="username"]', 'Bendtner')
+      .typeText('input[name="password"]', '12345')
+      .click('button[type="submit"]')
+})
