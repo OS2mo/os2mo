2322cc218161cc573720aca0eb95f51b35c097a9
Anders Jepsen <anders@magenta.dk>
2018-06-18 12:44:59 +0200

    removed build-test, it was unneccesary

--- config/test.env.js
+++ config/test.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"',
-  USERSNAP_KEY: '"0679ace2-8ba0-4e07-81a3-fc50d0eb1dea"'
+  NODE_ENV: '"testing"'
 })
