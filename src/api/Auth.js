a2f3d78de673243876914d8da51aff5369aecccb
Christoffer Moesgaard <christoffer@magenta.dk>
2018-06-22 13:08:43 +0200

    Attempt at capturing 401 errors on calls to Service

--- src/api/Auth.js
+++ src/api/Auth.js
@@ -1,20 +1,10 @@
-import { HTTP, Service } from './HttpCommon'
-import { EventBus } from '../EventBus'
+import Service from './HttpCommon'
 
 let currentUser = {
   username: 'Admin'
 }
 
 export default {
-
-  setUser (user) {
-    return HTTP.get('/o')
-      .then(response => {
-        EventBus.$emit('login-success', user)
-        currentUser = user
-        return true
-      })
-  },
 
   getUser () {
     return currentUser
--- src/api/HttpCommon.js
+++ src/api/HttpCommon.js
@@ -3,18 +3,8 @@ import axios from 'axios'
 /**
  * Defines the base url and headers for http calls
  */
-export const HTTP = axios.create({
-  // baseURL: 'http://localhost:8080',
-  baseURL: '/mo',
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT'
-  }
-})
 
-export const Service = axios.create({
+const Service = axios.create({
   baseURL: '/service',
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
@@ -23,3 +13,24 @@ export const Service = axios.create({
     'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT'
   }
 })
+
+function responseErrorHandler (error) {
+  if (error.response.status === 401) {
+    // Route to login page, or something
+    console.log(error.response.statusText)
+  }
+}
+
+export default {
+  get (url) {
+    return Service
+      .get(url)
+      .catch(responseErrorHandler)
+  },
+
+  post (url, payload) {
+    return Service
+      .post(url, payload)
+      .catch(responseErrorHandler)
+  }
+}
