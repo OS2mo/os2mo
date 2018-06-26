1205a8dc6d1e94a4a21863d40df7eb65fc31f670
Anders Jepsen <anders@magenta.dk>
2018-06-26 09:18:39 +0200

    can now handle allowed date intervals better

--- src/components/atoms/MoDatePicker.vue
+++ src/components/atoms/MoDatePicker.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="form-group col">
-      <label v-if="!noLabel" id="date-label" for="date">{{label}}</label>
-      <date-time-picker 
-        v-model="selected" 
-        format="dd-MM-yyyy"
-        :language="da"
-        monday-first
-        bootstrap-styling
-        clear-button
-        :disabled-dates="disabledDates"
-        :disabled="disabled"
-      />
+  <div class="form-group col">
+    <label v-if="!noLabel" id="date-label" for="date">{{label}}</label>
+    <date-time-picker 
+      v-model="selected" 
+      format="dd-MM-yyyy"
+      :language="da"
+      monday-first
+      bootstrap-styling
+      clear-button
+      :disabled-dates="disabledDates"
+      :disabled="disabled"
+    />
 
-      <input 
-        :name="nameId"
-        :data-vv-as="label" 
-        v-model="date_string"
-        type="hidden"
-        v-validate="{required: required}">
+    <input 
+      :name="nameId"
+      :data-vv-as="label" 
+      v-model="dateString"
+      type="hidden"
+      v-validate="{required: required, date_in_range: validDates}">
 
-      <span v-show="errors.has(nameId)" class="text-danger">{{ errors.first(nameId) }}</span>
-    </div>
+    <span v-show="errors.has(nameId)" class="text-danger">{{ errors.first(nameId) }}</span>
+  </div>
 </template>
 
 <script>
@@ -40,43 +40,44 @@ export default {
     required: Boolean,
     noLabel: Boolean,
     label: {default: 'Dato', type: String},
-    disabledTo: [Date, String],
-    disabledFrom: [Date, String],
+    validDates: Object,
     disabled: Boolean
   },
   data () {
     return {
-      disabledDates: {
-        to: null,
-        from: null
-      },
       selected: null,
-      date_string: null,
+      dateString: null,
       da: da
     }
   },
   computed: {
     nameId () {
       return 'date-picker-' + this._uid
+    },
+
+    disabledDates () {
+      return {
+        from: this.validDates.to ? this.validDates.to : null,
+        to: this.validDates.from ? this.validDates.from : null
+      }
     }
   },
   watch: {
     selected (newVal) {
       // send on a date-only string in ISO format, so that we
       // disregard timezones and the time-of-day
-      this.date_string = newVal ? this.$moment(new Date(newVal)).format('YYYY-MM-DD') : null
+      this.dateString = newVal ? this.$moment(new Date(newVal)).format('YYYY-MM-DD') : null
     },
 
-    date_string (newVal) {
+    disabledDates: {
+      handler (newVal) {
+        this.$validator.validate(this.nameId)
+      },
+      deep: true
+    },
+
+    dateString (newVal) {
       this.$emit('input', newVal)
-    },
-
-    disabledTo (newVal) {
-      this.disabledDates.to = newVal ? new Date(newVal) : null
-    },
-
-    disabledFrom (newVal) {
-      this.disabledDates.from = newVal ? new Date(newVal) : null
     },
 
     value (newVal) {
@@ -85,7 +86,7 @@ export default {
   },
   created () {
     this.selected = this.value
-    this.date_string = this.value
+    this.dateString = this.value
   }
 }
 </script>
