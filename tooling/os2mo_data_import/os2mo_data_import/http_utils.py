d4d4ee9fa4dc4bd921fdbe016faddef68b5ce3d0
Steffen Park <dev@istherepie.com>
2018-09-18 11:16:52 +0200

    23140: Fixed project structure

--- tooling/os2mo_data_import/os2mo_data_import/http_utils.py
+++ tooling/os2mo_data_import/os2mo_data_import/http_utils.py
@@ -77,3 +77,68 @@ def import_mox_data(resource, identifier, payload):
     )
 
     return put_request(service_endpoint, payload)
+
+
+def import_mora_data(resource, payload):
+
+    # TODO: add resolve functionality
+    service_endpoint = "{base}/{resource}".format(
+        base=MORA_BASE_URI,
+        resource=resource
+    )
+
+    return post_request(service_endpoint, payload)
+
+
+def import_org_unit(payload):
+
+    processed_payload = payload
+
+    return import_mora_data("service/ou/create", processed_payload)
+
+
+def import_relationship(unit_name, metadata):
+    print("METADATA for {}:".format(unit_name))
+    print(metadata)
+
+    return True
+
+
+def import_all_org_units(org_unit_object):
+
+    inserted = []
+
+    for unit_name in org_unit_object.get_available():
+        unit_uuid = org_unit_object.get_uuid(unit_name)
+        unit_data = org_unit_object.get_data(unit_name)
+        unit_metadata = org_unit_object.get_metadata(unit_name)
+
+        print(unit_name, unit_data, unit_metadata)
+
+        unit_import_uuid = {
+            "uuid": unit_uuid
+        }
+
+        unit_data.update(unit_import_uuid)
+
+        store = import_org_unit(unit_data)
+
+        if store:
+            inserted.append(unit_uuid)
+
+        create_rel = import_relationship(unit_name, unit_metadata)
+
+
+    return inserted
+
+def import_all_employees():
+
+    data = {
+        "name": "Jean-Luc And",
+        "cpr_no": "0101501234",
+        "user_key": "1234",
+        "org": {
+            "uuid": "62ec821f-4179-4758-bfdf-134529d186e9"
+        },
+        "uuid": "f005a114-e5ef-484b-acfd-bff321b26e3f"
+    }
