6360c2e84e7202d476c53a69a0d78c4396afc564
Christoffer Moesgaard <christoffer@magenta.dk>
2018-10-26 15:02:44 +0200

    Removed unused imports in settings.py

--- backend/mora/settings.py
+++ backend/mora/settings.py
@@ -5,8 +5,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-import json
-from os import getenv
 from os import path as _path
 
 from . import util as _util
