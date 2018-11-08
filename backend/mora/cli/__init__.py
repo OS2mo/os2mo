cdb4a77c35665bd13152b8bcad5b151caab5d5a6
jgj-magenta-aps <jgj@magenta-aps.dk>
2018-11-08 16:34:02 +0100

    patch for test

--- backend/mora/cli/__init__.py
+++ backend/mora/cli/__init__.py
@@ -6,5 +6,29 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
+import os
+import sys
+import oio_common
+
+# cant do monkeypatching only because file is used as program
+try:
+    #first file patching in case we have test module installed:
+    from oio_common import db_structure
+    orgfile = db_structure.__file__
+    backenddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    topdir = os.path.dirname(backenddir)
+    patchfile = os.path.join(topdir, 'setup', "db_structure.py")
+    if not open(orgfile).read() == open(patchfile).read():
+        open(orgfile,"w").write(open(patchfile).read())
+
+    # then monkeypatching necessary for first time running tests (jenkins)
+    sys.path.insert(0,os.path.join(topdir, 'setup',))
+    import db_structure as new_db_structure
+    oio_common.db_structure = new_db_structure
+    sys.path.pop(0)
+except:
+    pass
+
+
 from . import base  # noqa
 from . import lora  # noqa
