9efee8adc79946a996dc2ec4ae01b08dabf2cae0
Christoffer Moesgaard <christoffer@magenta.dk>
2018-11-19 11:22:07 +0100

    23799: Added custom UUID url converter, converting UUIDs to strings rather than UUID-objects.

--- backend/mora/util.py
+++ backend/mora/util.py
@@ -36,6 +36,7 @@ import uuid
 import flask
 import dateutil.parser
 import dateutil.tz
+from werkzeug.routing import UUIDConverter
 
 from . import exceptions
 from . import mapping
@@ -792,3 +793,9 @@ def get_args_flag(name: str):
         return False
     else:
         return bool(v)
+
+
+class StrUUIDConverter(UUIDConverter):
+    """Custom URL converter returning UUIDs as strings rather than UUIDs"""
+    def to_python(self, value):
+        return str(value)
