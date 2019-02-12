75c2f7242baddb1ec22420199deb45e62524b08a
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2019-02-12 16:14:26 +0100

    testcafé: consolidate IT reading tests into one

--- frontend/e2e-tests/MoReading.js
+++ frontend/e2e-tests/MoReading.js
@@ -1,6 +1,20 @@
 import VueSelector from 'testcafe-vue-selectors'
 import { baseURL } from './support'
 
+fixture('MoEmployeeITSystem')
+  .page(`${baseURL}/medarbejder/75878240-2999-4bb4-b9c0-665f1553ef25`)
+
+test('Read IT System', async t => {
+  await t
+    .click(VueSelector('employee-detail-tabs bTabButtonHelper').withText('IT'))
+    .expect(VueSelector('mo-link').filter('.itsystem-name').exists)
+    .ok()
+    .expect(VueSelector('mo-link').filter('.itsystem-name').innerText)
+    .contains('Active Directory')
+    .expect(VueSelector('mo-link').filter('.user_key').innerText)
+    .contains('LineC')
+})
+
 fixture('MoOrganisationUnitITSystem')
   .page(`${baseURL}/organisation/97337de5-6096-41f9-921e-5bed7a140d85`)
 
