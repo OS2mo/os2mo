e9a58881f8d9c30abd9698e06bbf47de0f30820f
Novik <ns@magenta.dk>
2019-03-12 15:46:23 +0100

    Select create button

--- frontend/e2e-tests/MoEmployeeAssociationTab.js
+++ frontend/e2e-tests/MoEmployeeAssociationTab.js
@@ -1,4 +1,5 @@
 import VueSelector from 'testcafe-vue-selectors'
+import { Selector } from 'testcafe'
 import { baseURL } from './support'
 
 let moment = require('moment')
@@ -6,7 +7,9 @@ let moment = require('moment')
 fixture('MoEmployeeAssociationTab')
   .page(`${baseURL}/medarbejder/542adb9a-4ec4-44b5-a0ad-3113d2987968`)
 
-const dialogCreate = VueSelector('#nameId')
+const dialog = VueSelector('#nameId')
+
+const createButtonClick = VueSelector('button btn btn-outline-primary]')
 
 test('Workflow: employee association tab', async t => {
   let today = moment()
@@ -16,5 +19,5 @@ test('Workflow: employee association tab', async t => {
 
     .click(VueSelector('employee-detail-tabs bTabButtonHelper').withText('Tilknytninger'))
 
-    .click(dialogCreate.find('btn btn-outline-primary'))
+    .click(Selector('.btn-outline-primary').withText('Opret ny'))
 })
