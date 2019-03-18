6aa52f29131e94580058490f8b80c2d42f685e9d
Novik <ns@magenta.dk>
2019-03-18 10:53:54 +0100

    Changed test

--- frontend/e2e-tests/MoEmployeeAssociationTab.js
+++ frontend/e2e-tests/MoEmployeeAssociationTab.js
@@ -7,9 +7,15 @@ let moment = require('moment')
 fixture('MoEmployeeAssociationTab')
   .page(`${baseURL}/medarbejder/542adb9a-4ec4-44b5-a0ad-3113d2987968`)
 
-const dialog = VueSelector('#nameId')
+const dialog = Selector('#nameId')
 
 const createButtonClick = VueSelector('button btn btn-outline-primary]')
+
+// Association
+const parentAssociationInput = dialog.find('input[data-vv-as="Angiv enhed"]')
+
+const associationTypeSelect = dialog.find('.select-association select[data-vv-as="Tilknytningsrolle"]')
+const associationTypeOption = associationTypeSelect.find('option')
 
 test('Workflow: employee association tab', async t => {
   let today = moment()
@@ -20,4 +26,13 @@ test('Workflow: employee association tab', async t => {
     .click(VueSelector('employee-detail-tabs bTabButtonHelper').withText('Tilknytninger'))
 
     .click(Selector('.btn-outline-primary').withText('Opret ny'))
+
+    // Create association
+    .click(dialog.find('.btn-association .btn-outline-success'))
+
+    .click(parentAssociationInput)
+    .click(dialog.find('.unit-association span.tree-anchor'))
+
+    .click(associationTypeSelect)
+    .click(associationTypeOption.withText('Konsulent'))
 })
