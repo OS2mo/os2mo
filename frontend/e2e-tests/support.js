525fdc14882c83c606924f79c08e22bacaab490e
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2019-03-18 18:04:33 +0100

    e2e-tests: use the axios package we already have available

--- frontend/e2e-tests/support.js
+++ frontend/e2e-tests/support.js
@@ -1,8 +1,8 @@
-import fetch from 'node-fetch'
+import axios from 'axios'
 
 export let baseURL = process.env.BASE_URL ?
   process.env.BASE_URL.replace(/\/+$/, '') : 'http://localhost:8080'
 
 export async function reset(test) {
-  await fetch(baseURL + '/reset-db')
+  await axios.get(baseURL + '/reset-db')
 }
