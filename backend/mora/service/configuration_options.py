77c9d8616b6f060cdf6c98d7b0ceb6ae2471428d
Robert Jensen <jensen.robert@gmail.com>
2019-05-23 09:39:46 +0000

    Fixed license headers.

--- backend/mora/service/configuration_options.py
+++ backend/mora/service/configuration_options.py
@@ -1,11 +1,10 @@
 #
-# Copyright (c) 2017-2018, Magenta ApS
+# Copyright (c) Magenta ApS
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# import sqlite3
 import psycopg2
 import flask
 from mora import exceptions
