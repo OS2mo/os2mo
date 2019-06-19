2ec92e5995e79dea88c92517dc5b9c3621529842
Robert Jensen <jensen.robert@gmail.com>
2019-06-19 09:25:19 +0000

    Flake8 fixes.

--- backend/mora/service/configuration_options.py
+++ backend/mora/service/configuration_options.py
@@ -27,14 +27,14 @@ def _get_connection():
 
 def get_configuration(unitid=None):
     if unitid:
-        query_suffix=" = %s"
+        query_suffix = " = %s"
     else:
-        query_suffix=" IS %s"
+        query_suffix = " IS %s"
 
     configuration = {}
     conn = _get_connection()
-    query = ("SELECT setting, value FROM orgunit_settings WHERE object"
-             + query_suffix)
+    query = ("SELECT setting, value FROM orgunit_settings WHERE object" +
+             query_suffix)
     try:
         cur = conn.cursor()
         cur.execute(query, (unitid,))
@@ -63,8 +63,8 @@ def set_configuration(configuration, unitid=None):
             cur.execute(query, (key, unitid))
             rows = cur.fetchall()
             if not rows:
-                query = ("INSERT INTO orgunit_settings (object, setting, "
-                         + "value) VALUES (%s, %s, %s)")
+                query = ("INSERT INTO orgunit_settings (object, setting, " +
+                         "value) VALUES (%s, %s, %s)")
                 cur.execute(query, (unitid, key, value))
             elif len(rows) == 1:
                 query = 'UPDATE orgunit_settings SET value=%s WHERE id = %s'
@@ -105,35 +105,6 @@ def set_org_unit_configuration(unitid):
     configuration = flask.request.get_json()
     return flask.jsonify(set_configuration(configuration, unitid))
 
-# 
-#    conn = _get_connection()
-#    try:
-#        cur = conn.cursor()
-# 
-#        configuration = flask.request.get_json()
-#        orgunit_conf = configuration['org_units']
-# 
-#        for key, value in orgunit_conf.items():
-#            query = ("SELECT id FROM orgunit_settings WHERE setting = %s " +
-#                     "AND object=%s")
-#            cur.execute(query, (key, unitid))
-#            rows = cur.fetchall()
-#            if not rows:
-#                query = ("INSERT INTO orgunit_settings (object, setting, " +
-#                         "value) VALUES (%s, %s, %s)")
-#                cur.execute(query, (unitid, key, value))
-#            elif len(rows) == 1:
-#                query = "UPDATE orgunit_settings SET value=%s WHERE id=%s"
-#                cur.execute(query, (value, rows[0][0]))
-#            else:
-#                exceptions.ErrorCodes.E_INCONSISTENT_SETTINGS(
-#                    'Inconsistent settings for {}'.format(unitid)
-#                )
-#            conn.commit()
-#    finally:
-#        conn.close()
-#    return flask.jsonify(True)
-
 
 @blueprint.route('/ou/<uuid:unitid>/configuration', methods=['GET'])
 @util.restrictargs()
@@ -143,7 +114,6 @@ def get_org_unit_configuration(unitid):
     .. :quickref: Unit; Read configuration setting.
 
     :statuscode 200: Setting returned.
-    :statuscode 404: No such unit found.
 
     :param unitid: The UUID of the organisational unit.
 
@@ -177,35 +147,6 @@ def set_global_configuration():
     configuration = flask.request.get_json()
     return flask.jsonify(set_configuration(configuration))
 
-#    conn = _get_connection()
-#    try:
-#        cur = conn.cursor()
-# 
-#        configuration = flask.request.get_json()
-#        orgunit_conf = configuration['org_units']
-# 
-#        for key, value in orgunit_conf.items():
-#            query = ("SELECT id FROM orgunit_settings WHERE setting = %s " +
-#                     "AND object IS NULL")
-#            cur.execute(query, (key,))
-#            rows = cur.fetchall()
-# 
-#            if len(rows) == 0:
-#                query = ("INSERT INTO orgunit_settings (object, setting, " +
-#                         "value) values (NULL, '%s', '%s')")
-#                cur.execute(query, (key, value))
-#            elif len(rows) == 1:
-#                query = "UPDATE orgunit_settings SET value=%s WHERE id=%s"
-#                cur.execute(query, (value, rows[0][0]))
-#            else:
-#                exceptions.ErrorCodes.E_INCONSISTENT_SETTINGS(
-#                    'Inconsistent global settings'
-#                )
-#            conn.commit()
-#    finally:
-#        conn.close()
-#    return flask.jsonify(True)
-
 
 @blueprint.route('/configuration', methods=['GET'])
 @util.restrictargs('at')
@@ -222,16 +163,3 @@ def get_global_configuration():
 
     configuration = get_configuration()
     return flask.jsonify(configuration)
-
-#    return_dict = {}
-#    conn = _get_connection()
-#    query = "SELECT setting, value FROM orgunit_settings WHERE object IS NULL"
-#    try:
-#        cur = conn.cursor()
-#        cur.execute(query)
-#        rows = cur.fetchall()
-#        for row in rows:
-#            return_dict[row[0]] = row[1]
-#    finally:
-#        conn.close()
-#    return flask.jsonify(return_dict)
