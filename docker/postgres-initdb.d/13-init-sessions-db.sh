e9a57db408f42ecc0a8e3a45463313569cd789c7
Mathias Dannesbo <md@magenta.dk>
2019-07-04 14:21:18 +0200

    Remove misleading comments

--- docker/postgres-initdb.d/13-init-sessions-db.sh
+++ docker/postgres-initdb.d/13-init-sessions-db.sh
@@ -13,7 +13,3 @@ EOSQL
 
 # we can connect without password because ``trust`` authentication for Unix
 # sockets is enabled inside the container.
-
-# Do not replace this script with a raw .sql script. If an .sql script fails
-# (entrypoint script will exit) and the container is restarted with an already
-# initialized data directory, the rest of the scripts will not be run.
