6048d459377e23717d4fbd10ac5608ae7022753e
jgj-magenta-aps <jgj@magenta-aps.dk>
2019-08-01 10:42:37 +0200

    triggers unit-test

---
+++ backend/tests/test_triggers.py
@@ -0,0 +1,129 @@
+
+#
+# Copyright (c) Magenta ApS
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from . import util
+from mora.triggers import Trigger
+from mora.service.handlers import (
+    RequestType,
+    RequestHandler,
+)
+
+# make registry local to this test
+class Trigger(Trigger):
+    registry={}
+
+class MockHandler(RequestHandler):
+    role_type = "mock"
+
+    def prepare_edit(self, req):
+        self.uuid = "edit"
+
+    def prepare_create(self, req):
+        self.uuid = "create"
+
+    def prepare_terminate(self, req):
+        self.uuid = "terminate"
+
+    def submit(self):
+        super().submit(result="okidoki")
+
+
+class Tests(util.TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        super().setUp()
+        self.trigger_called = False
+
+    def test_handler_trigger_before_edit(self):
+        @Trigger.on("mock", RequestType.EDIT, Trigger.Event.ON_BEFORE)
+        def trigger(trigger_dict):
+            self.trigger_called = True
+            self.assertEqual({
+                'event_type': Trigger.Event.ON_BEFORE,
+                'request': {},
+                'request_type': RequestType.EDIT,
+                'role_type': 'mock',
+                'uuid': 'edit'
+            }, trigger_dict)
+        MockHandler({}, RequestType.EDIT)
+        self.assertTrue(self.trigger_called)
+
+    def test_handler_trigger_after_edit(self):
+        @Trigger.on("mock", RequestType.EDIT, Trigger.Event.ON_AFTER)
+        def trigger(trigger_dict):
+            self.trigger_called = True
+            self.assertEqual({
+                'event_type': Trigger.Event.ON_AFTER,
+                'request': {},
+                'request_type': RequestType.EDIT,
+                'role_type': 'mock',
+                'uuid': 'edit',
+                'result': 'okidoki'
+            }, trigger_dict)
+        MockHandler({}, RequestType.EDIT).submit()
+        self.assertTrue(self.trigger_called)
+
+    def test_handler_trigger_before_create(self):
+        @Trigger.on("mock", RequestType.CREATE, Trigger.Event.ON_BEFORE)
+        def trigger(trigger_dict):
+            self.trigger_called = True
+            self.assertEqual({
+                'event_type': Trigger.Event.ON_BEFORE,
+                'request': {},
+                'request_type': RequestType.CREATE,
+                'role_type': 'mock',
+                'uuid': 'create'
+            }, trigger_dict)
+        MockHandler({}, RequestType.CREATE)
+        self.assertTrue(self.trigger_called)
+
+    def test_handler_trigger_after_create(self):
+        @Trigger.on("mock", RequestType.CREATE, Trigger.Event.ON_AFTER)
+        def trigger(trigger_dict):
+            self.trigger_called = True
+            self.assertEqual({
+                'event_type': Trigger.Event.ON_AFTER,
+                'request': {},
+                'request_type': RequestType.CREATE,
+                'uuid': 'create',
+                'role_type': 'mock',
+                'result': 'okidoki'
+            }, trigger_dict)
+        MockHandler({}, RequestType.CREATE).submit()
+        self.assertTrue(self.trigger_called)
+
+    def test_handler_trigger_before_terminate(self):
+        @Trigger.on("mock", RequestType.TERMINATE, Trigger.Event.ON_BEFORE)
+        def trigger(trigger_dict):
+            self.trigger_called = True
+            self.assertEqual({
+                'event_type': Trigger.Event.ON_BEFORE,
+                'request': {},
+                'request_type': RequestType.TERMINATE,
+                'role_type': 'mock',
+                'uuid': 'terminate'
+            }, trigger_dict)
+        MockHandler({}, RequestType.TERMINATE)
+        self.assertTrue(self.trigger_called)
+
+    def test_handler_trigger_after_terminate(self):
+        @Trigger.on("mock", RequestType.TERMINATE, Trigger.Event.ON_AFTER)
+        def trigger(trigger_dict):
+            self.trigger_called = True
+            self.assertEqual({
+                'event_type': Trigger.Event.ON_AFTER,
+                'request': {},
+                'request_type': RequestType.TERMINATE,
+                'uuid': 'terminate',
+                'role_type': 'mock',
+                'result': 'okidoki'
+            }, trigger_dict)
+        MockHandler({}, RequestType.TERMINATE).submit()
+        self.assertTrue(self.trigger_called)
