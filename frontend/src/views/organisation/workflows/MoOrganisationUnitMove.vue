2d71551997ebf8a9cf4c5d24b82b75e6455980e3
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2019-08-08 15:05:59 +0200

    unit move: make sure that the selected dates applies to the unit trees
    The end result of this change is that we can move a unit, but that the
    result of the move is that the unit is revived.

--- frontend/src/views/organisation/workflows/MoOrganisationUnitMove.vue
+++ frontend/src/views/organisation/workflows/MoOrganisationUnitMove.vue
@@ -24,8 +24,7 @@
             class="currentUnit"
             v-model="original"
             :label="$t('input_fields.select_unit')"
-            :date="move.data.validity.from"
-            :validity="validity"
+            :validity="requiredValidity"
             :extra-validations="unitValidations"
             required
           />
@@ -46,8 +45,7 @@
         class="parentUnit"
         v-model="parent"
         :label="$t('input_fields.select_new_super_unit')"
-        :date="move.data.validity.from"
-        :validity="validity"
+        :validity="requiredValidity"
         :disabled-unit="original"
         :extra-validations="parentValidations"
         required
@@ -110,9 +108,14 @@ export default {
   },
 
   computed: {
-    validity () {
+    /**
+     * A validity of one day, corresponding to the required validity
+     * of units: They only need to be valid on the date of the operation.
+     */
+    requiredValidity () {
       return {
-        'from': this.move.data.validity.from
+        from: this.move.data.validity.from,
+        to: this.move.data.validity.from
       }
     },
 
@@ -124,7 +127,7 @@ export default {
 
     parentValidations () {
       return {
-        candidate_parent_org_unit: [this.original, this.move.data.parent, this.validity]
+        candidate_parent_org_unit: [this.original, this.move.data.parent, this.move.data.validity]
       }
     }
   },
