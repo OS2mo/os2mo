550264cbe507357e1028a19bceafbfffff07ebea
jgj-magenta-aps <jgj@magenta-aps.dk>
2019-08-21 08:56:27 +0200

    check organisation config on startup

--- backend/mora/app.py
+++ backend/mora/app.py
@@ -93,5 +93,6 @@ def create_app(overrides: typing.Dict[str, typing.Any] = None):
         """
         exceptions.ErrorCodes.E_NO_SUCH_ENDPOINT()
 
+    service.org.check_config(app)
     serviceplatformen.check_config(app)
     return app
