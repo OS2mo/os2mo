fb72a0366577a8d3c1c5396cf2a53aeae836bd7c
jgj-magenta-aps <jgj@magenta-aps.dk>
2019-08-23 14:04:25 +0200

    repair tests

--- backend/tests/test_org.py
+++ backend/tests/test_org.py
@@ -10,17 +10,49 @@ from . import util
 from mora.service import org
 
 
-class Tests(util.LoRATestCase):
+class TestConfig(util.LoRATestCase):
     maxDiff = None
 
     def setUp(self):
         super().setUp()
-        self.load_sample_structures()
+        org.ConfiguredOrganisation.valid = False  # force validation each test
         self._get_valid_organisations = org.get_valid_organisations
 
     def tearDown(self):
         super().tearDown()
         org.get_valid_organisations = self._get_valid_organisations
+        org.ConfiguredOrganisation.valid = False  # force validation each test
+
+    def test_missing_conf_values_in_prod(self):
+        "error if not configured in production"
+        class AppProd():
+            env = "production"
+            config = {}
+        with self.assertRaises(KeyError):
+            org.check_config(AppProd)
+
+    def test_more_than_one_org_in_mo(self):
+        self.load_sample_structures()
+        org.get_valid_organisations = lambda: [{}, {}]
+        r = self.request('/service/o/')
+        self.assertEqual({
+            'error': True,
+            'count': 2,
+            'error_key': 'E_ORG_TOO_MANY',
+            'status': 400,
+            'description': 'Too many organisations in lora, max one allowed'
+        }, r.json)
+
+
+class Tests(util.LoRATestCase):
+    maxDiff = None
+
+    def setUp(self):
+        super().setUp()
+        org.ConfiguredOrganisation.valid = False  # force validation each test
+
+    def tearDown(self):
+        super().tearDown()
         org.ConfiguredOrganisation.valid = False  # force validation each test
 
     @util.override_config(
@@ -29,6 +61,7 @@ class Tests(util.LoRATestCase):
         ORGANISATION_UUID='456362c4-0ee4-4e5e-a72c-751239745e62',
     )
     def test_org_same_as_config(self):
+        self.load_sample_structures()
         r = self.request('/service/o/')
         self.assertEqual([{
             "uuid": '456362c4-0ee4-4e5e-a72c-751239745e62',
@@ -42,6 +75,7 @@ class Tests(util.LoRATestCase):
         ORGANISATION_UUID='456362c4-0ee4-4e5e-a72c-751239745e62',
     )
     def test_org_different_from_config(self):
+        self.load_sample_structures()
         r = self.request('/service/o/')
         self.assertEqual({
             'error': True,
@@ -51,30 +85,3 @@ class Tests(util.LoRATestCase):
             'status': 400,
             'description': 'Organisation configuration differs from database'
         }, r.json)
-
-    def test_more_than_one_org_in_mo(self):
-        org.get_valid_organisations = lambda: [{}, {}]
-        r = self.request('/service/o/')
-        self.assertEqual({
-            'error': True,
-            'count': 2,
-            'error_key': 'E_ORG_TOO_MANY',
-            'status': 400,
-            'description': 'Too many organisations in lora, max one allowed'
-        }, r.json)
-
-    def test_missing_conf_values_in_prod(self):
-        "error if not configured in production"
-        class AppProd():
-            env = "production"
-            config = {}
-        with self.assertRaises(KeyError):
-            org.check_config(AppProd)
-
-    def test_inferred_org_in_test_and_dev(self):
-        r = self.request('/service/o/')
-        self.assertEqual([{
-            "uuid": '456362c4-0ee4-4e5e-a72c-751239745e62',
-            "user_key": 'AU',
-            "name": 'Aarhus Universitet',
-        }], r.json)
