b8d3447ecd22929b72aee74a534244548816a48f
jgj-magenta-aps <jgj@magenta-aps.dk>
2019-08-23 11:43:11 +0200

    test reflecting changes in org

--- backend/tests/test_org.py
+++ backend/tests/test_org.py
@@ -10,49 +10,71 @@ from . import util
 from mora.service import org
 
 
-class AppProd():
-    env = "production"
-    config = {}
-
-
-class TestConfig(util.TestCase):
+class Tests(util.LoRATestCase):
+    maxDiff = None
 
     def setUp(self):
+        super().setUp()
+        self.load_sample_structures()
         self._get_valid_organisations = org.get_valid_organisations
 
     def tearDown(self):
+        super().tearDown()
         org.get_valid_organisations = self._get_valid_organisations
+        org.ConfiguredOrganisation.valid = False  # force validation each test
 
+    @util.override_config(
+        ORGANISATION_NAME='Aarhus Universitet',
+        ORGANISATION_USER_KEY='AU',
+        ORGANISATION_UUID='456362c4-0ee4-4e5e-a72c-751239745e62',
+    )
     def test_org_same_as_config(self):
-        "succed if ORGANISATION_UUID is the same as the one in lora"
-        with util.override_config(
-            ORGANISATION_UUID="c5395419-4c76-417f-9939-5a4bf81648d8"
-        ):
-            org.check_config(self.app)
+        r = self.request('/service/o/')
+        self.assertEqual([{
+            "uuid": '456362c4-0ee4-4e5e-a72c-751239745e62',
+            "user_key": 'AU',
+            "name": 'Aarhus Universitet',
+        }], r.json)
 
+    @util.override_config(
+        ORGANISATION_NAME='Universitet',
+        ORGANISATION_USER_KEY='UU',
+        ORGANISATION_UUID='456362c4-0ee4-4e5e-a72c-751239745e62',
+    )
     def test_org_different_from_config(self):
-        "raise ValueError if ORGANISATION_UUID is different from loras"
-        with util.override_config(
-            ORGANISATION_UUID="1234"
-        ):
-            with self.assertRaises(ValueError):
-                org.check_config(self.app)
+        r = self.request('/service/o/')
+        self.assertEqual({
+            'error': True,
+            'ORGANISATION_NAME': 'Universitet',
+            'ORGANISATION_USER_KEY': 'UU',
+            'error_key': 'E_ORG_CONFIG_BAD',
+            'status': 400,
+            'description': 'Organisation configuration differs from database'
+        }, r.json)
 
     def test_more_than_one_org_in_mo(self):
-        "only one organisation allowed in lora - IndexError"
         org.get_valid_organisations = lambda: [{}, {}]
-        with self.assertRaises(IndexError):
-            org.check_config(self.app)
+        r = self.request('/service/o/')
+        self.assertEqual({
+            'error': True,
+            'count': 2,
+            'error_key': 'E_ORG_TOO_MANY',
+            'status': 400,
+            'description': 'Too many organisations in lora, max one allowed'
+        }, r.json)
 
     def test_missing_conf_values_in_prod(self):
         "error if not configured in production"
+        class AppProd():
+            env = "production"
+            config = {}
         with self.assertRaises(KeyError):
             org.check_config(AppProd)
 
-    def test_inferred_org_in_mo(self):
-        "succeed if only one org in mo - config is inferred"
-        org.check_config(self.app)
-        self.assertEqual(
-            "c5395419-4c76-417f-9939-5a4bf81648d8",
-            self.app.config["ORGANISATION_UUID"]
-        )
+    def test_inferred_org_in_test_and_dev(self):
+        r = self.request('/service/o/')
+        self.assertEqual([{
+            "uuid": '456362c4-0ee4-4e5e-a72c-751239745e62',
+            "user_key": 'AU',
+            "name": 'Aarhus Universitet',
+        }], r.json)
