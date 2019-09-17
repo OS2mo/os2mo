1282776e72fd1036b788e0d2cf3523005069f4ba
Mathias Dannesbo <md@magenta.dk>
2019-09-17 11:00:17 +0200

    Upgrade database user to SUPERUSER for development

---
+++ dev-environment/postgres-initdb.d/30-upgrade-to-superuser.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+true "${DB_USER:?DB_USER is unset. Error}"
+
+psql -v ON_ERROR_STOP=1 <<-EOSQL
+    ALTER ROLE $DB_USER WITH SUPERUSER;
+EOSQL
+
+# we can connect without password because ``trust`` authentication for Unix
+# sockets is enabled inside the container.
