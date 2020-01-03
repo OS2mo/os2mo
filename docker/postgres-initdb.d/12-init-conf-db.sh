8be1d26a1d0cf678b6997820a0b54d5762cd28ca
Mathias Dannesbo <md@magenta.dk>
2020-01-03 09:18:44 +0100

    33656 Add licence and comment about approval from labs to some files

--- docker/postgres-initdb.d/12-init-conf-db.sh
+++ docker/postgres-initdb.d/12-init-conf-db.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+# Copyright (C) 2020 Magenta ApS, http://magenta.dk.
+# Contact: info@magenta.dk.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+################################################################################
+# Changes to this file requires approval from Labs. Please add a person from   #
+# Labs as required approval to your MR if you have any changes.                #
+################################################################################
+
 set -e
 
 true "${CONF_DB_USER:?CONF_DB_USER is unset. Error!}"
