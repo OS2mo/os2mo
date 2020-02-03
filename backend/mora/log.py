b61d161780f429c50817e04fce122e0ee774dd79
Christoffer Moesgaard <christoffer@magenta.dk>
2020-02-03 14:40:06 +0100

    33450: Further streamline logging in OS2mo
    Remove activity log until we figure out what we actually need it for

--- backend/mora/log.py
+++ backend/mora/log.py
@@ -26,10 +26,6 @@ def init():
     stdout_log_handler.setLevel(log_level)  # this can be higher
     logger.addHandler(stdout_log_handler)
 
-    # The trace log contains debug statements (in context with everything
-    # higher precedens!) and is intended to be read by humans (tm) when
-    # something goes wrong. Please *do* write tracebacks and perhaps even
-    # pprint.pformat these messages.
     file_log_handler = RotatingFileHandler(
         filename=settings.config["log"]["log_path"],
         maxBytes=1000000,
@@ -37,14 +33,3 @@ def init():
     file_log_handler.setFormatter(log_format)
     file_log_handler.setLevel(log_level)
     logger.addHandler(file_log_handler)
-
-    # The activity log is for everything that isn't debug information. Only
-    # write single lines and no exception tracebacks here as it is harder to
-    # parse.
-    activity_log_handler = RotatingFileHandler(
-        filename=settings.config["log"]["activity_log_path"],
-        maxBytes=1000000,
-    )
-    activity_log_handler.setFormatter(log_format)
-    activity_log_handler.setLevel(logging.INFO)
-    logger.addHandler(activity_log_handler)
