e9654a762b084037d6d9602deab52af4a821bae1
Mathias Wilken <mw@magenta.dk>
2020-12-10 11:46:25 +0100

    [#38650] added some functions used in test

--- backend/mora/conf_db/__init__.py
+++ backend/mora/conf_db/__init__.py
@@ -12,10 +12,12 @@ from sqlalchemy import Column, Integer, String, Text, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select
-from sqlalchemy_utils import UUIDType
+from sqlalchemy_utils import UUIDType, database_exists, create_database
+from alembic.config import Config as AlembicConfig
+from alembic import command
+from pathlib import Path
 
 logger = logging.getLogger("mo_configuration")
-
 
 Base = declarative_base()
 
@@ -60,6 +62,49 @@ def _get_session_maker():
     engine = _get_engine()
     Session = sessionmaker(bind=engine)
     return Session
+
+
+def _createdb(force: bool = True):
+    """
+    Create a new database and initialize table.
+    Requires CREATEDB or SUPERUSER privileges.
+
+    :param force: create even if database already exists
+    :return: None
+
+    """
+    engine = _get_engine()
+    if force or not database_exists(engine.url):
+        create_database(engine.url)
+    create_db_table()
+
+
+def drop_db_table():
+    """Drop the config database."""
+    logger.info("Dropping configuration database.")
+    engine = _get_engine()
+    Base.metadata.drop_all(engine)
+    logger.info("Configuration database dropped.")
+
+
+def create_db_table():
+    """Initialize the config database.
+    """
+    logger.info("Initializing configuration database.")
+    engine = _get_engine()
+    Base.metadata.create_all(engine)
+
+    base_path = Path("backend/mora/conf_db")
+    ini = Path('alembic.ini')
+    alembic_dir = Path('alembic')
+    ini_path = base_path / ini
+    alembic_path = base_path / alembic_dir
+
+    alembic_cfg = AlembicConfig(file_=str(ini_path))
+    alembic_cfg.set_main_option("script_location", str(alembic_path))
+
+    command.upgrade(alembic_cfg, "head")
+    logger.info("Configuration database initialised.")
 
 
 @contextmanager
