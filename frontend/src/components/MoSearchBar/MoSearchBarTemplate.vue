3d5720e208d8af81272e08edf810543e05f0d1de
Mathias Wilken <mw@magenta.dk>
2020-12-11 14:32:49 +0100

    [#39858] Added user_key to search

--- frontend/src/components/MoSearchBar/MoSearchBarTemplate.vue
+++ frontend/src/components/MoSearchBar/MoSearchBarTemplate.vue
@@ -2,7 +2,7 @@ SPDX-FileCopyrightText: 2017-2020 Magenta ApS
 SPDX-License-Identifier: MPL-2.0
 <template>
   <div>
-    <span>{{ item.name }}</span>
+    <span>{{ item.name }} <span v-if=showUserKeyInSearch style="color:grey">{{" " + item.user_key }}</span></span>
   </div>
 </template>
 
@@ -17,6 +17,13 @@ export default {
      * Defines a required item name.
      */
     item: { required: true }
+  },
+  computed: {
+    showUserKeyInSearch () {
+      let conf = this.$store.getters['conf/GET_CONF_DB']
+
+      return conf.show_user_key_in_search
+    },
   }
 }
 </script>
