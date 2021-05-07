d7e4996b5a0ce89835a1b33dbf7072bf50cfff87
Nina Jensen <nkj@magenta-aps.dk>
2021-05-07 11:45:29 +0200

    Version 0.3.2

--- ramodels/__init__.py
+++ ramodels/__init__.py
@@ -3,4 +3,4 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 # --------------------------------------------------------------------------------------
-__version__ = "0.3.1"
+__version__ = "0.3.2"
