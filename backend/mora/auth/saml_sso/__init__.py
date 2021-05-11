5955526038bebbebd0ad3020e69a2ec7d41f5f14
Christoffer Moesgaard <christoffer@magenta.dk>
2021-05-11 15:11:37 +0200

    [#43300] Add flask_saml_sso enum compatibility
    The existing sessions are saved containing an enum, referring to an enum
    located in the old namespace of the original library
    
    As the code has been copied over, including the enum, we perform some
    disgusting monkey patching to be able to resolve the enums, despite
    using the old namespace.

--- backend/mora/auth/saml_sso/__init__.py
+++ backend/mora/auth/saml_sso/__init__.py
@@ -1,6 +1,13 @@
 # SPDX-FileCopyrightText: 2021- Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
 
+# XXX: This is a terrible compatibility hack, to ensure we are able to resolve the
+# existing sessions containing an enum from the original library
+import sys
+from . import session
+sys.modules['flask_saml_sso'] = sys.modules[__name__]
+sys.modules['flask_saml_sso.session'] = session
+
 from .base import init_app # noqa
 from .base import check_saml_authentication # noqa
 from .base import get_session_attributes # noqa
