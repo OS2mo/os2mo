97a01955c30344ae03ec7e40df6217e6f7e53982
Mads Riis-Weitling <mrw@magenta.dk>
2021-05-14 16:55:51 +0200

    [#43405] Remove `frontend/src/modules/timeMachine`
    [#43405] Remove unused URL and `common.json` i18n content

--- frontend/src/modules/timeMachine/install.js
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2019-2020 Magenta ApS
-// SPDX-License-Identifier: MPL-2.0
-
-import MoNavbar from '@/api/MoNavbar.js'
-import 'vue-awesome/icons/history'
-
-const ShortcutButton = () => import('./_components/ShortcutButton.vue')
-
-MoNavbar.addShortcut(ShortcutButton, 200)
