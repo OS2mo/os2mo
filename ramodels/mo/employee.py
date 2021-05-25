b3c2116d0199fb3e02693890ffcb47e1d4d6184d
Nina Jensen <nkj@magenta-aps.dk>
2021-05-25 15:53:02 +0200

    [#43422] Update employee validators & tests

--- ramodels/mo/employee.py
+++ ramodels/mo/employee.py
@@ -6,19 +6,65 @@
 # --------------------------------------------------------------------------------------
 # Imports
 # --------------------------------------------------------------------------------------
+from datetime import date
 from datetime import datetime
+from typing import Any
 from typing import Literal
 from typing import Optional
 
+from pydantic import Field
+from pydantic import validator
+
 from ._shared import MOBase
+from ramodels.base import tz_isodate
 
 # --------------------------------------------------------------------------------------
-# Employee model
+# Employee modelcode_msd
 # --------------------------------------------------------------------------------------
 
 
 class Employee(MOBase):
     type: Literal["employee"] = "employee"
     name: str
-    cpr_no: Optional[str]
+    cpr_no: Optional[str] = Field(regex=r"^\d{9}[1-9]$")
     seniority: Optional[datetime]
+
+    @validator("seniority", pre=True, always=True)
+    def parse_seniority(cls, seniority: Optional[Any]) -> Optional[datetime]:
+        return tz_isodate(seniority) if seniority is not None else None
+
+    @validator("cpr_no")
+    def validate_cpr(cls, cpr_no: Optional[str]) -> Optional[str]:
+        if cpr_no is not None:
+            # The string is validated with regex first, so we know this works
+            # We only obtain the most significant digit of the code part, since it's
+            # what we need for century calculations,
+            # cf. https://da.wikipedia.org/wiki/CPR-nummer
+            day, month = (cpr_no[0:2], cpr_no[2:4])
+            year, code_msd = map(int, (cpr_no[4:6], cpr_no[6]))
+
+            # TODO: let's do pattern matching in 3.10:
+            # https://www.python.org/dev/peps/pep-0622/ <3
+            century: int = 0
+            if code_msd < 4:
+                century = 1900
+            elif code_msd in {4, 9}:
+                if 0 <= year <= 36:
+                    century = 2000
+                if 37 <= year <= 99:
+                    century = 1900
+            elif 5 <= code_msd <= 8:
+                if 0 <= year <= 57:
+                    century = 2000
+                if 58 <= year <= 99:
+                    century = 1800
+
+            err_msg = f"CPR number {cpr_no} is not valid."
+            if century not in {1800, 1900, 2000}:
+                raise ValueError(err_msg)
+            try:
+                date.fromisoformat(f"{century+year}-{month}-{day}")
+            except Exception:
+                raise ValueError(err_msg)
+
+        return cpr_no
--- tests/mo/test_employee.py
+++ tests/mo/test_employee.py
@@ -1,33 +1,59 @@
 #!/usr/bin/env python3
-# --------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
-# --------------------------------------------------------------------------------------
-# --------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
 # Imports
-# --------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
+import re
 from datetime import datetime
+
+import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+from pydantic import ValidationError
 
 from ramodels.mo.employee import Employee
 
 
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
 # Tests
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
+
+
+@st.composite
+def valid_cprs(draw):
+    valid_date = draw(st.dates())
+    valid_code = draw(st.from_regex(r"^\d{3}[1-9]$"))
+    return valid_date.strftime("%d%m%y") + valid_code
 
 
 class TestEmployee:
-    def test_required_fields(self):
-        # Fails if new required fields are added or existing is removed
-        assert Employee(
-            name="Anders And",
-        )
+    invalid_cprs = st.text().filter(lambda s: re.match(r"^\d{9}[1-9]$", s) is None)
 
-    def test_optional_fields(self):
-        # Fails if an optional field is removed
-        assert Employee(
-            type="employee",
-            name="Anders And",
-            cpr_no="0707614285",
-            seniority=datetime(year=2021, month=5, day=12),
-        )
+    @given(
+        st.text().filter(lambda s: s != "employee"),
+        st.text(),
+        valid_cprs(),
+        st.dates(),
+    )
+    def test_init(self, type, name, cpr_no, hy_date):
+        # Required
+        assert Employee(name=name)
+
+        # Optional
+        assert Employee(name=name, cpr_no=cpr_no, seniority=hy_date)
+
+        # type value error
+        with pytest.raises(ValidationError, match="unexpected value;"):
+            Employee(type=type, name=name)
+
+    @given(st.text(), invalid_cprs, st.dates())
+    def test_validators(self, name, invalid_cpr, hy_date):
+        with pytest.raises(ValidationError, match="string does not match regex"):
+            Employee(name=name, cpr_no=invalid_cpr)
+
+        empl = Employee(name=name, seniority=hy_date)
+        assert isinstance(empl.seniority, datetime)
+        assert empl.seniority.tzinfo
