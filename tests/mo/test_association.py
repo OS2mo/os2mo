d3a5f8697c7e5ff0354243d0bd9e5971be903cb5
Nina Jensen <nkj@magenta-aps.dk>
2021-05-25 15:51:40 +0200

    [#43422] Update MO/Association tests

--- tests/mo/test_association.py
+++ tests/mo/test_association.py
@@ -6,34 +6,62 @@
 # --------------------------------------------------------------------------------------
 # Imports
 # --------------------------------------------------------------------------------------
-from uuid import uuid4
+import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+from pydantic import ValidationError
 
-from ramodels.mo._shared import AssociationType
-from ramodels.mo._shared import OrgUnitRef
-from ramodels.mo._shared import PersonRef
-from ramodels.mo._shared import Validity
 from ramodels.mo.association import Association
+from tests.conftest import valid_dt_range
+from tests.mo.test__shared import valid_assoc_type
+from tests.mo.test__shared import valid_org_unit
+from tests.mo.test__shared import valid_pers
+from tests.mo.test__shared import valid_validity
 
 
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
 # Tests
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
 
 
 class TestAssociation:
-    def test_required_fields(self):
+    @given(
+        st.text().filter(lambda s: s != "association"),
+        valid_org_unit(),
+        valid_pers(),
+        valid_assoc_type(),
+        valid_validity(),
+    )
+    def test_init(self, type, org_unit, person, assoc_type, validity):
+        # Required
         assert Association(
-            org_unit=OrgUnitRef(uuid=uuid4()),
-            person=PersonRef(uuid=uuid4()),
-            association_type=AssociationType(uuid=uuid4()),
-            validity=Validity(from_date="1930-01-01", to_date=None),
+            org_unit=org_unit,
+            person=person,
+            association_type=assoc_type,
+            validity=validity,
         )
 
-    def test_optional_fields(self):
-        assert Association(
-            type="association",
-            org_unit=OrgUnitRef(uuid=uuid4()),
-            person=PersonRef(uuid=uuid4()),
-            association_type=AssociationType(uuid=uuid4()),
-            validity=Validity(from_date="1930-01-01", to_date=None),
+        # type value error
+        with pytest.raises(ValidationError, match="unexpected value;"):
+            Association(
+                type=type,
+                org_unit=org_unit,
+                person=person,
+                association_type=assoc_type,
+                validity=validity,
+            )
+
+    @given(st.uuids(), st.uuids(), st.uuids(), st.uuids(), valid_dt_range())
+    def test_from_simplified_fields(
+        self, uuid, org_unit_uuid, person_uuid, assoc_type_uuid, valid_dts
+    ):
+        # Required
+        assert Association.from_simplified_fields(
+            uuid, org_unit_uuid, person_uuid, assoc_type_uuid
+        )
+
+        # Optional
+        from_dt, to_dt = valid_dts
+        assert Association.from_simplified_fields(
+            uuid, org_unit_uuid, person_uuid, assoc_type_uuid, from_dt, to_dt
         )
