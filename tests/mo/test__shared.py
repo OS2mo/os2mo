e69348cfbab25bd32ad3ba728452f59178239602
Nina Jensen <nkj@magenta-aps.dk>
2021-05-31 13:25:14 +0200

    [#43422] Update mo/_shared test strategies

--- tests/mo/test__shared.py
+++ tests/mo/test__shared.py
@@ -34,7 +34,6 @@ from ramodels.mo._shared import Primary
 from ramodels.mo._shared import Responsibility
 from ramodels.mo._shared import Validity
 from ramodels.mo._shared import Visibility
-from tests.conftest import valid_dt_range
 
 # --------------------------------------------------------------------------------------
 # MOBase
@@ -60,50 +59,67 @@ class TestMOBase:
 # --------------------------------------------------------------------------------------
 
 
+@st.composite
+def address_type_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
+
+
 class TestAddressType:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert AddressType(uuid=hy_uuid)
+    @given(address_type_strat())
+    def test_init(self, model_dict):
+        assert AddressType(**model_dict)
 
 
 @st.composite
 def valid_addr_type(draw):
-    uuid = draw(st.uuids())
-    return AddressType(uuid=uuid)
+    model_dict = draw(address_type_strat())
+    return AddressType(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # EngagementAssociationType
 # --------------------------------------------------------------------------------------
+@st.composite
+def eng_assoc_type_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestEngagementAssociationType:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert EngagementAssociationType(uuid=hy_uuid)
+    @given(eng_assoc_type_strat())
+    def test_init(self, model_dict):
+        assert EngagementAssociationType(**model_dict)
 
 
 @st.composite
 def valid_eng_assoc_type(draw):
-    uuid = draw(st.uuids())
-    return EngagementAssociationType(uuid=uuid)
+    model_dict = draw(eng_assoc_type_strat())
+    return EngagementAssociationType(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # EngagementRef
 # --------------------------------------------------------------------------------------
+@st.composite
+def eng_ref_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestEngagementRef:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert EngagementRef(uuid=hy_uuid)
+    @given(eng_ref_strat())
+    def test_init(self, model_dict):
+        assert EngagementRef(**model_dict)
 
 
 @st.composite
 def valid_eng(draw):
-    uuid = draw(st.uuids())
-    return EngagementRef(uuid=uuid)
+    model_dict = draw(eng_ref_strat)
+    return EngagementRef(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
@@ -111,237 +127,309 @@ def valid_eng(draw):
 # --------------------------------------------------------------------------------------
 
 
+@st.composite
+def eng_type_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
+
+
 class TestEngagementType:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert EngagementType(uuid=hy_uuid)
+    @given(eng_type_strat())
+    def test_init(self, model_dict):
+        assert EngagementType(**model_dict)
 
 
 @st.composite
 def valid_eng_type(draw):
-    uuid = draw(st.uuids())
-    return EngagementType(uuid=uuid)
+    model_dict = draw(eng_type_strat())
+    return EngagementType(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # AssociationType
 # --------------------------------------------------------------------------------------
+@st.composite
+def assoc_type_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestAssociationType:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert AssociationType(uuid=hy_uuid)
+    @given(assoc_type_strat())
+    def test_init(self, model_dict):
+        assert AssociationType(**model_dict)
 
 
 @st.composite
 def valid_assoc_type(draw):
-    uuid = draw(st.uuids())
-    return AssociationType(uuid=uuid)
+    model_dict = draw(assoc_type_strat())
+    return AssociationType(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # JobFunction
 # --------------------------------------------------------------------------------------
+@st.composite
+def job_function_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestJobFunction:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert JobFunction(uuid=hy_uuid)
+    @given(job_function_strat())
+    def test_init(self, model_dict):
+        assert JobFunction(**model_dict)
 
 
 @st.composite
 def valid_job_fun(draw):
-    uuid = draw(st.uuids())
-    return JobFunction(uuid=uuid)
+    model_dict = draw(job_function_strat())
+    return JobFunction(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # ManagerLevel
 # --------------------------------------------------------------------------------------
+@st.composite
+def manager_level_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestManagerLevel:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert ManagerLevel(uuid=hy_uuid)
+    @given(manager_level_strat())
+    def test_init(self, model_dict):
+        assert ManagerLevel(**model_dict)
 
 
 @st.composite
 def valid_man_level(draw):
-    uuid = draw(st.uuids())
-    return ManagerLevel(uuid=uuid)
+    model_dict = draw(manager_level_strat())
+    return ManagerLevel(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # ManagerType
 # --------------------------------------------------------------------------------------
+@st.composite
+def manager_type_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestManagerType:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert ManagerType(uuid=hy_uuid)
+    @given(manager_type_strat())
+    def test_init(self, model_dict):
+        assert ManagerType(**model_dict)
 
 
 @st.composite
 def valid_man_type(draw):
-    uuid = draw(st.uuids())
-    return ManagerType(uuid=uuid)
+    model_dict = draw(manager_type_strat())
+    return ManagerType(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # OrganisationRef
 # --------------------------------------------------------------------------------------
+@st.composite
+def org_ref_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestOrganisationRef:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert OrganisationRef(uuid=hy_uuid)
+    @given(org_ref_strat())
+    def test_init(self, model_dict):
+        assert OrganisationRef(**model_dict)
 
 
 @st.composite
 def valid_org_ref(draw):
-    uuid = draw(st.uuids())
-    return OrganisationRef(uuid=uuid)
+    model_dict = draw(org_ref_strat())
+    return OrganisationRef(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # OrgUnitHierarchy
 # --------------------------------------------------------------------------------------
+@st.composite
+def org_unit_hierarchy_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestOrgUnitHierarchy:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert OrgUnitHierarchy(uuid=hy_uuid)
+    @given(org_unit_hierarchy_strat())
+    def test_init(self, model_dict):
+        assert OrgUnitHierarchy(**model_dict)
 
 
 @st.composite
 def valid_org_unit_hier(draw):
-    uuid = draw(st.uuids())
-    return OrganisationRef(uuid=uuid)
+    model_dict = draw(org_unit_hierarchy_strat())
+    return OrganisationRef(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # OrgUnitLevel
 # --------------------------------------------------------------------------------------
+@st.composite
+def org_unit_level_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestOrgUnitLevel:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert OrgUnitLevel(uuid=hy_uuid)
+    @given(org_unit_level_strat())
+    def test_init(self, model_dict):
+        assert OrgUnitLevel(**model_dict)
 
 
 @st.composite
 def valid_org_unit_level(draw):
-    uuid = draw(st.uuids())
-    return OrgUnitLevel(uuid=uuid)
+    model_dict = draw(org_unit_level_strat())
+    return OrgUnitLevel(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # OrgUnitRef
 # --------------------------------------------------------------------------------------
+@st.composite
+def org_unit_ref_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestOrgUnitRef:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert OrgUnitRef(uuid=hy_uuid)
+    @given(org_unit_ref_strat())
+    def test_init(self, model_dict):
+        assert OrgUnitRef(**model_dict)
 
 
 @st.composite
 def valid_org_unit(draw):
-    uuid = draw(st.uuids())
-    return OrgUnitRef(uuid=uuid)
+    model_dict = draw(org_unit_ref_strat())
+    return OrgUnitRef(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # OrgUnitType
 # --------------------------------------------------------------------------------------
+@st.composite
+def org_unit_type_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestOrgUnitType:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert OrgUnitType(uuid=hy_uuid)
+    @given(org_unit_type_strat())
+    def test_init(self, model_dict):
+        assert OrgUnitType(**model_dict)
 
 
 @st.composite
 def valid_org_unit_type(draw):
-    uuid = draw(st.uuids())
-    return OrgUnitType(uuid=uuid)
+    model_dict = draw(org_unit_type_strat())
+    return OrgUnitType(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # ParentRef
 # --------------------------------------------------------------------------------------
+@st.composite
+def parent_ref_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestParentRef:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert ParentRef(uuid=hy_uuid)
+    @given(parent_ref_strat())
+    def test_init(self, model_dict):
+        assert ParentRef(**model_dict)
 
 
 @st.composite
 def valid_parent(draw):
-    uuid = draw(st.uuids())
-    return ParentRef(uuid=uuid)
+    model_dict = draw(parent_ref_strat())
+    return ParentRef(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # PersonRef
 # --------------------------------------------------------------------------------------
+@st.composite
+def person_ref_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestPersonRef:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert PersonRef(uuid=hy_uuid)
+    @given(person_ref_strat())
+    def test_init(self, model_dict):
+        assert PersonRef(**model_dict)
 
 
 @st.composite
 def valid_pers(draw):
-    uuid = draw(st.uuids())
-    return PersonRef(uuid=uuid)
+    model_dict = draw(person_ref_strat())
+    return PersonRef(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # Primary
 # --------------------------------------------------------------------------------------
+@st.composite
+def primary_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestPrimary:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert Primary(uuid=hy_uuid)
+    @given(primary_strat())
+    def test_init(self, model_dict):
+        assert Primary(**model_dict)
 
 
 @st.composite
 def valid_primary(draw):
-    uuid = draw(st.uuids())
-    return Primary(uuid=uuid)
+    model_dict = draw(primary_strat())
+    return Primary(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # Responsibility
 # --------------------------------------------------------------------------------------
+@st.composite
+def responsibility_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestResponsibility:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert Responsibility(uuid=hy_uuid)
+    @given(responsibility_strat())
+    def test_init(self, model_dict):
+        assert Responsibility(**model_dict)
 
 
 @st.composite
 def valid_resp(draw):
-    uuid = draw(st.uuids())
-    return Responsibility(uuid=uuid)
+    model_dict = draw(responsibility_strat())
+    return Responsibility(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
@@ -349,12 +437,24 @@ def valid_resp(draw):
 # --------------------------------------------------------------------------------------
 
 
+@st.composite
+def validity_strat(draw):
+    required = dict()  # type: ignore
+    optional = {"from_date": st.datetimes(), "to_date": st.datetimes() | st.none()}
+    st_dict = draw(st.fixed_dictionaries(required, optional=optional))
+    # from_date must be less than or equal to to_date in all cases
+    from_date, to_date = st_dict.get("from_date"), st_dict.get("to_date")
+    if all([from_date, to_date]):
+        assume(from_date <= to_date)
+    if from_date is None and to_date:
+        assume(to_date >= datetime(1930, 1, 1))
+    return st_dict
+
+
 class TestValidity:
-    @given(st.tuples(st.datetimes(), st.datetimes()))
-    def test_init(self, dt_tup):
-        from_dt, to_dt = dt_tup
-        assume(from_dt <= to_dt)
-        assert Validity(from_date=from_dt, to_date=to_dt)
+    @given(validity_strat())
+    def test_init(self, model_dict):
+        assert Validity(**model_dict)
 
     @given(st.tuples(st.datetimes(), st.datetimes()), st.dates())
     def test_validators(self, dt_tup, from_date_no_tz):
@@ -374,22 +474,27 @@ class TestValidity:
 
 @st.composite
 def valid_validity(draw):
-    from_dt, to_dt = draw(valid_dt_range())
-    return Validity(from_date=from_dt, to_date=to_dt)
+    model_dict = draw(validity_strat())
+    return Validity(**model_dict)
 
 
 # --------------------------------------------------------------------------------------
 # Visibility
 # --------------------------------------------------------------------------------------
+@st.composite
+def visibility_strat(draw):
+    required = {"uuid": st.uuids()}
+    st_dict = draw(st.fixed_dictionaries(required))
+    return st_dict
 
 
 class TestVisibility:
-    @given(st.uuids())
-    def test_init(self, hy_uuid):
-        assert Visibility(uuid=hy_uuid)
+    @given(visibility_strat())
+    def test_init(self, model_dict):
+        assert Visibility(**model_dict)
 
 
 @st.composite
 def valid_vis(draw):
-    uuid = draw(st.uuids())
-    return Visibility(uuid=uuid)
+    model_dict = draw(visibility_strat())
+    return Visibility(**model_dict)
