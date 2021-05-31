7befda540cdb035b6de3182f0afcc63221b35beb
iamfrank <frank@magenta.dk>
2021-05-31 14:22:35 +0200

    Display query data in MO GUI

---
+++ frontend/src/components/Datagrid/DataGrid.vue
@@ -0,0 +1,253 @@
+SPDX-FileCopyrightText: 2018-2021 Magenta ApS
+SPDX-License-Identifier: MPL-2.0
+
+<template>
+
+    <section class="datagrid-container">
+
+        <header class="datagrid-header">
+
+            <div>
+                <slot name="datagrid-header"></slot>
+            </div>
+
+            <form class="datagrid-filter" @submit.prevent>
+                <label :for="`filter-field-${ componentId }`" 
+                    title="Find i liste"
+                    class="form-label">
+                    <span class="sr-only">Find i liste</span>
+                </label>
+                <input type="search"
+                    class="form-control"
+                    name="query"
+                    v-model="filterKey"
+                    :id="`filter-field-${ componentId }`"
+                    placeholder="Find i liste ..."
+                    :disabled="dataList.length < 1 ? true : false">
+            </form>
+
+        </header>
+    
+        <div class="datagrid-table-wrapper">
+            <table class="datagrid table" v-if="filteredData.length > 0">
+                
+                <thead>
+                    <tr>
+                        <th v-for="c in dataFields"
+                            :key="c.name"
+                            @click="sortBy(c.name)"
+                            :class="`datagrid-filter-th ${ c.class ? c.class : '' }${ sortKey === c.name ? ' active' : '' }`">
+                            <span v-if="c.name" class="datagrid-th-arrow" :class="sortOrders[c.name] > 0 ? 'asc' : 'dsc'"></span>
+                            <span class="datagrid-th-title">{{ c.name }}</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="d in filteredData" :key="d.id" :class="`datagrid-r-${ d.id}`">
+                        <template v-for="f in dataFields">
+                            <td
+                                :key="f.name" 
+                                :title="d[f.name]">
+                                {{ d[f.name] }}
+                            </td>
+                        </template>
+                    </tr>
+                    <slot name="datagrid-table-footer"></slot>
+                </tbody>
+            </table>
+        </div>
+
+        <slot name="datagrid-footer"></slot>
+
+        <p v-if="filteredData.length < 1 && filterKey">Kan ikke finde nogen resultater, der matcher de valgte kriterier</p>
+
+    </section>
+
+</template>
+
+<script>
+
+    export default {
+
+        props: {
+            dataList: [Array, Boolean],
+            dataFields: Array
+        },
+        data: function () {
+            let sortOrders = {}
+            for (let f of this.dataFields) {
+                sortOrders[f.name] = -1
+            }
+            return {
+                componentId: null,
+                sortKey: '',
+                sortOrders: sortOrders,
+                filterKey: ''
+            }
+        },
+        computed: {
+            filteredData: function () {
+                var sortKey = this.sortKey
+                var filterKey = this.filterKey && this.filterKey.toLowerCase()
+                var order = this.sortOrders[sortKey] || 1
+                var list = this.dataList
+                if (filterKey) {
+                    list = list.filter((row) => {
+                        let fewer_keys = Object.keys(row)
+                        fewer_keys = fewer_keys.filter((key) => {
+                            for (let field of this.dataFields) {
+                                if (field.name === key) { // Add "&& field.searchable !== false" if you want to exclude some columns in searches
+                                    return key
+                                }
+                            }
+                        })
+                        return fewer_keys.some(function (key) {
+                            return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+                        })
+                    })
+                }
+                if (sortKey) {
+                    list = list.slice().sort(function (a, b) {
+                        a = a[sortKey]
+                        b = b[sortKey]
+                        return (a === b ? 0 : a > b ? 1 : -1) * order
+                    })
+                }
+                return list
+            }
+        },
+        methods: {
+            sortBy: function(key) {
+                console.log('sorting', key, 'sortkey', this.sortKey, this.sortOrders)
+                if (key) {
+                    this.sortKey = key
+                    this.sortOrders[key] = this.sortOrders[key] * -1
+                }
+            }
+        },
+        mounted: function() {
+            this.componentId = this._uid
+        }
+    }
+    
+</script>
+
+<style>
+
+    .datagrid-container {
+        margin: 0 0 1rem;
+    }
+
+    .datagrid-table-wrapper {
+        overflow: auto;
+    }
+
+    .datagrid {
+        margin-top: 0;
+    }
+
+    .datagrid th {
+        background-color: var(--light);
+        color: var(--gray);
+        cursor: pointer;
+        user-select: none;
+        vertical-align: middle;
+        font-weight: normal;
+        font-size: .85rem;
+        padding-left: 0;
+    }
+
+    .datagrid-filter-th {
+        padding-left: 0;
+        white-space: nowrap;
+    }
+
+    .datagrid th.active {
+        color: var(--dark);
+    }
+
+    .datagrid th.active .datagrid-th-arrow {
+        opacity: 1; 
+    }
+
+    .datagrid .datagrid-th-arrow {
+        display: inline-block;
+        vertical-align: middle;
+        width: 0;
+        height: 0;
+        margin: 0 .25rem 0 .5rem;
+        opacity: 0.5;
+    }
+
+    .datagrid .datagrid-th-arrow.asc {
+        border-left: 4px solid transparent;
+        border-right: 4px solid transparent;
+        border-top: 4px solid var(--dark);
+    }
+
+    .datagrid .datagrid-th-arrow.dsc {
+        border-left: 4px solid transparent;
+        border-right: 4px solid transparent;
+        border-bottom: 4px solid var(--dark);
+    }
+
+    .datagrid td {
+        vertical-align: middle;
+        padding: .5rem;
+    }
+
+    .datagrid td.datagrid-action {
+        padding: 0;
+    }
+
+    .datagrid td.datagrid-action > a:link,
+    .datagrid td.datagrid-action > a:visited,
+    .datagrid button.datagrid-action-btn {
+        display: block;
+        border: none;
+        padding: .75rem;
+        background-color: transparent;
+        text-align: left;
+        transition: transform .33s;
+        box-shadow: none;
+        font-size: 1rem;
+        height: auto;
+        margin: 0;
+    }
+
+    .datagrid td.datagrid-action > a:hover,
+    .datagrid td.datagrid-action > a:active,
+    .datagrid td.datagrid-action > a:focus,
+    .datagrid button.datagrid-action-btn:hover,
+    .datagrid button.datagrid-action-btn:active,
+    .datagrid button.datagrid-action-btn:focus {
+        transform: translate(.5rem,0);
+        color: var(--primary);
+    }
+
+    .datagrid-header {
+        display: grid;
+        grid-template-columns: auto auto;
+        margin: 1rem 0;
+    }
+
+    .datagrid-filter {
+        background-color: transparent;
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        justify-content: flex-end;
+        padding: .5rem 0;
+        margin: 0;
+    }
+
+    .datagrid-filter label {
+        margin: 0 .5rem 0 0;
+    }
+
+    .datagrid-filter input {
+        width: 8.5rem;
+        padding: .125rem .5rem;
+    }
+
+</style>
--- frontend/src/modules/query/index.vue
+++ frontend/src/modules/query/index.vue
@@ -3,41 +3,106 @@ SPDX-License-Identifier: MPL-2.0
 <template>
   <div class="card col">
     <div class="card-body d-flex flex-column">
-      <h4 class="card-title">
+      <h4 class="card-title" style="margin-bottom: 2rem;">
         <icon name="exchange-alt"/>
         {{$tc('shared.query', 2)}}
       </h4>
-      <span v-for="(q, index) in queries" :key="index">
-        <icon name="download"/>
-      <a :href="downloadLink(q)">{{q}}</a>
-      </span>
+
+      <b-tabs content-class="mt-3">
+        <b-tab 
+          v-for="(q, index) in query_data"
+          :key="index"
+          :title="q.title">
+          <data-grid :data-list="q.data" :data-fields="q.schema.fields" style="margin: 2rem 0;">
+            <h5 style="margin: 1rem 0;" slot="datagrid-header">{{q.title}}</h5>
+          </data-grid>
+        </b-tab>
+      </b-tabs>
+
     </div>
   </div>
 </template>
 
 <script>
+import Service from '@/api/HttpCommon'
 import store from './_store'
 import { mapGetters } from 'vuex'
+import DataGrid from '../../components/Datagrid/DataGrid'
+import bTabs from 'bootstrap-vue/es/components/tabs/tabs'
+import bTab from 'bootstrap-vue/es/components/tabs/tab'
 
 const STORE_KEY = '$_queryList'
 
 export default {
+  components: {
+    DataGrid,
+    bTabs,
+    bTab
+  },
+  data: function() {
+    return {
+      query_data: []
+    }
+  },
   computed: {
     ...mapGetters(STORE_KEY, {
       queries: 'getQueries'
     })
   },
   created () {
+    // This might be unnecessary in the future
     if (!(STORE_KEY in this.$store._modules.root._children)) {
       this.$store.registerModule(STORE_KEY, store)
     }
     this.$store.dispatch(`${STORE_KEY}/getQueries`)
+
+    // Get all the query data from dummy JSON files
+    this.update()
+
   },
   methods: {
     downloadLink(file) {
       return "/service/exports/" + file
-    }
+    },
 
+    update: function() {
+      Service.get('../dummy-data/all_leader_functions.json')
+      .then(response => {
+        let data = response.data
+        data.title='Alle Lederfunktioner'
+        this.query_data.push(data)
+      })
+      Service.get('../dummy-data/all_engagements.json')
+      .then(response => {
+        let data = response.data
+        data.title = 'Alle Stillinger'
+        this.query_data.push(data)
+      })
+      Service.get('../dummy-data/orgs_and_engagements.json')
+      .then(response => {
+        let data = response.data
+        data.title = 'Organisationsstruktur og Stillinger'
+        this.query_data.push(data)
+      })
+      Service.get('../dummy-data/orgunits.json')
+      .then(response => {
+        let data = response.data
+        data.title = 'Organisationsenheder'
+        this.query_data.push(data)
+      })
+      Service.get('../dummy-data/sd_and_p_no.json')
+      .then(response => {
+        let data = response.data
+        data.title = 'SDLønorganisation og P-Nummer'
+        this.query_data.push(data)
+      })
+    }
   }
 }
 </script>
+
+<style scoped>
+  .query-item {
+    margin-bottom: 1rem;
+  }
+</style>
