f6d06206a71b4e88819ad5c1b8cc95e9b132a695
Andreas Brodersen <apb@magenta-aps.dk>
2021-07-23 13:41:49 +0200

    [#44596] Fix Duplicated CollectorRegistry error in tests

--- backend/mora/metrics.py
+++ backend/mora/metrics.py
@@ -1,15 +1,18 @@
 # SPDX-FileCopyrightText: 2017-2021 Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
 from typing import Callable
-
 from prometheus_client import Info
 from prometheus_fastapi_instrumentator import Instrumentator
 from prometheus_fastapi_instrumentator.metrics import Info as InstInfo
 
 from . import __version__
+from .config import Environment, get_settings
 
 
 def setup_metrics(app):
+    if get_settings().environment is Environment.TESTING:
+        return
+
     instrumentator = Instrumentator()
     instrumentator.add(os2mo_version())
     instrumentator.instrument(app).expose(app)
