21e5dd68cfede2fb644deafc9c2e27f8f56774bd
Andreas Kring <andreas@magenta.dk>
2021-09-02 15:45:35 +0200

    [#45263] Only perform token UUID check when RBAC is enabled

--- backend/mora/auth/keycloak/models.py
+++ backend/mora/auth/keycloak/models.py
@@ -31,11 +31,14 @@ class KeycloakToken(BaseModel):
     def uuid_attribute_required_for_mo_client(
         cls, values: Dict[str, Any]
     ) -> Dict[str, Any]:
-        if values.get("azp") == settings.keycloak_mo_client:
-            if values.get("uuid") is None:
-                raise ValueError(
-                    'The uuid user attribute is missing in the token'
-                )
+        if (
+            settings.keycloak_rbac_enabled and
+            values.get("azp") == settings.keycloak_mo_client and
+            values.get("uuid") is None
+        ):
+            raise ValueError(
+                'The uuid user attribute is missing in the token'
+            )
         return values
 
     class Config:
