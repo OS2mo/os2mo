64f68d79bc0cc93f84b49d6ec32657954eef8e1a
Mads Riis-Weitling <mrw@magenta.dk>
2021-09-09 16:33:46 +0200

    [#45492] Fix `i18n.spec.js`, so differing keys are detected both ways between English and Danish locales
    Previously, the difference was only detected "one way" - if keys were
    present in Danish, but missing in English, it was not detected. But the
    other way around worked.
    
    [#45492] Use MDN definition of `symmetricDifference` instead

--- frontend/tests/unit/i18n.spec.js
+++ frontend/tests/unit/i18n.spec.js
@@ -4,11 +4,34 @@
 import i18n from '@/i18n.js'
 import { getDatepickerLocales } from '@/i18n.js'
 
+function symmetricDifference (setA, setB) {
+  // Source:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set#implementing_basic_set_operations
+  let _difference = new Set(setA)
+  for (let elem of setB) {
+      if (_difference.has(elem)) {
+          _difference.delete(elem)
+      } else {
+          _difference.add(elem)
+      }
+  }
+  return _difference
+}
+
+function expectNoDifference(a1, a2) {
+  const emptySet = new Set()
+  const diff = symmetricDifference(a1, a2)
+  expect(diff).toEqual(emptySet)
+}
+
 describe('i18n.js:loadLocaleMessages', () => {
   it('should detect "en" and "da" locales', async () => {
     const messages = i18n.messages
     expect(Object.keys(messages)).toEqual(expect.arrayContaining(['da', 'en']))
-    expect(Object.keys(messages['da'])).toEqual(Object.keys(messages['en']))
+
+    const daKeys = Object.keys(messages['da'])
+    const enKeys = Object.keys(messages['en'])
+    expectNoDifference(daKeys, enKeys)
   })
 
   it('should detect the same i18n keys for all locales', async () => {
@@ -32,7 +55,7 @@ describe('i18n.js:loadLocaleMessages', () => {
 
     const daKeys = listAllKeys(i18n.messages['da'])
     const enKeys = listAllKeys(i18n.messages['en'])
-    expect(daKeys).toEqual(expect.arrayContaining(enKeys))
+    expectNoDifference(daKeys, enKeys)
   })
 })
 
@@ -40,6 +63,9 @@ describe('i18n.js:getDatepickerLocales', () => {
   it('should load "en" and "da" locales', async () => {
     const locales = getDatepickerLocales()
     expect(Object.keys(locales)).toEqual(expect.arrayContaining(['da', 'en']))
-    expect(Object.keys(locales['da'])).toEqual(Object.keys(locales['en']))
+
+    const daKeys = Object.keys(locales['da'])
+    const enKeys = Object.keys(locales['en'])
+    expectNoDifference(daKeys, enKeys)
   })
 })
