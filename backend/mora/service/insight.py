a023d6090688fb6043a4587e89ee4ff29e202a3f
Andreas Brodersen <apb@magenta-aps.dk>
2021-09-21 13:50:29 +0200

    Add support for exporter + refactor

--- backend/mora/service/insight.py
+++ backend/mora/service/insight.py
@@ -3,9 +3,12 @@
 
 import json
 import os
+
 from typing import List, Union, Optional
 from pydantic import BaseModel, Extra
 from fastapi import APIRouter, Query
+
+from .. import exceptions, config
 
 router = APIRouter()
 
@@ -26,38 +29,36 @@ class Insight(BaseModel):
         extra = Extra.forbid
 
 
-@router.get('/insight')
-async def get_insight_data(q: Optional[List[str]] = Query(['all'])
-                           ) -> List[Insight]:
+@router.get("/insight")
+async def get_insight_data(q: Optional[List[str]] = Query(["all"])) -> List[Insight]:
     """Loads data from a directory of JSONs and returns it as a list
 
     **param q:** Enables the frontend to choose a specific file or show all files
     """
-    directory = 'backend/mora/service/dummy-data'
+    directory = config.get_settings().query_export_dir
+    if not os.path.isdir(directory):
+        exceptions.ErrorCodes.E_DIR_NOT_FOUND()
 
-    if q == ['all']:
+    if q == ["all"]:
         return [
-            json.load(
-                open(os.path.join(directory, filename))
-            )
+            json.load(open(os.path.join(directory, filename)))
             for filename in os.listdir(directory)
             if os.path.isfile(os.path.join(directory, filename))
         ]
     else:
         return [
-            json.load(
-                open(os.path.join(directory, filename))
-            )
+            json.load(open(os.path.join(directory, filename)))
             for filename in q
             if os.path.isfile(os.path.join(directory, filename))
         ]
 
 
-@router.get('/insight/files')
+@router.get("/insight/files")
 async def get_insight_filenames() -> List[str]:
-    """Lists all available files
-    """
-    directory = 'backend/mora/service/dummy-data'
+    """Lists all available files"""
+    directory = config.get_settings().query_export_dir
+    if not os.path.isdir(directory):
+        exceptions.ErrorCodes.E_DIR_NOT_FOUND()
 
     return [
         filename
