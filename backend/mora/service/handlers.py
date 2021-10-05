340a10ba8fb1778781e01400e791a5edaf15017e
Andreas Brodersen <apb@magenta-aps.dk>
2021-10-05 13:57:24 +0200

    [#45935] Add aprepare_edit to kle.py

--- backend/mora/service/handlers.py
+++ backend/mora/service/handlers.py
@@ -397,7 +397,7 @@ def generate_requests(
                     requesthandler_klasse.construct)(req, request_type)
             )
         elif req.get('type') in [
-            'role'
+            'role', 'kle',
         ] and request_type == RequestType.EDIT:
             requesthandlers.append(
                 async_to_sync(
