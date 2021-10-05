e8d92a0d8b8126eb973f3c269dd4b3183fbef376
Frederik Vincent Primdahl <fv@magenta-aps.dk>
2021-10-05 14:31:08 +0200

    [#45939] Add aprepare_edit to engagement.py

--- backend/mora/service/engagement.py
+++ backend/mora/service/engagement.py
@@ -309,6 +309,123 @@ class EngagementRequestHandler(handlers.OrgFunkRequestHandler):
             {'uuid': employee_uuid},
             new_from, new_to)
 
+    async def aprepare_edit(self, req: dict):
+        engagement_uuid = util.get_uuid(req)
+
+        # Get the current org-funktion which the user wants to change
+        c = lora.Connector(virkningfra='-infinity', virkningtil='infinity')
+        original = await c.organisationfunktion.get(
+            uuid=engagement_uuid)
+
+        # Get org unit uuid for validation purposes
+        org_unit_uuid = mapping.ASSOCIATED_ORG_UNIT_FIELD.get_uuid(original)
+
+        # Get employee uuid for validation purposes
+        employee_uuid = mapping.USER_FIELD.get_uuid(original)
+
+        data = util.checked_get(req, 'data', {}, required=True)
+        new_from, new_to = util.get_validities(data)
+
+        try:
+            exts = mapping.ORG_FUNK_UDVIDELSER_FIELD(original)[-1].copy()
+        except (TypeError, LookupError):
+            exts = {}
+
+        payload = dict()
+        payload['note'] = 'Rediger engagement'
+
+        original_data = req.get('original')
+        if original_data:
+            # We are performing an update
+            old_from, old_to = util.get_validities(original_data)
+            payload = common.inactivate_old_interval(
+                old_from, old_to, new_from, new_to, payload,
+                ('tilstande', 'organisationfunktiongyldighed')
+            )
+
+        update_fields = list()
+
+        # Always update gyldighed
+        update_fields.append((
+            mapping.ORG_FUNK_GYLDIGHED_FIELD,
+            {'gyldighed': "Aktiv"}
+        ))
+
+        try:
+            attributes = mapping.ORG_FUNK_EGENSKABER_FIELD(original)[-1].copy()
+        except (TypeError, LookupError):
+            attributes = {}
+        new_attributes = {}
+
+        if mapping.USER_KEY in data:
+            new_attributes['brugervendtnoegle'] = util.checked_get(
+                data, mapping.USER_KEY, "")
+
+        if new_attributes:
+            update_fields.append((
+                mapping.ORG_FUNK_EGENSKABER_FIELD,
+                {
+                    **attributes,
+                    **new_attributes
+                },
+            ))
+
+        if mapping.JOB_FUNCTION in data:
+            update_fields.append((
+                mapping.JOB_FUNCTION_FIELD,
+                {'uuid': data.get(mapping.JOB_FUNCTION).get('uuid')}
+            ))
+
+        if mapping.ENGAGEMENT_TYPE in data:
+            update_fields.append((
+                mapping.ORG_FUNK_TYPE_FIELD,
+                {'uuid': data.get(mapping.ENGAGEMENT_TYPE).get('uuid')},
+            ))
+
+        if mapping.ORG_UNIT in data:
+            org_unit_uuid = util.get_mapping_uuid(data, mapping.ORG_UNIT,
+                                                  required=True)
+            update_fields.append((mapping.ASSOCIATED_ORG_UNIT_FIELD,
+                                  {'uuid': org_unit_uuid}))
+
+        if mapping.PRIMARY in data and data.get(mapping.PRIMARY):
+            primary = util.get_mapping_uuid(data, mapping.PRIMARY)
+
+            update_fields.append((mapping.PRIMARY_FIELD, {'uuid': primary}))
+
+        # Attribute extensions
+        new_extensions = {}
+
+        if mapping.FRACTION in data:
+            fraction = util.checked_get(data, mapping.FRACTION, default=100)
+
+            new_extensions['fraktion'] = fraction
+
+        fields = self.get_extension_attribute_fields(data)
+        new_extensions.update(fields)
+
+        if new_extensions:
+            update_fields.append((
+                mapping.ORG_FUNK_UDVIDELSER_FIELD,
+                {
+                    **exts,
+                    **new_extensions
+                },
+            ))
+
+        payload = common.update_payload(new_from, new_to, update_fields,
+                                        original, payload)
+
+        bounds_fields = list(
+            mapping.ENGAGEMENT_FIELDS.difference(
+                {x[0] for x in update_fields}))
+        payload = common.ensure_bounds(new_from, new_to, bounds_fields,
+                                       original, payload)
+
+        await validator.is_date_range_in_employee_range(
+            {'uuid': employee_uuid},
+            new_from, new_to)
+
         def to_edit_request(address_obj):
             addr_uuid = address_obj.get(mapping.UUID)
             addr_handler = AddressRequestHandler(
