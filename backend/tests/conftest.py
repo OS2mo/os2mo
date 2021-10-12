32103402d07c17fcfebd9ae0ae0a474b8c1e6aae
Andreas Brodersen <apb@magenta-aps.dk>
2021-10-12 15:53:01 +0200

    [#46018] Remove hypothesis deadline from CI

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,9 +4,16 @@
 import os
 
 from hypothesis import strategies as st
+from hypothesis import Verbosity, settings as h_settings
 
 from mora.api.v1.models import Validity
 from tests.hypothesis_utils import validity_model_strat
+
+
+h_settings.register_profile("ci", max_examples=100, deadline=None)
+h_settings.register_profile("dev", max_examples=10)
+h_settings.register_profile("debug", max_examples=10, verbosity=Verbosity.verbose)
+h_settings.load_profile(os.getenv(u"HYPOTHESIS_PROFILE", "default"))
 
 
 def pytest_runtest_setup(item):
