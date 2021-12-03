be1d4daea8da6babf1f0b5426e08d4ac513db5ef
Paw Møller <pawsen@gmail.com>
2021-12-03 11:50:02 +0100

    [#47415] Add OrganisationRead to MO data models

--- tests/mo/test_organisation.py
+++ tests/mo/test_organisation.py
@@ -6,28 +6,31 @@
 # --------------------------------------------------------------------------------------
 # Imports
 # --------------------------------------------------------------------------------------
-from ._shared import MOBase
-from .employee import Employee
-from .employee import EmployeeRead
-from .employee import EmployeeWrite
-from .facet import FacetClass
-from .organisation import OrganisationRead
-from .organisation_unit import OrganisationUnit
-from .organisation_unit import OrganisationUnitRead
-from .organisation_unit import OrganisationUnitWrite
+from hypothesis import given
+from hypothesis import strategies as st
+
+from ramodels.mo.organisation import OrganisationRead
 
 # --------------------------------------------------------------------------------------
-# All
+# Tests
 # --------------------------------------------------------------------------------------
 
-__all__ = [
-    "MOBase",
-    "Employee",
-    "EmployeeRead",
-    "EmployeeWrite",
-    "FacetClass",
-    "OrganisationRead",
-    "OrganisationUnit",
-    "OrganisationUnitRead",
-    "OrganisationUnitWrite",
-]
+
+@st.composite
+def read_strat(draw):
+    required = {
+        "name": st.text(),
+    }
+    optional = {
+        "type": st.just("organisation"),
+        "user_key": st.none() | st.text(),
+    }
+
+    st_dict = draw(st.fixed_dictionaries(required, optional=optional))  # type: ignore
+    return st_dict
+
+
+class TestOrganisationUnit:
+    @given(read_strat())
+    def test_read(self, model_dict):
+        assert OrganisationRead(**model_dict)
