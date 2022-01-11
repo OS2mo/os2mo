4612e1f4f9a21fc2ae29071c8c878d430e042bf1
Paw Møller <pawsen@gmail.com>
2022-01-11 01:14:15 +0100

    [#47771] Test of GraphQL related unit reading

---
+++ backend/tests/graphapi/test_related_units.py
@@ -0,0 +1,96 @@
+# SPDX-FileCopyrightText: 2021- Magenta ApS
+# SPDX-License-Identifier: MPL-2.0
+# --------------------------------------------------------------------------------------
+# Imports
+# --------------------------------------------------------------------------------------
+from fastapi.encoders import jsonable_encoder
+from hypothesis import given
+from hypothesis import strategies as st
+from pytest import MonkeyPatch
+from ramodels.mo.details import RelatedUnitRead
+
+import mora.graphapi.dataloaders as dataloaders
+from mora.graphapi.main import get_schema
+
+# --------------------------------------------------------------------------------------
+# Tests
+# --------------------------------------------------------------------------------------
+
+SCHEMA = str(get_schema())
+
+
+class TestRelated_UnitsQuery:
+    """Class collecting related_units query tests.
+
+    Data loaders are mocked to return specific values, generated via
+    Hypothesis.
+    MonkeyPatch.context is used as a context manager to achieve this,
+    because mocks are *not* reset between invocations of Hypothesis examples.
+    """
+
+    @given(test_data=st.lists(st.builds(RelatedUnitRead)))
+    def test_query_all(self, test_data, graphapi_test, patch_loader):
+        """Test that we can query all attributes of the related_unit data model."""
+
+        # JSON encode test data
+        test_data = jsonable_encoder(test_data)
+
+        # Patch dataloader
+        with MonkeyPatch.context() as patch:
+            patch.setattr(dataloaders, "search_role_type", patch_loader(test_data))
+            query = """
+                query {
+                    related_units {
+                        org_unit_uuid
+                        type
+                        validity {from to}
+                    }
+                }
+            """
+            response = graphapi_test.post("/graphql", json={"query": query})
+
+        data, errors = response.json().get("data"), response.json().get("errors")
+        assert errors is None
+        assert data is not None
+        assert data["related_units"] == [
+            {
+                "org_unit_uuid": related_unit["org_unit_uuid"],
+                "type": related_unit["type"],
+                "validity": related_unit["validity"],
+            }
+            for related_unit in test_data
+        ]
+
+    @given(test_data=st.lists(st.builds(RelatedUnitRead)), st_data=st.data())
+    def test_query_by_uuid(self, test_data, st_data, graphapi_test, patch_loader):
+        """Test that we can query related_units by UUID."""
+
+        # Sample UUIDs
+        uuids = [str(model.uuid) for model in test_data]
+        test_uuids = st_data.draw(st.lists(st.sampled_from(uuids))) if uuids else []
+
+        # JSON encode test data
+        test_data = jsonable_encoder(test_data)
+
+        # Patch dataloader
+        with MonkeyPatch.context() as patch:
+            patch.setattr(dataloaders, "get_role_type_by_uuid", patch_loader(test_data))
+            query = """
+                    query TestQuery($uuids: [UUID!]) {
+                        related_units(uuids: $uuids) {
+                            uuid
+                        }
+                    }
+                """
+            response = graphapi_test.post(
+                "/graphql", json={"query": query, "variables": {"uuids": test_uuids}}
+            )
+
+        data, errors = response.json().get("data"), response.json().get("errors")
+        assert errors is None
+        assert data is not None
+
+        # Check UUID equivalence
+        result_uuids = [assoc.get("uuid") for assoc in data["related_units"]]
+        assert set(result_uuids) == set(test_uuids)
+        assert len(result_uuids) == len(set(test_uuids))
