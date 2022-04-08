924250642fa3f2d5e68b739b5ac6d0331c7c03e6
Mads Riis-Weitling <mrw@magenta.dk>
2022-04-08 18:59:06 +0200

    Add migration to remove unused indexes
    This removes the unused indexes on:
    `rel_maal_urn_isolated`
    `rel_maal_uuid_isolated`
    `virkning_aktoertypekode`
    `virkning_aktoerref`
    `virkning_notetekst`
    across most of the LoRa database tables (the indexes appear to be in use
    for a small subset of the LoRa tables.)

---
+++ oio_rest/alembic/versions/05765df45c68_drop_unused_indexes.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2022 Magenta ApS
+# SPDX-License-Identifier: MPL-2.0
+
+"""drop unused indexes
+
+Revision ID: 05765df45c68
+Revises: b27228471604
+Create Date: 2022-04-08 17:57:45.439305
+"""
+from oio_rest.db.alembic_migration_helpers import apply_sql_from_file
+
+
+# revision identifiers, used by Alembic.
+revision = "05765df45c68"
+down_revision = "b27228471604"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    apply_sql_from_file("05765df45c68_drop_unused_indexes__upgrade.sql")
+
+
+def downgrade():
+    apply_sql_from_file("05765df45c68_drop_unused_indexes__downgrade.sql")
