5bc7344dd5acdf56876a49ab82e4d42ae85425c4
Casper V. Kristensen <vk@magenta.dk>
2022-04-29 18:17:18 +0200

    Revert "[#48632] Remove performance test skeleton for service API"
    This reverts commit 0b4c0865cb50a26c7d20d8bf8f23908acc93c399.

---
+++ tempotest/tests/api.service.js
@@ -0,0 +1,123 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Magenta ApS
+ * SPDX-License-Identifier: MPL-2.0
+ */
+import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
+import { group } from 'k6';
+import http from 'k6/http';
+import { asJSON, client } from '../util.js';
+
+function engagement() {
+  group('engagement', () => {
+    const uuid = uuidv4();
+    client.post(
+      http.url`/service/details/create`,
+      ...asJSON({
+        uuid,
+        todo,
+      }, {
+        tags: {
+          name: '',
+        },
+      }),
+    );
+  });
+}
+
+function association() {
+  group('association', () => {
+    const uuid = uuidv4();
+    client.post(
+      http.url`/service/details/create`,
+      ...asJSON({
+        uuid,
+        todo,
+      }),
+    );
+  });
+}
+
+function itSystem() {
+  group('itSystem', () => {
+    const uuid = uuidv4();
+    client.post(
+      http.url`/service/details/create`,
+      ...asJSON({
+        uuid,
+        todo,
+      }),
+    );
+  });
+}
+
+function role() {
+  group('role', () => {
+    const uuid = uuidv4();
+    client.post(
+      http.url`/service/details/create`,
+      ...asJSON({
+        uuid,
+        todo,
+      }),
+    );
+  });
+}
+
+function manager() {
+  group('manager', () => {
+    const uuid = uuidv4();
+    client.post(
+      http.url`/service/details/create`,
+      ...asJSON({
+        uuid,
+        todo,
+      }),
+    );
+  });
+}
+
+function leave() {
+  group('leave', () => {
+    const uuid = uuidv4();
+    client.post(
+      http.url`/service/details/create`,
+      ...asJSON({
+        uuid,
+        todo,
+      }),
+    );
+  });
+}
+
+function address() {
+  group('address', () => {
+    const uuid = uuidv4();
+    client.post(
+      http.url`/service/details/create`,
+      ...asJSON({
+        uuid,
+        todo,
+      }),
+    );
+  });
+}
+
+function employee() {
+  group('employee', () => {
+    const uuid = uuidv4();
+    client.post(
+      http.url`/service/e/create`,
+      ...asJSON({
+        uuid,
+        givenname: 'Alice',
+      }),
+    );
+    client.get(http.url`/service/e/${uuid}/`);
+  });
+}
+
+export default function apiServiceTests() {
+  group('api.service', () => {
+    employee();
+  });
+}
