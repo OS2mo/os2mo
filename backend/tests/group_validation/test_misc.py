37d73cd8f17604510a6c6d6571ac550f25dc0c71
Mads Riis-Weitling <mrw@magenta.dk>
2022-06-13 10:12:52 +0200

    [#49594] Test coverage for "primary class helper functions"

--- backend/tests/group_validation/test_misc.py
+++ backend/tests/group_validation/test_misc.py
@@ -1,18 +1,36 @@
 # SPDX-FileCopyrightText: 2022 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 from unittest import mock
+from uuid import uuid4
 
 import pytest
 from parameterized import parameterized
 
 from mora import mapping
+from mora.service.facet import get_mo_object_primary_value
+from mora.service.facet import is_class_primary
 from mora.service.facet import is_class_uuid_primary
 
 
 class TestPrimaryClassHelpers:
     @parameterized.expand(
         [
+            # 1. MO class is primary
+            ({mapping.USER_KEY: mapping.PRIMARY}, True),
+            # 2. MO class is not primary
+            ({mapping.USER_KEY: "non-primary"}, False),
+            # 3. MO class is empty
+            ({}, False),
+        ]
+    )
+    def test_is_class_primary(self, mo_class: dict, expected_result: bool):
+        assert is_class_primary(mo_class) == expected_result
+
+    @parameterized.expand(
+        [
+            # MO class is primary
             (mapping.PRIMARY, True),
+            # MO class is not primary
             ("not-primary", False),
         ]
     )
@@ -26,3 +44,21 @@ class TestPrimaryClassHelpers:
         with mock.patch("mora.service.facet.get_one_class", mock_get):
             actual_result = await is_class_uuid_primary("primary-class-uuid")
             assert actual_result == expected_result
+
+    @parameterized.expand(
+        [
+            # 1. MO object contains a `primary` dict with a `user_key` "primary"
+            ({mapping.PRIMARY: {mapping.USER_KEY: mapping.PRIMARY}}, True),
+            # 2. MO object contains a `primary` dict with a `user_key` "non-primary"
+            ({mapping.PRIMARY: {mapping.USER_KEY: "non-primary"}}, False),
+            # 3. MO object contains a `primary` dict with a class UUID
+            ({mapping.PRIMARY: {mapping.UUID: str(uuid4())}}, False),
+            # 4. MO object contains an empty `primary` dict
+            ({mapping.PRIMARY: {}}, False),
+        ]
+    )
+    @pytest.mark.asyncio
+    async def test_get_mo_object_primary_value(
+        self, mo_object: dict, expected_result: bool
+    ):
+        assert (await get_mo_object_primary_value(mo_object)) == expected_result
