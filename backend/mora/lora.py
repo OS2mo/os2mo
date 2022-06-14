81313a1cc30ba0fdb1fa0331bc01b05f9c85dc5f
Mads Riis-Weitling <mrw@magenta-aps.dk>
2022-06-14 14:05:35 +0200

    [#50878] Use `httpx` client in `AutocompleteScope.fetch`

--- backend/mora/lora.py
+++ backend/mora/lora.py
@@ -746,7 +746,6 @@ class AutocompleteScope(BaseScope):
         params = {"phrase": phrase}
         if class_uuids:
             params["class_uuids"] = [str(uuid) for uuid in class_uuids]
-        async with ClientSession() as session:
-            response = await session.get(self.base_path, params=params)
-            await _check_response(response)
-            return {"items": (await response.json())["results"]}
+        response = await clients.lora.get(url=self.path, params=params)
+        await _check_response(response)
+        return {"items": response.json()["results"]}
---
+++ backend/tests/test_autocomplete.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2022 Magenta ApS
+# SPDX-License-Identifier: MPL-2.0
+import pytest
+from parameterized import parameterized
+
+from .cases import AsyncLoRATestCase
+from mora.lora import AutocompleteScope
+from mora.lora import Connector
+
+
+@pytest.mark.asyncio
+class TestAutocompleteScope(AsyncLoRATestCase):
+    @parameterized.expand(
+        [
+            ("bruger", []),
+            ("organisationsenhed", []),
+        ]
+    )
+    async def test_autocomplete(self, path: str, expected_result: list):
+        connector = Connector()
+        scope = AutocompleteScope(connector, path)
+        response = await scope.fetch("phrase")
+        assert "items" in response
+        result = response["items"]
+        assert result == expected_result
