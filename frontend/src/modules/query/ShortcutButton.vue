6c943ab346ec3a150bcdb9e757094d26cea2e061
Oliver Magenta <oe@magenta-aps.dk>
2022-07-04 14:25:27 +0200

    Fixed styling and translastion for Query button

--- frontend/src/modules/query/ShortcutButton.vue
+++ frontend/src/modules/query/ShortcutButton.vue
@@ -2,7 +2,7 @@ SPDX-FileCopyrightText: 2019-2020 Magenta ApS
 SPDX-License-Identifier: MPL-2.0
 <template>
    <router-link :to="{ name: 'QueryList'}">
-      <button type="button" aria-label="Foresørgsler" class="btn btn-link text-white">
+      <button type="button" :title="$tc('shared.query',2)" :aria-label="$tc('shared.query',2)" class="btn btn-primary">
         <icon name="exchange-alt"/>
       </button>
   </router-link>
