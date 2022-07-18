debbc529057905ea45c8987e712df52ef4203990
Casper V. Kristensen <vk@magenta.dk>
2022-07-18 17:28:37 +0200

    [#51177] Revert "[Remove FacetClass] (21a86484)"

--- tests/mo/test_facet.py
+++ tests/mo/test_facet.py
@@ -9,6 +9,7 @@
 from hypothesis import given
 from hypothesis import strategies as st
 
+from ramodels.mo.facet import FacetClass
 from ramodels.mo.facet import FacetRead
 
 
@@ -34,7 +35,27 @@ def read_strat(draw):
     return st_dict
 
 
+@st.composite
+def facet_class_strat(draw):
+    required = {
+        "facet_uuid": st.uuids(),
+        "name": st.text(),
+        "user_key": st.text(),
+        "org_uuid": st.uuids(),
+    }
+    optional = {"scope": st.none() | st.text()}
+
+    st_dict = draw(st.fixed_dictionaries(required, optional=optional))  # type: ignore
+    return st_dict
+
+
 class TestFacetRead:
     @given(read_strat())
     def test_read(self, model_dict):
         assert FacetRead(**model_dict)
+
+
+class TestFacetClass:
+    @given(facet_class_strat())
+    def test_init(self, model_dict):
+        assert FacetClass(**model_dict)
