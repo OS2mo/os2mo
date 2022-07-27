08d866b86b7e7fba7272b52d3556373e5d13520e
Thor Astrup Pedersen <tap@magenta-aps.dk>
2022-07-27 09:12:39 +0200

    Removed old grapql-mutator after moving it to mora.grapql.mutators

--- backend/mora/graphapi/main.py
+++ backend/mora/graphapi/main.py
@@ -436,24 +436,6 @@ class Query:
         return cast(list[Configuration], parsed_settings)
 
 
-@strawberry.type
-class MutationOld:
-    @strawberry.mutation(description="Upload a file")
-    async def upload_file(
-        self, file_store: FileStore, file: Upload, force: bool = False
-    ) -> str:
-        file_name = file.filename
-        file_bytes = await file.read()
-        save_file(file_store, file_name, file_bytes, force)
-        return "OK"
-
-    @strawberry.mutation(description="Trigger refresh for an organisation unit")
-    async def org_unit_refresh(self, uuid: UUID) -> OrganisationUnitRefresh:
-        result = await trigger_org_unit_refresh(uuid)
-        organisation_unit_refresh = OrganisationUnitRefreshRead(**result)
-        return OrganisationUnitRefresh.from_pydantic(organisation_unit_refresh)
-
-
 # --------------------------------------------------------------------------------------
 # Auxiliary functions
 # --------------------------------------------------------------------------------------
