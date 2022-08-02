354701422d5a197cbd4e9d32cc1439549f9fa919
Thor Astrup Pedersen <tap@magenta-aps.dk>
2022-08-02 16:01:28 +0200

    Created pydantic data models for prev. trigger_dict, now called MoraTrigger.. and a child model called MoraTriggerRequest, which uses a new more general model called Validity.

--- backend/mora/graphapi/models.py
+++ backend/mora/graphapi/models.py
@@ -7,6 +7,7 @@
 # Imports
 # --------------------------------------------------------------------------------------
 import datetime
+import typing
 from enum import Enum
 from typing import Optional
 from uuid import UUID
@@ -73,4 +74,69 @@ class OrganisationUnitTerminate(OrganisationUnit):
         alias="triggerless",
         description="Flag specifying if triggers should not be invoked, if true.",
         default=False,
+    )
+
+
+class Validity(BaseModel):
+    """Model representing an entities validity range."""
+
+    from_date: Optional[datetime.datetime] = Field(
+        alias="from", description="Start date of the validity."
+    )
+
+    to_date: Optional[datetime.datetime] = Field(
+        alias="to", description="End date of the validity, if applicable."
+    )
+
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {
+            datetime: lambda v: v.isoformat(),
+        }
+
+
+class MoraTriggerRequest(BaseModel):
+    """Model representing a MoRa Trigger Request."""
+
+    type: str = Field(description="Type of the request, ex. 'org_unit'.")
+
+    uuid: UUID = Field(
+        description="UUID for the entity accessed in the request. "
+        "Ex type=ORG_UNIT, then this UUID will be the UUID of the ORG_UNIT"
+    )
+
+    validity: Validity = Field(description="Type of the request, ex. 'org_unit'.")
+
+
+class MoraTrigger(BaseModel):
+    """Model representing a MoRa Trigger."""
+
+    request_type: str = Field(
+        description="Request type to do, ex CREATE, EDIT, TERMINATE or REFRESH. "
+        "Ref: mora.mapping.RequestType"
+    )
+
+    request: MoraTriggerRequest = Field(description="The Request for the trigger.")
+
+    role_type: str = Field(description="Role type for the trigger, ex 'org_unit'.")
+
+    event_type: str = Field(
+        description="Trigger event-type. " "Ref: mora.mapping.EventType"
+    )
+
+    uuid: UUID = Field(
+        description="UUID of the entity being handled in the trigger. "
+        "Ex. type=ORG_UNIT, this this is the org-unit-uuid."
+    )
+
+    result: typing.Any = Field(description="Result of the trigger", default=None)
+
+    # org_unit_uuid: str
+
+
+class MoraTriggerOrgUnit(MoraTrigger):
+    """Model representing a mora-trigger, specific for organization-units."""
+
+    org_unit_uuid: UUID = Field(
+        description="UUID for the organization unit in question."
     )
