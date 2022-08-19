34820907d864ee122bc4992d90bd9a2bf90b5dc3
Emil Madsen <sovende@gmail.com>
2022-08-19 20:52:19 +0200

    [#xxxxx] Restructure and document autocomplete helper

--- backend/mora/service/autocomplete.py
+++ backend/mora/service/autocomplete.py
@@ -1,43 +1,65 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
-from .. import common
-from .. import util
-from ..lora import AutocompleteScope
+from asyncio import gather
+from itertools import starmap
+from uuid import UUID
+
+from mora import common
+from mora import util
+from mora.lora import AutocompleteScope
 
 
-async def get_results(entity, class_uuids, query):
+async def get_results(
+    entity: str, class_uuids: list[UUID], query: str
+) -> dict[str, list]:
+    """Run an autocomplete search query.
+
+    Args:
+        entity:
+            The entity type to autocomplete.
+            One of 'bruger' and 'organisationsenhed'.
+        class_uuids:
+            List of class UUIDs whose title and value will be displayed for match.
+        query:
+            The search query string.
+
+    Returns:
+        A dictionary with key 'items' and a value that are the matches.
+    """
+    if not query:
+        return {"items": []}
+
     connector = common.get_connector()
-    if query:
-        # Build map of {uuid: class data} so that we can look up each class
-        # title later.
-        class_uuids = class_uuids or []
-        class_map = {
-            str(class_uuid): await connector.klasse.get(class_uuid)
-            for class_uuid in class_uuids
+    # Build map of {uuid: class data} so that we can look up each class title later.
+    class_uuids = class_uuids or []
+    loaded_classes = await gather(*map(connector.klasse.get, class_uuids))
+    class_map = dict(zip(class_uuids, loaded_classes))
+
+    # Fetch autocomplete results from LoRa
+    scope = AutocompleteScope(connector, entity)
+    results = await scope.fetch(
+        phrase=util.query_to_search_phrase(query),
+        class_uuids=class_uuids,
+    )
+
+    def convert_attrs(class_uuid, value):
+        title = None
+        try:
+            class_data = class_map[UUID(class_uuid)]
+            title = class_data["attributter"]["klasseegenskaber"][0]["titel"]
+        except (KeyError, TypeError):
+            pass
+        return {
+            "uuid": class_uuid,
+            "value": value,
+            "title": title,
         }
 
-        # Fetch autocomplete results from LoRa
-        scope = AutocompleteScope(connector, entity)
-        results = await scope.fetch(
-            phrase=util.query_to_search_phrase(query),
-            class_uuids=class_uuids,
-        )
+    # Add class title to each attr of each result
+    for result in results["items"]:
+        attrs = result.get("attrs")
+        if attrs is None:
+            continue
+        result["attrs"] = list(starmap(convert_attrs, attrs))
 
-        # Add class title to each attr of each result
-        for result in results["items"]:
-            attrs = result.get("attrs") or []
-            for idx, attr in enumerate(attrs):
-                class_uuid = attr[0]
-                out = {"uuid": class_uuid, "value": attr[1]}
-                try:
-                    class_data = class_map[class_uuid]
-                    out["title"] = class_data["attributter"]["klasseegenskaber"][0][
-                        "titel"
-                    ]
-                except (KeyError, TypeError):
-                    out["title"] = None
-                attrs[idx] = out
-
-        return results
-
-    return {"items": []}
+    return results
