8542cda49cfa86b1862668e1f197c6a940eaa368
Thor Astrup Pedersen <tap@magenta-aps.dk>
2022-08-23 10:33:55 +0200

    Moved DetailTerminate to its own detail module instead of being in _shared.. also added a general detail class since there are gonna be more details in the future.

---
+++ ra-data-models/ramodels/mo/detail.py
@@ -0,0 +1,37 @@
+from typing import Literal
+
+from pydantic import Field
+from ramodels.mo import OpenValidity
+from ramodels.mo._shared import UUIDBase
+
+
+class Detail(UUIDBase):
+    type: Literal["address", "association"] = Field(
+        description="Name of the type of detail we wish to terminate. "
+        "Must be a valid lora role_type like:  'address', "
+        "'association' etc."
+    )
+
+
+class DetailTermination(Detail):
+    validity: OpenValidity = Field(
+        description="MO unit validity, determining in what date-interval "
+        "a unit is available."
+    )
+
+    def to_dict(self) -> dict:
+        request_dict = self.dict(by_alias=True)
+        request_dict["uuid"] = str(self.uuid)
+        if self.validity.from_date:
+            request_dict["validity"][
+                "from"
+            ] = self.validity.from_date.date().isoformat()
+        else:
+            del request_dict["validity"]["from"]
+
+        if self.validity.to_date:
+            request_dict["validity"]["to"] = self.validity.to_date.date().isoformat()
+        else:
+            del request_dict["validity"]["to"]
+
+        return request_dict
