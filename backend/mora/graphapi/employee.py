9602c6fae629709df9f0708df69663e73ad6b96f
Thor Astrup Pedersen <tap@magenta-aps.dk>
2022-08-29 12:10:12 +0200

    Implemented existing logic from /service/e/create into new graphql mutator for the creating employees.

--- backend/mora/graphapi/employee.py
+++ backend/mora/graphapi/employee.py
@@ -1,6 +1,15 @@
+from uuid import UUID
+
+from mora import mapping
 from mora.graphapi.models import EmployeeCreate
 from mora.graphapi.types import EmployeeType
+from mora.service.employee import EmployeeRequestHandler
 
 
 async def create(ec: EmployeeCreate) -> EmployeeType:
-    return EmployeeType()
+    request = await EmployeeRequestHandler.construct(
+        ec.dict(), mapping.RequestType.CREATE
+    )
+    uuid = await request.submit()
+
+    return EmployeeType(uuid=UUID(uuid))
