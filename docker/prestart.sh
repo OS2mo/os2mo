7758e80751f9ca90c1dde64ef6ee44fd6af1a17c
Mads Riis-Weitling <mrw@magenta.dk>
2022-08-30 16:10:43 +0200

    [#51935] Change check on `ENABLE_INTERNAL_LORA` to parse "y", "yes", "1", "true", etc. as truthy

--- docker/prestart.sh
+++ docker/prestart.sh
@@ -12,7 +12,8 @@ set -e
 
 # If DISABLE_ALEMBIC is unset or false, run alembic
 if [ -z "$DISABLE_ALEMBIC" ] || [ "$DISABLE_ALEMBIC" = "false" ]; then
-    if [ "$ENABLE_INTERNAL_LORA" = "True" ]; then
+    # Check if 'ENABLE_INTERNAL_LORA' is "truthy" ("y", "yes", "true", "1", etc.)
+    if [ "`python3 -c "from os import environ; from distutils.util import strtobool; print(strtobool(environ['ENABLE_INTERNAL_LORA']))"`" = 1 ]; then
         python3 -m oio_rest initdb --wait 30
     else
         echo "ENABLE_INTERNAL_LORA is $ENABLE_INTERNAL_LORA, not running 'initdb'"
