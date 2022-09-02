17a2122d60bf985059e3f160f764b7893136eefe
fh@magenta-aps.dk <fh@magenta-aps.dk>
2022-09-02 11:00:22 +0200

    [#52088] Rewrote TextAddressHandlerTests into seperate functions

--- backend/tests/address_handler/test_text.py
+++ backend/tests/address_handler/test_text.py
@@ -2,77 +2,74 @@
 # SPDX-License-Identifier: MPL-2.0
 from unittest.mock import patch
 
-import tests.cases
-from mora.service.address_handler import text
+from mora.service.address_handler.text import TextAddressHandler
 
 
-async def async_facet_get_one_class(x, y, *args, **kwargs):
-    return {"uuid": y}
-
-
-@patch("mora.service.facet.get_one_class", new=async_facet_get_one_class)
-class TextAddressHandlerTests(tests.cases.IsolatedAsyncioTestCase):
-    handler = text.TextAddressHandler
-    visibility = "dd5699af-b233-44ef-9107-7a37016b2ed1"
+async def test_from_effect():
+    # Arrange
     value = "Test text whatever"
 
-    async def test_from_effect(self):
-        # Arrange
-        value = "Test text whatever"
+    effect = {
+        "relationer": {"adresser": [{"urn": "urn:text:%54est%20text%20whatever"}]}
+    }
 
-        effect = {
-            "relationer": {"adresser": [{"urn": "urn:text:%54est%20text%20whatever"}]}
-        }
+    address_handler = await TextAddressHandler.from_effect(effect)
 
-        address_handler = await self.handler.from_effect(effect)
+    # Act
+    actual_value = address_handler.value
 
-        # Act
-        actual_value = address_handler.value
+    # Assert
+    assert value == actual_value
 
-        # Assert
-        self.assertEqual(value, actual_value)
 
-    async def test_from_request(self):
-        # Arrange
-        value = "Test text whatever"
+async def test_from_request():
+    # Arrange
+    value = "Test text whatever"
 
-        request = {"value": value}
-        address_handler = await self.handler.from_request(request)
+    request = {"value": value}
+    address_handler = await TextAddressHandler.from_request(request)
 
-        # Act
-        actual_value = address_handler.value
+    # Act
+    actual_value = address_handler.value
 
-        # Assert
-        self.assertEqual(value, actual_value)
+    # Assert
+    assert value == actual_value
 
-    async def test_get_mo_address(self):
-        # Arrange
-        value = "Test text whatever"
-        address_handler = self.handler(value, self.visibility)
 
-        expected = {
-            "href": None,
-            "name": "Test text whatever",
-            "value": "Test text whatever",
-            "value2": None,
-            "visibility": {"uuid": "dd5699af-b233-44ef-9107-7a37016b2ed1"},
-        }
+async def test_get_mo_address():
+    async def async_facet_get_one_class(x, y, *args, **kwargs):
+        return {"uuid": y}
 
-        # Act
+    # Arrange
+    value = "Test text whatever"
+    visibility = "dd5699af-b233-44ef-9107-7a37016b2ed1"
+    address_handler = TextAddressHandler(value, visibility)
+
+    expected = {
+        "href": None,
+        "name": "Test text whatever",
+        "value": "Test text whatever",
+        "value2": None,
+        "visibility": {"uuid": "dd5699af-b233-44ef-9107-7a37016b2ed1"},
+    }
+
+    # Act
+    with patch("mora.service.facet.get_one_class", new=async_facet_get_one_class):
         actual = await address_handler.get_mo_address_and_properties()
 
         # Assert
-        self.assertEqual(expected, actual)
+        assert expected == actual
 
-    def test_get_lora_address(self):
-        # Arrange
-        value = "Test text whatever"
-        address_handler = self.handler(value, None)
 
-        expected = {"objekttype": "TEXT", "urn": "urn:text:%54est%20text%20whatever"}
+def test_get_lora_address():
+    # Arrange
+    value = "Test text whatever"
+    address_handler = TextAddressHandler(value, None)
 
-        # Act
-        actual = address_handler.get_lora_address()
+    expected = {"objekttype": "TEXT", "urn": "urn:text:%54est%20text%20whatever"}
 
-        # Assert
-        self.assertEqual(expected, actual)
+    # Act
+    actual = address_handler.get_lora_address()
+
+    # Assert
+    assert expected == actual
