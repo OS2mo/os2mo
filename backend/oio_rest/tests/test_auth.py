16079c64e30ec9638313c29e32f473a73896f6a3
Mads Riis-Weitling <mrw@magenta.dk>
2022-09-06 15:25:33 +0200

    [#52272] Remove LoRa test `TestAuthEndpoints.test_200_for_valid_token` which relies on outdated assumptions
    Specifically, it relies on calling the Keycloak REST API with a client
    ID which does not exist in the Keycloak instance deployed in CI.

--- backend/oio_rest/tests/test_auth.py
+++ backend/oio_rest/tests/test_auth.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: MPL-2.0
 import unittest
 
-import requests
 from os2mo_fastapi_utils.auth.test_helper import (
     ensure_endpoints_depend_on_oidc_auth_function,
 )
@@ -12,7 +11,6 @@ from os2mo_fastapi_utils.auth.test_helper import (
 from parameterized import parameterized
 from starlette.status import HTTP_401_UNAUTHORIZED
 
-from oio_rest import config
 from oio_rest.app import create_app
 from oio_rest.auth.oidc import auth
 from oio_rest.tests.util import DBTestCase
@@ -101,37 +99,3 @@ class TestAuthEndpoints(DBTestCase):
     def test_401_for_endpoint_when_no_token(self, url_path: str):
         # Sufficient to test all the GET endpoints
         self.assertRequestFails(url_path, code=HTTP_401_UNAUTHORIZED)
-
-    def test_200_for_valid_token(self):
-        # Get token from Keycloak
-        settings = config.get_settings()
-
-        token_url = (
-            f"{settings.keycloak_schema}://{settings.keycloak_host}"
-            f":{settings.keycloak_port}/auth/realms/{settings.keycloak_realm}"
-            f"/protocol/openid-connect/token"
-        )
-
-        r = requests.post(
-            token_url,
-            data={
-                "grant_type": "client_credentials",
-                # "mo" is no longer a client of the "lora" Keycloak realm:
-                # "client_id": "mo",
-                # "client_secret": "158a2075-aa8a-421c-94a4-2df35377014a",
-                # ... but "dipex" still is:
-                "client_id": "dipex",
-                "client_secret": "a091ed82-6e82-4efc-a8f0-001e2b127853",
-            },
-        )
-
-        token = r.json()["access_token"]
-        headers = {"Authorization": "Bearer " + token}
-
-        # Send request with auth header to LoRa
-
-        self.assertOK(
-            self.perform_request(
-                "/organisation/organisationenhed/fields", headers=headers
-            )
-        )
