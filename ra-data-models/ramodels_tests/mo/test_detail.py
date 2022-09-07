2d8f0189a381e7cc8c08e675a2b2347308d5f74f
Thor Astrup Pedersen <tap@magenta-aps.dk>
2022-09-07 16:11:45 +0200

    Fixed new to_dict test have changing the DetailTerminate.validity back to be a anon-dict.

--- ra-data-models/ramodels_tests/mo/test_detail.py
+++ ra-data-models/ramodels_tests/mo/test_detail.py
@@ -9,7 +9,6 @@
 from hypothesis import given
 from hypothesis import strategies as st
 
-from ramodels.mo._shared import OpenValidity
 from ramodels.mo.detail import DetailTermination
 
 
@@ -24,26 +23,18 @@ class TestDetailTerminate:
         details_terminate = DetailTermination(
             uuid="7902e588-3c69-405a-8f6c-1717d396086a",
             type="address",
-            validity=OpenValidity(
-                from_date=given_from_date,
-                to_date=given_to_date,
-            ),
+            validity={"from": given_from_date, "to": given_to_date},
         )
         details_terminate_dict = details_terminate.to_dict()
+
+        # Assert normal stuff
         assert details_terminate_dict.get("uuid", None) == str(details_terminate.uuid)
         assert details_terminate_dict.get("type", None) == details_terminate.type
 
+        # Assert validity
         dict_validity = details_terminate_dict.get("validity", {})
-        model_from_date = (
-            details_terminate.validity.from_date.date().isoformat()
-            if details_terminate.validity.from_date
-            else None
-        )
+        model_from_date = details_terminate.validity.get("from", None)
         assert dict_validity.get("from", None) == model_from_date
 
-        model_to_date = (
-            details_terminate.validity.to_date.date().isoformat()
-            if details_terminate.validity.to_date
-            else None
-        )
+        model_to_date = details_terminate.validity.get("to", None)
         assert dict_validity.get("to", None) == model_to_date
