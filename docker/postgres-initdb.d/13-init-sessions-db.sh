ae9141b0c47c8e5041f0bf0d42c4aaf94d341f66
Mathias Dannesbo (Magenta) <md@magenta.dk>
2022-09-08 10:40:04 +0200

    Remove obsolete comments

--- docker/postgres-initdb.d/13-init-sessions-db.sh
+++ docker/postgres-initdb.d/13-init-sessions-db.sh
@@ -3,11 +3,6 @@
 # SPDX-FileCopyrightText: 2019-2020 Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
 
-################################################################################
-# Changes to this file requires approval from Labs. Please add a person from   #
-# Labs as required approval to your MR if you have any changes.                #
-################################################################################
-
 set -e
 
 true "${SESSIONS_DB_USER:?SESSIONS_DB_USER is unset. Error!}"
