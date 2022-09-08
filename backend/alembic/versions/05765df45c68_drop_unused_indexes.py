e33b725c5577c59404a2d2fab5a71cba5e470b6e
Mads Riis-Weitling <mrw@magenta.dk>
2022-09-08 14:40:27 +0200

    [#44361] Move Alembic migrations to correct folder; ensure `alembic_helpers.apply_sql_from_file` exists

--- backend/alembic/versions/05765df45c68_drop_unused_indexes.py
+++ backend/alembic/versions/05765df45c68_drop_unused_indexes.py
@@ -1,13 +1,12 @@
 # SPDX-FileCopyrightText: 2022 Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
-
 """drop unused indexes
 
 Revision ID: 05765df45c68
 Revises: b27228471604
 Create Date: 2022-04-08 17:57:45.439305
 """
-from oio_rest.db.alembic_migration_helpers import apply_sql_from_file
+from oio_rest.db.alembic_helpers import apply_sql_from_file
 
 
 # revision identifiers, used by Alembic.
