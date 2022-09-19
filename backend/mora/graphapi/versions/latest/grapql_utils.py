6a98a4230c4e80bd8fd9f6261e76157bbb186fc5
Thor Astrup Pedersen <tap@magenta-aps.dk>
2022-09-19 14:48:45 +0200

    Moved CprNo type to graphql_utils module, to graphapi latest

---
+++ backend/mora/graphapi/versions/latest/grapql_utils.py
@@ -0,0 +1,4 @@
+
+
+class CprNo(ConstrainedStr):
+    regex = re.compile(r"^\d{10}$")
--- backend/mora/graphapi/versions/latest/models.py
+++ backend/mora/graphapi/versions/latest/models.py
@@ -16,7 +16,7 @@ from mora import common
 from mora import exceptions
 from mora import mapping
 from mora import util
-from mora.graphapi.utils import CprNo
+from mora.graphapi.versions.latest.grapql_utils import CprNo
 from mora.graphapi.versions.latest.grapql_utils import PrintableStrAllowEmpty
 from mora.util import ONE_DAY
 from mora.util import POSITIVE_INFINITY
