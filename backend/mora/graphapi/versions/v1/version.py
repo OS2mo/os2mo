9816bf9a3fd44b9d8c62148a4b10fefcc54c414d
Casper V. Kristensen <vk@magenta.dk>
2022-09-19 19:47:01 +0200

    [#50861] Implement compatibility shim for old org unit parent behaviour

--- backend/mora/graphapi/versions/v1/version.py
+++ backend/mora/graphapi/versions/v1/version.py
@@ -1,18 +1,26 @@
 # SPDX-FileCopyrightText: 2022 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
-from ..latest.version import LatestGraphQLSchema
-from ..latest.version import LatestGraphQLVersion
+from datetime import date
+
+from ..v2.version import GraphQLSchema2
+from ..v2.version import GraphQLVersion2
+from .schema import OrganisationUnit
 
 
-class GraphQLSchema1(LatestGraphQLSchema):
-    """First pinned version of the GraphQL Schema."""
+class GraphQLSchema1(GraphQLSchema2):
+    """First pinned version of the GraphQL Schema.
 
-    pass
+    Implements backwards-compatibility of org unit parent, making it return an optional
+    list of exactly one element.
+    """
+
+    # Override organisation unit object type to object with legacy behaviour
+    types = [OrganisationUnit]
 
 
-class GraphQLVersion1(LatestGraphQLVersion):
+class GraphQLVersion1(GraphQLVersion2):
     """First pinned version of the GraphQL API."""
 
     version = 1
-    deprecation_date = None
+    deprecation_date = date(2023, 3, 1)
     schema = GraphQLSchema1
