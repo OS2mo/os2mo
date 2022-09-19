7b8a1f58096160ec15957b327dc6fb8fea8c853d
Michael Søegaard <ms@magenta-aps.dk>
2022-09-19 08:32:24 +0200

    Instantiate test class

--- backend/tests/graphapi/test_class_mutators.py
+++ backend/tests/graphapi/test_class_mutators.py
@@ -76,91 +76,96 @@ def prepare_query_data(test_data, query_response):
     return test_data, query
 
 
-class TestClassMutator:
-    @settings(max_examples=5)
-    @given(test_data=write_strat())
-    async def test_create_class(self, test_data):
-        """Test that we can write all attributes of the class data model."""
+def execute_test_create_class():
+    class TestClassMutator:
+        @settings(max_examples=20)
+        @given(test_data=write_strat())
+        async def execute(self, test_data):
+            """Test that we can write all attributes of the class data model."""
 
-        mutate_query = """
-                        mutation CreateClass($input: ClassCreateInput!){
-                            class_create(input: $input){
-                                                        uuid
-                                                        }
-                        }
+            mutate_query = """
+                            mutation CreateClass($input: ClassCreateInput!){
+                                class_create(input: $input){
+                                                            uuid
+                                                            }
+                            }
+                            """
+
+            test_data = prepare_mutator_data(test_data)
+
+            mut_response = await LatestGraphQLSchema.get().execute(
+                query=mutate_query, variable_values={"input": test_data}
+            )
+
+            response_uuid = (
+                mut_response.data.get("class_create", {}).get("uuid", {})
+                if isinstance(mut_response.data, dict)
+                else {}
+            )
+
+            """Query data to check that it actually gets written to database"""
+            query_query = """query ($uuid: [UUID!]!)
+                            {
+                                __typename
+                                classes(uuids: $uuid)
+                                {
+                                uuid
+                                type
+                                org_uuid
+                                user_key
+                                name
+                                facet_uuid
+                                }
+                            }
+
                         """
 
-        test_data = prepare_mutator_data(test_data)
+            graphql_version = LatestGraphQLVersion
+            context_value = await graphql_version.get_context()
 
-        mut_response = await LatestGraphQLSchema.get().execute(
-            query=mutate_query, variable_values={"input": test_data}
-        )
+            query_response = await LatestGraphQLSchema.get().execute(
+                query=query_query,
+                variable_values={"uuid": str(response_uuid)},
+                context_value=context_value,
+            )
 
-        response_uuid = (
-            mut_response.data.get("class_create", {}).get("uuid", {})
-            if isinstance(mut_response.data, dict)
-            else {}
-        )
+            test_data, query = prepare_query_data(test_data, query_response)
 
-        """Query data to check that it actually gets written to database"""
-        query_query = """query ($uuid: [UUID!]!)
-                        {
-                            __typename
-                            classes(uuids: $uuid)
-                            {
-                            uuid
-                            type
-                            org_uuid
-                            user_key
-                            name
-                            facet_uuid
-                            }
-                        }
+            """Assert response returned by mutation."""
+            assert mut_response.errors is None
+            assert mut_response.data
+            assert response_uuid == test_data["uuid"]
 
-                    """
+            """Assert response returned by quering data written."""
+            assert query_response.errors is None
+            assert query == test_data
 
-        graphql_version = LatestGraphQLVersion
-        context_value = await graphql_version.get_context()
+    TestClassMutator().execute()
 
-        query_response = await LatestGraphQLSchema.get().execute(
-            query=query_query,
-            variable_values={"uuid": str(response_uuid)},
-            context_value=context_value,
-        )
 
-        test_data, query = prepare_query_data(test_data, query_response)
+"""Test exception gets raised if illegal values are entered"""
 
-        """Assert response returned by mutation."""
-        assert mut_response.errors is None
-        assert mut_response.data
-        assert response_uuid == test_data["uuid"]
 
-        """Assert response returned by quering data written."""
-        assert query_response.errors is None
-        assert query == test_data
+@pytest.mark.parametrize(
+    "uuid, type_, user_key, name, org_uuid",
+    [
+        (
+            "23d891b5-85aa-4eee-bec7-e84fe21883c5",
+            "class",
+            "",
+            "\x01",
+            "8d6c00dd-4be9-4bdb-a558-1f85183cd920",
+        ),
+        (
+            "23d891b5-85aa-4eee-bec7-e84fe21883c5",
+            "class",
+            "",
+            "",
+            "8d6c00dd-4be9-4bdb-a558-1f85183cd920",
+        ),
+    ],
+)
+def test_write_fails(uuid, type_, user_key, name, org_uuid):
 
-    """Test exception gets raised if illegal values are entered"""
-
-    @pytest.mark.parametrize(
-        "uuid, type_, user_key, name, org_uuid",
-        [
-            (
-                "23d891b5-85aa-4eee-bec7-e84fe21883c5",
-                "class",
-                "",
-                "\x01",
-                "8d6c00dd-4be9-4bdb-a558-1f85183cd920",
-            ),
-            (
-                "23d891b5-85aa-4eee-bec7-e84fe21883c5",
-                "class",
-                "",
-                "",
-                "8d6c00dd-4be9-4bdb-a558-1f85183cd920",
-            ),
-        ],
-    )
-    def test_write_fails(self, uuid, type_, user_key, name, org_uuid):
-
-        with pytest.raises(Exception):
-            ClassCreate(uuid, user_key, type_, name, org_uuid)
+    with pytest.raises(Exception):
+        ClassCreate(uuid, user_key, type_, name, org_uuid)
