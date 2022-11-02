6e52d6fa19c2fa87695543f6f182e7efe1c9b4a2
Thor Astrup Pedersen <tap@magenta-aps.dk>
2022-11-02 13:56:07 +0100

    Fixed unit tests after removing Triggerless from all mora.graphapi.models

--- backend/tests/graphapi/test_associations.py
+++ backend/tests/graphapi/test_associations.py
@@ -483,12 +483,11 @@ async def test_update_association_unit_test(
 
 @given(
     given_uuid=st.uuids(),
-    triggerless=st.booleans(),
     given_validity_dts=st.tuples(st.datetimes() | st.none(), st.datetimes()).filter(
         lambda dts: dts[0] <= dts[1] if dts[0] and dts[1] else True
     ),
 )
-async def test_association_terminate(given_uuid, triggerless, given_validity_dts):
+async def test_association_terminate(given_uuid, given_validity_dts):
     from_date, to_date = given_validity_dts
 
     # The terminate logic have a check that verifies we don't use times other than:
@@ -500,7 +499,6 @@ async def test_association_terminate(given_uuid, triggerless, given_validity_dts
 
     test_data = AssociationTerminate(
         uuid=given_uuid,
-        triggerless=triggerless,
         from_date=from_date,
         to_date=to_date,
     )
