87e594ad04920e9113e035efb5195572a9f353d4
Thor Astrup Pedersen <tap@magenta-aps.dk>
2022-11-23 15:21:49 +0100

    feat: created feature flag for hiding employee assoication columns
    Also created unit-test to verify new feature flag

--- backend/tests/test_configuration_settings.py
+++ backend/tests/test_configuration_settings.py
@@ -19,12 +19,7 @@ async def test_feature_flag_it_system_entry_edit_fields_disabled(
 ):
     feature_flag = test_data[0]
     feature_flag_expected_val = test_data[1]
-
-    # Create computed version of the feature flag, which are the keys that actually
-    # exist on the Settings class / is returned from our GraphQL
-    # SERVICE_ENDPOINTS_CONFIGURATIONS-endpoint.
-    # FYI: lstrip = backend\mora\service\util.py:51 - we remove 'confdb_' prefixes
-    feature_flag_computed = feature_flag.lower().lstrip("confdb_")
+    feature_flag_computed = _compute_feature_flag_name(feature_flag)
 
     with util.set_settings_contextmanager(**{feature_flag: feature_flag_expected_val}):
         response = serviceapi_post(url=SERVICE_ENDPOINTS_CONFIGURATIONS)
@@ -32,3 +27,36 @@ async def test_feature_flag_it_system_entry_edit_fields_disabled(
         assert (
             str(response.data.get(feature_flag_computed)) == feature_flag_expected_val
         )
+
+
+@pytest.mark.parametrize(
+    "feature_flag_val,confdb_expected_return",
+    [
+        ("null", None),
+        ("[]", []),
+        (
+            '["third_party_association_type"]',
+            ["third_party_association_type"],
+        ),
+    ],
+)
+async def test_feature_flag_employee_hide_association_columns(
+    serviceapi_post, feature_flag_val, confdb_expected_return
+):
+    """Test feature flag CONFDB_EMPLOYEE_HIDE_ASSOCIATION_COLUMNS.
+
+    The flag is used by the frontend to hide employee association columns in the details table."""
+    feature_flag_name = "CONFDB_EMPLOYEE_HIDE_ASSOCIATION_COLUMNS"
+    feature_flag_name_computed = _compute_feature_flag_name(feature_flag_name)
+    with util.set_settings_contextmanager(**{feature_flag_name: feature_flag_val}):
+        response = serviceapi_post(url=SERVICE_ENDPOINTS_CONFIGURATIONS)
+        assert feature_flag_name_computed in response.data.keys()
+        assert response.data.get(feature_flag_name_computed) == confdb_expected_return
+
+
+def _compute_feature_flag_name(feature_flag_name: str):
+    # Create computed version of the feature flag, which are the keys that actually
+    # exist on the Settings class / is returned from our GraphQL
+    # SERVICE_ENDPOINTS_CONFIGURATIONS-endpoint.
+    # FYI: lstrip = backend\mora\service\util.py:51 - we remove 'confdb_' prefixes
+    return feature_flag_name.lower().lstrip("confdb_")
