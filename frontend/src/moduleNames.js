c8d37ab4e879a7b9549c940fc767eb387b8731c7
Emil Madsen <sovende@gmail.com>
2022-11-25 17:58:49 +0100

    chore: [#51194] removed the frontend code from the OS2mo repository

--- frontend/src/moduleNames.js
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: 2018-2020 Magenta ApS
-// SPDX-License-Identifier: MPL-2.0
-
-// Names of folders in ./src/modules/
-export const moduleNames = ["organisationMapper", "query", "insight", "admin"]
