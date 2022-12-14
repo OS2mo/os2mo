fa204ac496f7c641f740254a89fc8f21c013f9f8
Thor Astrup Pedersen <tap@magenta-aps.dk>
2022-12-14 15:47:01 +0100

    feat: implemented logic for creating a sime/basic organisation through LoRa

--- backend/mora/graphapi/versions/latest/organisation.py
+++ backend/mora/graphapi/versions/latest/organisation.py
@@ -1,9 +1,27 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
+from fastapi.encoders import jsonable_encoder
+
 from .models import OrganisationCreate
 from .types import OrganisationType
+from mora import lora
+from ramodels.lora.organisation import Organisation
 
 
 async def create_organisation(input: OrganisationCreate) -> OrganisationType:
-    # 00000000-0000-0000-0000-000000000000
-    return OrganisationType(uuid="00000000-0000-0000-0000-000000000000")
+    # Convert input to LoRa payload
+    root_organisation = Organisation.from_simplified_fields(
+        name=input.name,
+        user_key=input.name,
+    )
+
+    jsonified = jsonable_encoder(
+        obj=root_organisation, by_alias=True, exclude={"uuid"}, exclude_none=True
+    )
+
+    # Create the ORG in LoRa
+    c = lora.Connector(virkningfra="-infinity", virkningtil="infinity")
+    uuid = await c.organisation.create(jsonified)
+
+    # Return
+    return OrganisationType(uuid=uuid)
