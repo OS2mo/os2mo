ff64642f41fd3eea5c626b59678225cacb23b815
Emil Madsen <sovende@gmail.com>
2023-01-07 03:13:39 +0100

    test: [#xxxxx] rework test_autocomplete.py

--- backend/tests/test_autocomplete.py
+++ backend/tests/test_autocomplete.py
@@ -3,29 +3,27 @@
 import pytest
 import respx
 from httpx import Response
-from parameterized import parameterized
 
-from .cases import AsyncLoRATestCase
 from mora.lora import AutocompleteScope
 from mora.lora import Connector
 
 
 @pytest.mark.usefixtures("mock_asgi_transport")
-class TestAutocompleteScope(AsyncLoRATestCase):
-    @parameterized.expand(
-        [
-            ("bruger", []),
-            ("organisationsenhed", []),
-        ]
+@pytest.mark.parametrize(
+    "path,expected_result",
+    [
+        ("bruger", []),
+        ("organisationsenhed", []),
+    ],
+)
+@respx.mock
+async def test_autocomplete(path: str, expected_result: list) -> None:
+    respx.get(f"http://localhost/lora/autocomplete/{path}?phrase=phrase").mock(
+        return_value=Response(200, json={"results": []})
     )
-    @respx.mock
-    async def test_autocomplete(self, path: str, expected_result: list):
-        respx.get(f"http://localhost/lora/autocomplete/{path}?phrase=phrase").mock(
-            return_value=Response(200, json={"results": []})
-        )
-        connector = Connector()
-        scope = AutocompleteScope(connector, path)
-        response = await scope.fetch("phrase")
-        assert "items" in response
-        result = response["items"]
-        assert result == expected_result
+    connector = Connector()
+    scope = AutocompleteScope(connector, path)
+    response = await scope.fetch("phrase")
+    assert "items" in response
+    result = response["items"]
+    assert result == expected_result
