7415d7ca7eff4cb32930424f4aba0efc0be7010a
Casper V. Kristensen <vk@magenta.dk>
2023-01-26 17:05:24 +0100

    feat!: [#54590] Remove DUMMY_MODE feature flag
    The flag has been superseded by the ENABLE_SP flag. Note, however, that
    if DUMMY_MODE was previously true, ENABLE_SP should be false.

--- backend/mora/service/shimmed/cpr.py
+++ backend/mora/service/shimmed/cpr.py
@@ -1,6 +1,5 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
-import random
 from typing import Any
 
 import requests
@@ -21,8 +20,6 @@ logger = get_logger()
 
 def get_citizen(cpr: str) -> dict[str, Any]:
     settings = config.get_settings()
-    if settings.is_dummy_mode():
-        return _get_citizen_stub(cpr)
 
     sp_uuids = {
         "service_agreement": str(settings.sp_settings.sp_agreement_uuid),
@@ -45,160 +42,6 @@ def get_citizen(cpr: str) -> dict[str, Any]:
     except requests.exceptions.SSLError as e:
         logger.exception(exception=e)
         exceptions.ErrorCodes.E_SP_SSL_ERROR()
-
-
-MALE_FIRST_NAMES = [
-    "William",
-    "Oliver",
-    "Noah",
-    "Emil",
-    "Victor",
-    "Magnus",
-    "Frederik",
-    "Mikkel",
-    "Lucas",
-    "Alexander",
-    "Oscar",
-    "Mathias",
-    "Sebastian",
-    "Malthe",
-    "Elias",
-    "Christian",
-    "Mads",
-    "Gustav",
-    "Villads",
-    "Tobias",
-    "Anton",
-    "Carl",
-    "Silas",
-    "Valdemar",
-    "Benjamin",
-    "Nikolaj",
-    "Marcus",
-    "August",
-    "Sander",
-    "Jacob",
-    "Jonas",
-    "Adam",
-    "Andreas",
-    "Simon",
-    "Jonathan",
-    "Alfred",
-    "Philip",
-    "Storm",
-    "Nicklas",
-    "Rasmus",
-    "Felix",
-    "Aksel",
-    "Johan",
-    "Daniel",
-    "Tristan",
-    "Bertram",
-    "Liam",
-    "Kasper",
-    "Laurits",
-    "Marius",
-]
-
-FEMALE_FIRST_NAMES = [
-    "Emma",
-    "Ida",
-    "Clara",
-    "Laura",
-    "Isabella",
-    "Sofia",
-    "Sofie",
-    "Anna",
-    "Mathilde",
-    "Freja",
-    "Caroline",
-    "Lærke",
-    "Maja",
-    "Josefine",
-    "Liva",
-    "Alberte",
-    "Karla",
-    "Victoria",
-    "Olivia",
-    "Alma",
-    "Mille",
-    "Sarah",
-    "Frida",
-    "Julie",
-    "Emilie",
-    "Marie",
-    "Ella",
-    "Nanna",
-    "Signe",
-    "Agnes",
-    "Nicoline",
-    "Malou",
-    "Filippa",
-    "Johanne",
-    "Cecilie",
-    "Silje",
-    "Lea",
-    "Asta",
-    "Astrid",
-    "Naja",
-    "Celina",
-    "Tilde",
-    "Emily",
-    "Luna",
-    "Ellen",
-    "Katrine",
-    "Esther",
-    "Merle",
-    "Selma",
-    "Liv",
-]
-
-LAST_NAMES = [
-    "Nielsen",
-    "Jensen",
-    "Hansen",
-    "Pedersen",
-    "Andersen",
-    "Christensen",
-    "Larsen",
-    "Sørensen",
-    "Rasmussen",
-    "Jørgensen",
-    "Petersen",
-    "Madsen",
-    "Kristensen",
-    "Olsen",
-    "Thomsen",
-    "Christiansen",
-    "Poulsen",
-    "Johansen",
-    "Møller",
-    "Mortensen",
-]
-
-# as of 2018, the oldest living Dane was born in 1908...
-EARLIEST_BIRTHDATE = util.parsedatetime("1901-01-01")
-
-
-def _get_citizen_stub(cpr: str) -> dict[str, str]:
-    # Seed random with CPR number to ensure consistent output
-    random.seed(cpr)
-
-    # If feature flag is set, check birthdate encoded in CPR number
-    settings = config.get_settings()
-    if settings.cpr_validate_birthdate:
-        # disallow future CPR numbers and people too old to occur
-        # (interestingly, the latter also avoids weirdness related to
-        # Denmark using Copenhagen solar time in the 19th century...)
-        if not EARLIEST_BIRTHDATE < util.get_cpr_birthdate(cpr) < util.now():
-            raise KeyError("CPR not found")
-
-    if (int(cpr[-1]) % 2) == 0:
-        first_name = random.choice(FEMALE_FIRST_NAMES)
-    else:
-        first_name = random.choice(MALE_FIRST_NAMES)
-
-    return {"fornavn": first_name, "efternavn": random.choice(LAST_NAMES)}
 
 
 class SearchCPRReturn(BaseModel):
@@ -240,12 +83,12 @@ def search_cpr(
         An empty dict if Serviceplatformen is disabled (`ENABLE_SP=false`).
         A SearchCPRReturn model if Serviceplatform is enabled.
     """
-    if not config.get_settings().enable_sp:
-        return {}
-
     cpr = q
     if not util.is_cpr_number(cpr):
         exceptions.ErrorCodes.V_CPR_NOT_VALID(cpr=cpr)
+
+    if not config.get_settings().enable_sp:
+        return {}
 
     try:
         sp_data = get_citizen(cpr)
