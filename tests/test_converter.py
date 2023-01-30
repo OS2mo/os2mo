e7c662a4fe035b471dadab0181ddca73f240fb26
Lars Peter Thomsen <larsp@magenta-aps.dk>
2023-01-30 14:19:56 +0100

    [#53377] name normalization

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -880,6 +880,21 @@ def test_get_object_uuid_from_name(converter: LdapConverter):
     with pytest.raises(UUIDNotFoundException):
         converter.get_object_uuid_from_name(info_dict, "")
 
+    uuid2 = uuid4()
+    # Check that a perfect match will be preferred over a normalized match
+    info_dict = {
+        uuid2: {"uuid": uuid2, "user_key": name.lower()},
+        uuid: {"uuid": uuid, "user_key": name},
+    }
+    assert converter.get_object_uuid_from_name(info_dict, name) == uuid
+
+    # Check that if no perfect matches exist, use the first match
+    info_dict = {
+        uuid: {"uuid": uuid, "user_key": name.upper()},
+        uuid2: {"uuid": uuid2, "user_key": name.lower()},
+    }
+    assert converter.get_object_uuid_from_name(info_dict, name) == uuid
+
 
 def test_create_org_unit(converter: LdapConverter):
     uuids = [str(uuid4()), str(uuid4()), str(uuid4())]
