fba2b1aea9b879f6f295ae6f0ae8fabee7f872ed
Emil Madsen <sovende@gmail.com>
2023-05-08 13:46:14 +0200

    test: [#55975] rewrite endpoint uniqueness test
    This change rewrites `test_ensure_endpoint_function_names_are_unique`
    from including and thereby instancing our top-level uvicorn app, to
    instead using the `test_app` fixture.

--- backend/tests/test_endpoint_uniqueness.py
+++ backend/tests/test_endpoint_uniqueness.py
@@ -1,9 +1,9 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
-import mora.main
+from fastapi import FastAPI
 
 
-def test_ensure_endpoint_function_names_are_unique():
+def test_ensure_endpoint_function_names_are_unique(fastapi_test_app: FastAPI) -> None:
     # RBAC requires these endpoints to be unique
     must_be_unique = [
         "create_org_unit",
@@ -11,7 +11,7 @@ def test_ensure_endpoint_function_names_are_unique():
         "terminate_org_unit",
         "terminate_employee",
     ]
-    endpoint_func_names = [route.name for route in mora.main.app.routes]
+    endpoint_func_names = [route.name for route in fastapi_test_app.routes]
 
     for endpoint in must_be_unique:
         endpoint_func_names.remove(endpoint)
