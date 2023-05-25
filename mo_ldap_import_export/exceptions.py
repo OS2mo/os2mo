1930de41c470447fa2141b9dfd739be905d3db16
Nick Janssen <nj@magenta-aps.dk>
2023-05-25 11:51:24 +0200

    fix: [#56217] removed log messages
    They confuse us more, than they help us. We get the message anyway, if
    the exception is raised. If the exception is not raised, we are not
    interested in getting an exception in the logs.

--- mo_ldap_import_export/exceptions.py
+++ mo_ldap_import_export/exceptions.py
@@ -3,49 +3,40 @@
 # SPDX-License-Identifier: MPL-2.0
 from fastapi import HTTPException
 
-from .logging import logger
-
 
 class MultipleObjectsReturnedException(HTTPException):
     def __init__(self, message):
         super().__init__(status_code=404, detail=message)
-        logger.exception(message)
 
 
 class NoObjectsReturnedException(HTTPException):
     def __init__(self, message):
         super().__init__(status_code=404, detail=message)
-        logger.exception(message)
 
 
 class CPRFieldNotFound(HTTPException):
     def __init__(self, message):
         super().__init__(status_code=404, detail=message)
-        logger.exception(message)
 
 
 class AttributeNotFound(HTTPException):
     def __init__(self, message):
         super().__init__(status_code=404, detail=message)
-        logger.exception(message)
 
 
 class IncorrectMapping(HTTPException):
     def __init__(self, message):
         super().__init__(status_code=400, detail=message)
-        logger.exception(message)
 
 
 class NotSupportedException(HTTPException):
     def __init__(self, message):
         super().__init__(status_code=404, detail=message)
-        logger.exception(message)
 
 
 class InvalidNameException(HTTPException):
     def __init__(self, message):
         super().__init__(status_code=404, detail=message)
-        logger.exception(message)
 
 
 class UUIDNotFoundException(HTTPException):
