d082524c63e90976a5d1a4d395de61516a70bd12
Carl Bordum Hansen <carl@bordum.dk>
2023-06-20 13:21:06 +0200

    chore: [#54447] move comment, remove CMD overwrite

--- docker/start.sh
+++ docker/start.sh
@@ -9,6 +9,12 @@ set -ex
 # (Currently this means applying Alembic migrations to the `mox` database.)
 . ./docker/prestart.sh
 
+# In production, we use gunicorn with the worker class
+# "uvicorn.workers.UvicornWorker", but that does not work with --reload, so we
+# use uvicorn in development at the cost of some dev/prod parity. This has one
+# issue in dev where the service won't restart (because of the --reload flag)
+# when an error occurs. The service restarts as soon as a file is edited
+# though.
 if [ "$ENVIRONMENT" = "development" ]; then
     echo "Running MO in development mode (live reload)"
     exec uvicorn --reload --host 0.0.0.0 --port 80 --factory mora.app:create_app --timeout-keep-alive 100
