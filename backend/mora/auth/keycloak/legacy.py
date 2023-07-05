1e41502790eca93197cbf56c4d9c85e1551cbca4
Emil Madsen <sovende@gmail.com>
2023-07-05 12:27:50 +0200

    feat!: [#52180] remove support from legacy database sessions
    This commit removes the code that allows OS2mo to reads legacy session
    tokens from the database. A replacement for this mechanism was
    introduced in OS2mo 21.1.0 where it became possible to give valid legacy
    session tokens via the `os2mo_legacy_sessions` environmental variable.
    
    Additionally the following environmental variables have been removed:
    
    * `os2mo_legacy_session_support`:
    
      Was previously a boolean flag stating whether to activate the legacy
      session backend. This has been superseeded by the `os2mo_legacy_sessions`
      environmental variable, such that if a non-empty list of legacy sessions
      are provided, the legacy backend is activated.
    
    * `session_db_*`:
    
      Was previously various configuration variables to connect to the
      `sessions` database, has now been eliminated completely.
    
    BREAKING CHANGE:
    
    This commit is a breaking change for customers that have not yet
    migrated away from legacy sessions or whom have not migrated to the
    newly introduced environmental variable for legacy sessions.
    
    The preferred migration path is to avoid using legacy sessions as they
    are potentially insecure, however if a temporary workaround is required
    a somewhat straight forward migrations path to using the environmental
    variable exists.
    
    Simply extract all active session tokens from the `sessions` database
    table (i.e. the `session_id`) column. Entries have a format similar to:
    ```
    session:a5b7b0b7-77c5-4ede-ae2e-a8c6e2998a6d
    ```
    Remove the `session:` prefix and provide the UUIDs as a JSON-list to the
    `os2mo_legacy_sessions` environmental variable.

--- backend/mora/auth/keycloak/legacy.py
+++ backend/mora/auth/keycloak/legacy.py
@@ -1,95 +1,14 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 from contextlib import suppress
-from datetime import datetime
-from functools import lru_cache
 from uuid import UUID
-
-from sqlalchemy import Column
-from sqlalchemy import create_engine
-from sqlalchemy import DateTime
-from sqlalchemy import Integer
-from sqlalchemy import LargeBinary
-from sqlalchemy import String
-from sqlalchemy import update
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
-from structlog import get_logger
 
 from mora import config
 
-logger = get_logger()
-
-Base = declarative_base()
-
-
-class SessionModel(Base):
-    __tablename__ = "sessions"
-
-    id = Column(Integer, primary_key=True)
-    session_id = Column(String(255), unique=True)
-    data = Column(LargeBinary)
-    expiry = Column(DateTime)
-
-    def __init__(self, session_id, data, expiry):
-        self.session_id = session_id
-        self.data = data
-        self.expiry = expiry
-
-    def __repr__(self):
-        return "<Session data %s>" % self.data
-
-
-def _get_connection_url():
-    settings = config.get_settings()
-    dbname = settings.session_db_name
-    user = settings.session_db_user
-    password = settings.session_db_password
-    host = settings.session_db_host
-    port = settings.session_db_port
-    connection_url = "postgresql+psycopg2://"
-    connection_url += str(user) + ":" + str(password)
-    connection_url += "@" + str(host) + ":" + str(port)
-    connection_url += "/" + str(dbname)
-    return connection_url
-
-
-@lru_cache
-def _get_engine():
-    connection_url = _get_connection_url()
-    logger.debug("Open connection to database")
-    try:
-        # Create exactly one database connection per MO worker process.
-        # In case this worker process has already created a database connection, reuse
-        # that connection.
-        engine = create_engine(
-            connection_url,
-            execution_options={"isolation_level": "AUTOCOMMIT"},
-            pool_size=1,
-            max_overflow=0,
-        )
-        return engine
-    except Exception:
-        logger.error("Database connection error")
-        raise
-
 
 def validate_session(session_id: str) -> bool:
-    """Validate the existence of a session from legacy session table"""
+    """Validate the existence of a session in our legacy sessions."""
     settings = config.get_settings()
-    if settings.os2mo_legacy_sessions is not None:
-        with suppress(ValueError):
-            return UUID(session_id) in settings.os2mo_legacy_sessions
-        return False
-
-    store_id = f"session:{session_id}"
-    engine = _get_engine()
-    with Session(engine) as session:
-        # Mark session as used by setting expiry to max
-        result = session.execute(
-            update(SessionModel)
-            .values(expiry=datetime.max)
-            .where(SessionModel.session_id == store_id)
-            .returning(SessionModel.id)
-        )
-        return result.scalar() is not None
+    with suppress(ValueError):
+        return UUID(session_id) in settings.os2mo_legacy_sessions
+    return False
