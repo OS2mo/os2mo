64fb623e4b0abd45bb9565298c983334f1ca45a0
Casper V. Kristensen <vk@magenta.dk>
2023-08-21 12:53:07 +0200

    feat: [#53863] add testing endpoint to flush AMQP messages
    This is needed for integration tests to avoid having to wait up to a
    minute for the AMQP messages.

--- backend/mora/testing.py
+++ backend/mora/testing.py
@@ -1,9 +1,13 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 from fastapi import APIRouter
+from ramqp import AMQPSystem
+from starlette.requests import Request
 from starlette.status import HTTP_204_NO_CONTENT
 from structlog import get_logger
 
+from mora import amqp
+from mora import config
 from oio_rest.db import get_connection
 
 logger = get_logger()
@@ -12,7 +16,22 @@ logger = get_logger()
 router = APIRouter()
 
 
-@router.post("/autocommit", status_code=HTTP_204_NO_CONTENT)
+@router.post("/amqp/flush", status_code=HTTP_204_NO_CONTENT)
+async def flush(request: Request) -> None:
+    """
+    Flush queued AMQP events immediately.
+
+    Note that this is only needed for the "new" AMQP subsystem. Events in the old one
+    are always sent immediately.
+    """
+    logger.warning("Flushing AMQP events")
+
+    settings = config.get_settings()
+    amqp_system = AMQPSystem(settings.amqp)
+    await amqp._emit_events(request.app.state.sessionmaker, amqp_system)
+
+
+@router.post("/database/autocommit", status_code=HTTP_204_NO_CONTENT)
 async def autocommit(enable: bool) -> None:
     """
     Toggle database autocommit.
@@ -22,7 +41,7 @@ async def autocommit(enable: bool) -> None:
     connection.autocommit = enable
 
 
-@router.post("/commit", status_code=HTTP_204_NO_CONTENT)
+@router.post("/database/commit", status_code=HTTP_204_NO_CONTENT)
 async def commit() -> None:
     """
     Commit pending changes to the database.
@@ -32,7 +51,7 @@ async def commit() -> None:
     connection.commit()
 
 
-@router.post("/rollback", status_code=HTTP_204_NO_CONTENT)
+@router.post("/database/rollback", status_code=HTTP_204_NO_CONTENT)
 async def rollback() -> None:
     """
     Roll back pending changes to the database.
