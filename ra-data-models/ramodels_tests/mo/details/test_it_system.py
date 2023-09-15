ab81e7aafa6c2a0d420464122e98ad42be9a683d
Emil Madsen <sovende@gmail.com>
2023-09-15 13:51:07 +0200

    test(ramodels): [#57549] add validity to ramodels itsystem test

--- ra-data-models/ramodels_tests/mo/details/test_it_system.py
+++ ra-data-models/ramodels_tests/mo/details/test_it_system.py
@@ -3,12 +3,17 @@
 from hypothesis import given
 from hypothesis import strategies as st
 
+from ramodels.mo._shared import Validity
 from ramodels.mo.details.it_system import ITSystemRead
 
 
 @st.composite
 def read_strat(draw):
-    required = {"name": st.text(), "user_key": st.text()}
+    required = {
+        "name": st.text(),
+        "user_key": st.text(),
+        "validity": st.builds(Validity),
+    }
     optional = {"type": st.just("itsystem"), "system_type": st.none() | st.text()}
 
     st_dict = draw(st.fixed_dictionaries(required, optional=optional))  # type: ignore
