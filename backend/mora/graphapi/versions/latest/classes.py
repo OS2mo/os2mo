71b09be9390657f1cc4f2b59d3e216772e97cfbf
Thor Astrup Pedersen <tap@magenta-aps.dk>
2023-09-18 13:00:58 +0200

    fix: [#57548] gql create_class now checks for existing UUID in the ClassCreate input model

--- backend/mora/graphapi/versions/latest/classes.py
+++ backend/mora/graphapi/versions/latest/classes.py
@@ -7,14 +7,12 @@ from uuid import uuid4
 from .models import ClassCreate
 from .models import ClassUpdate
 from mora import lora
-from mora.util import get_uuid
 from oio_rest import db
 
 
 async def create_class(input: ClassCreate, organisation_uuid: UUID) -> UUID:
     registration = input.to_registration(organisation_uuid=organisation_uuid)
-    new_uuid = get_uuid(registration, required=False) or str(uuid4())
-
+    new_uuid = str(input.uuid) or str(uuid4())
     c = lora.Connector()
     return await c.klasse.create(registration, new_uuid)
 
