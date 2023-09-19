59de6c87e3c9f5f9f40910f774420423f48db7f5
Thor Astrup Pedersen <tap@magenta-aps.dk>
2023-09-19 15:17:50 +0200

    fix: #57548 - Swap to OpenValidity in ClassBase; org_uuid now optional
    Ex classes can have validities from infinity to infinity.
    Also to my knowledge, it is just annying having to deal with org_uuids
    when we dont really use it.. espeially on classes

--- backend/ramodels/mo/class_.py
+++ backend/ramodels/mo/class_.py
@@ -5,7 +5,7 @@ from uuid import UUID
 from pydantic import Field
 
 from ramodels.mo._shared import MOBase
-from ramodels.mo._shared import Validity
+from ramodels.mo._shared import OpenValidity
 
 
 class ClassBase(MOBase):
@@ -15,13 +15,17 @@ class ClassBase(MOBase):
 
     # type is always "class"
     type_: str = Field("class", alias="type", description="The object type")
+
+    # Required
+    validity: OpenValidity = Field(description="Validity of the facet.")
+
     # These are always Optional:
     scope: str | None = Field(description="Scope of the class.")
     published: str = Field(description="Published state of the class object.")
     parent_uuid: UUID | None = Field(description="UUID of the parent class.")
     example: str | None = Field(description="Example usage.")
     owner: UUID | None = Field(description="Owner of class")
-    validity: Validity = Field(description="Validity of the facet.")
+    org_uuid: UUID | None = Field(None, description="UUID of the related organisation.")
 
 
 class ClassRead(ClassBase):
@@ -29,7 +33,7 @@ class ClassRead(ClassBase):
 
     name: str = Field(description="Name/title of the class.")
     facet_uuid: UUID = Field(description="UUID of the related facet.")
-    org_uuid: UUID = Field(description="UUID of the related organisation.")
+    # org_uuid: UUID = Field(description="UUID of the related organisation.")
 
 
 class ClassWrite(ClassBase):
@@ -38,4 +42,4 @@ class ClassWrite(ClassBase):
 
     name: str = Field(description="Mo-class name.")
     facet_uuid: UUID | None = Field(description="UUID of the related facet.")
-    org_uuid: UUID = Field(description="UUID of the related organisation.")
+    # org_uuid: UUID = Field(description="UUID of the related organisation.")
