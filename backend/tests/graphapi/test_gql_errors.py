d23b94545b3b2c32ac6598a9c998d35549828865
Emil Madsen <sovende@gmail.com>
2023-09-19 13:21:16 +0200

    fix(graphql): [#57754] allow multiple errors in handle_gql_error
    This commit ensures that multiple concurrent GraphQL exceptions are
    handled gracefully within the `handle_gql_error` helper function.
    
    As such a proper error message should now be produced instead of the
    current:
    ```
    Expected exactly one item in iterable, but got ... and perhaps more.
    ```

---
+++ backend/tests/graphapi/test_gql_errors.py
@@ -0,0 +1,70 @@
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+from collections.abc import Callable
+
+import pytest
+from strawberry.exceptions import GraphQLError
+
+from mora.graphapi.shim import execute_graphql
+from mora.service.util import handle_gql_error
+from tests.conftest import GQLResponse
+
+
+query = """
+    query TestMultipleErrors {
+      itsystems {
+        objects {
+          current {
+            _non_existent_field_1
+            _non_existent_field_2
+          }
+        }
+      }
+    }
+"""
+
+
+@pytest.mark.integration_test
+@pytest.mark.usefixtures("load_fixture_data_with_reset")
+async def test_multiple_errors(graphapi_post: Callable) -> None:
+    """Test how multiple errors are handled."""
+
+    response: GQLResponse = graphapi_post(query)
+    assert response.errors == [
+        {
+            "locations": [{"column": 13, "line": 6}],
+            "message": "Cannot query field '_non_existent_field_1' on type 'ITSystem'.",
+        },
+        {
+            "locations": [{"column": 13, "line": 7}],
+            "message": "Cannot query field '_non_existent_field_2' on type 'ITSystem'.",
+        },
+    ]
+    assert response.data is None
+
+
+@pytest.mark.integration_test
+@pytest.mark.usefixtures("load_fixture_data_with_reset")
+async def test_handle_gql_errors() -> None:
+    """Test how handle_gql_errors handles multiple exceptions."""
+
+    response = await execute_graphql(query)
+    with pytest.raises(ExceptionGroup) as exc_info:  # noqa: F821
+        handle_gql_error(response)
+
+    assert exc_info.value.message == "GraphQL Errors"
+
+    exceptions = exc_info.value.exceptions
+    assert len(exceptions) == 2
+
+    assert isinstance(exceptions[0], GraphQLError)
+    assert (
+        exceptions[0].message
+        == "Cannot query field '_non_existent_field_1' on type 'ITSystem'."
+    )
+
+    assert isinstance(exceptions[1], GraphQLError)
+    assert (
+        exceptions[1].message
+        == "Cannot query field '_non_existent_field_2' on type 'ITSystem'."
+    )
