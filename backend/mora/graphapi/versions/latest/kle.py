d8ca510000c9cec68f9c94ee9bbe60a02680cbe2
Emil Madsen <sovende@gmail.com>
2023-11-08 17:02:39 +0100

    feat(graphql): [#58392] rework terminate kle

--- backend/mora/graphapi/versions/latest/kle.py
+++ backend/mora/graphapi/versions/latest/kle.py
@@ -5,10 +5,8 @@ from uuid import UUID
 from .models import KLECreate
 from .models import KLETerminate
 from .models import KLEUpdate
-from mora import lora
 from mora import mapping
 from mora.service.kle import KLERequestHandler
-from mora.triggers import Trigger
 
 
 async def create_kle(input: KLECreate) -> UUID:
@@ -38,26 +36,11 @@ async def update_kle(input: KLEUpdate) -> UUID:
 
 
 async def terminate_kle(input: KLETerminate) -> UUID:
-    trigger = input.get_kle_trigger()
-    trigger_dict = trigger.to_trigger_dict()
+    input_dict = input.to_handler_dict()
 
-    # ON_BEFORE
-    _ = await Trigger.run(trigger_dict)
-
-    # Do LoRa update
-    lora_conn = lora.Connector()
-    lora_result = await lora_conn.organisationfunktion.update(
-        input.get_lora_payload(), str(input.uuid)
+    request = await KLERequestHandler.construct(
+        input_dict, mapping.RequestType.TERMINATE
     )
+    await request.submit()
 
-    # ON_AFTER
-    trigger_dict.update(
-        {
-            Trigger.RESULT: lora_result,
-            Trigger.EVENT_TYPE: mapping.EventType.ON_AFTER,
-        }
-    )
-
-    _ = await Trigger.run(trigger_dict)
-
-    return UUID(lora_result)
+    return input.uuid
--- backend/mora/graphapi/versions/latest/models.py
+++ backend/mora/graphapi/versions/latest/models.py
@@ -1055,30 +1055,6 @@ class KLETerminate(ValidityTerminate):
 
     uuid: UUID = Field(description="UUID of the KLE annotation we want to terminate.")
 
-    def get_lora_payload(self) -> dict:
-        return {
-            "tilstande": {
-                "organisationfunktiongyldighed": [
-                    {"gyldighed": "Inaktiv", "virkning": self.get_termination_effect()}
-                ]
-            },
-            "note": "Afsluttet",
-        }
-
-    def get_kle_trigger(self) -> OrgFuncTrigger:
-        return OrgFuncTrigger(
-            role_type=mapping.KLE,
-            event_type=mapping.EventType.ON_BEFORE,
-            uuid=self.uuid,
-            org_unit_uuid=self.uuid,
-            request_type=mapping.RequestType.TERMINATE,
-            request=MoraTriggerRequest(
-                type=mapping.KLE,
-                uuid=self.uuid,
-                validity=Validity(from_date=self.from_date, to_date=self.to_date),
-            ),
-        )
-
 
 # Leaves
 # --------
