7f98cb6efb1622680739a16558157818531c5e78
Casper V. Kristensen <vk@magenta.dk>
2024-01-30 15:54:06 +0100

    chore: [#58757] use upstream alembic config from `alembic init -t async`
    This doesn't work, but it does provide a nicer diff.

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,13 +1,11 @@
-# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
-# SPDX-License-Identifier: MPL-2.0
+import asyncio
 from logging.config import fileConfig
 
-from alembic import context
-from alembic_helpers import register_helpers
-from oio_rest.db.engine import get_engine
-from oio_rest.db.metadata import metadata
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
-register_helpers()
+from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -15,10 +13,14 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
 
-# add your model's MetaData object here for 'autogenerate' support
-target_metadata = metadata
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -26,7 +28,7 @@ target_metadata = metadata
 # ... etc.
 
 
-def run_migrations_offline():
+def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -36,6 +38,7 @@ def run_migrations_offline():
 
     Calls to context.execute() here emit the given string to the
     script output.
+
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -49,21 +52,35 @@ def run_migrations_offline():
         context.run_migrations()
 
 
-def run_migrations_online():
-    """Run migrations in 'online' mode.
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
 
-    In this scenario we need to create an Engine
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations() -> None:
+    """In this scenario we need to create an Engine
     and associate a connection with the context.
+
     """
-    connectable = get_engine()
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            # Where to create Alembics own "alembic_version" table
-            version_table_schema="public",
-        )
-        with context.begin_transaction():
-            context.run_migrations()
+
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+
+    asyncio.run(run_async_migrations())
 
 
 if context.is_offline_mode():
