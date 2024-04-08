8268dee78983cb202329aa1e28f44ff478b98b58
Emil Madsen <sovende@gmail.com>
2024-04-08 17:55:07 +0200

    feat: [#59702] throw away ldap messages on issues
    This commit introduces a temporary change that rejects AMQP messages
    whenever a lookup for a DN fails. The proper solution would be for the
    program to gracefully handle this scenario, but a proper solution takes
    time, so for now we just throw random messages away.

--- tests/test_ldap_amqp.py
+++ tests/test_ldap_amqp.py
@@ -3,6 +3,10 @@
 from unittest.mock import AsyncMock
 from uuid import uuid4
 
+import pytest
+from fastramqpi.ramqp.utils import RejectMessage
+
+from mo_ldap_import_export.exceptions import NoObjectsReturnedException
 from mo_ldap_import_export.ldap_amqp import process_dn
 
 
@@ -12,4 +16,18 @@ async def test_process_dn() -> None:
     dn = str(uuid4())
 
     await process_dn(sync_tool, dn, _=None)
+    sync_tool.import_single_user.assert_called_once_with(dn)
+
+
+async def test_process_missing_dn() -> None:
+    """Test that process_dn calls sync_tool as expected."""
+    sync_tool = AsyncMock()
+    sync_tool.import_single_user.side_effect = NoObjectsReturnedException("BOOM")
+
+    dn = str(uuid4())
+
+    with pytest.raises(RejectMessage) as exc_info:
+        await process_dn(sync_tool, dn, _=None)
+    assert f"DN could not be found: {dn}" in str(exc_info.value)
+
     sync_tool.import_single_user.assert_called_once_with(dn)
