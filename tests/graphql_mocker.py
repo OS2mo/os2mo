46e893ea7413b3a612182c4f6e5303e48c28c934
Emil Madsen <sovende@gmail.com>
2024-04-09 23:25:57 +0200

    test: [#60440] introduce GraphQL respx-powered mocking
    This commit introduces a new testing fixture `graphql_mock` which
    behaves exactly like the `respx_mock`, except that it has an extra
    top-level function `query` that can be used to target a specific GraphQL
    query, and `result` on the produced route which can be used to mock a
    GraphQL return.
    
    Two tests have been modified to use the new mocking system as a
    proof-of-concept and it is the goal that future tests should utilize the
    new scheme to ensure mocking happens as close to the interface between
    components as possible, in this case as close to OS2mo as possible.

---
+++ tests/graphql_mocker.py
@@ -0,0 +1,40 @@
+# SPDX-FileCopyrightText: 2019-2020 Magenta ApS
+# SPDX-License-Identifier: MPL-2.0
+"""Respx helpers for mocking GraphQL endpoints."""
+from typing import Any
+
+from fastapi.encoders import jsonable_encoder
+from httpx import Response
+from respx import Route
+from respx.types import URLPatternTypes
+
+
+class GraphQLRoute(Route):
+    @property
+    def result(self):
+        return self.return_value.json()["data"]
+
+    @result.setter
+    def result(self, value) -> None:
+        # TODO: Support errors
+        self.return_value = Response(200, json={"data": jsonable_encoder(value)})
+
+
+class GraphQLMocker:
+    def __init__(self, respx_mock: Any) -> None:
+        self.respx_mock = respx_mock
+
+    def query(
+        self,
+        query_name: str,
+        url: URLPatternTypes | None = None,
+        *,
+        name: str | None = None,
+        **lookups: Any,
+    ) -> GraphQLRoute:
+        # TODO: Match more strictly on query name
+        route = GraphQLRoute(
+            method="POST", url=url, content__contains=query_name, **lookups
+        )
+        self.respx_mock.add(route, name=name)
+        return route
