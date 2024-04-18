286990264097213a5dd731b170036d939843bf63
Emil Madsen <sovende@gmail.com>
2024-04-18 22:50:47 +0200

    test: [#60178] fix missing await in test

--- tests/test_customer_specific.py
+++ tests/test_customer_specific.py
@@ -1,6 +1,5 @@
 # SPDX-FileCopyrightText: 2019-2020 Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
-import asyncio
 from datetime import datetime
 from datetime import timedelta
 from unittest.mock import AsyncMock
@@ -49,10 +48,7 @@ async def test_template(sync_tool: SyncTool):
     assert list_to_ignore == []
 
 
-async def test_import_jobtitlefromadtomo_objects(context: Context):
-    test_user_uuid = uuid4()
-    test_job_function_uuid = uuid4()
-    test_job_function_fallback_uuid = uuid4()
+async def test_import_jobtitlefromadtomo_objects(context: Context) -> None:
     test_eng_uuid = uuid4()
     start_time = datetime.now() - timedelta(minutes=10)
     end_time = datetime.now()
@@ -73,18 +69,30 @@ async def test_import_jobtitlefromadtomo_objects(context: Context):
     )
     context["graphql_client"] = graphql_client_mock
 
+    test_user_uuid = uuid4()
+    test_job_function_uuid = uuid4()
+    test_job_function_fallback_uuid = uuid4()
     test_object = JobTitleFromADToMO.from_simplified_fields(
         user_uuid=test_user_uuid,
         job_function_uuid=test_job_function_uuid,
         job_function_fallback_uuid=test_job_function_fallback_uuid,
     )
 
-    await asyncio.gather(test_object.sync_to_ldap())
+    await test_object.sync_to_ldap()
 
+    graphql_client_mock.set_job_title.assert_not_called()
     job = one(await test_object.sync_to_mo(context))
 
     assert job["uuid_to_ignore"] == test_eng_uuid
     graphql_client_mock.set_job_title.assert_called_once_with(
+        job_function=test_job_function_uuid,
+        uuid=test_eng_uuid,
+        **{"from": start_time, "to": end_time},
+    )
+
+    graphql_client_mock.set_job_title.assert_not_awaited()
+    await job["task"]
+    graphql_client_mock.set_job_title.assert_awaited_once_with(
         job_function=test_job_function_uuid,
         uuid=test_eng_uuid,
         **{"from": start_time, "to": end_time},
