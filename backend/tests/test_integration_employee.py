5ba32f88a29fae7069560511eaae6224f529f951
Casper V. Kristensen <vk@magenta.dk>
2024-04-24 17:16:50 +0200

    test: [#60000]: test_edit_remove_seniority

--- backend/tests/test_integration_employee.py
+++ backend/tests/test_integration_employee.py
@@ -9,6 +9,7 @@ from fastapi.testclient import TestClient
 from more_itertools import one
 
 from . import util
+from .conftest import AnotherTransaction
 from mora import lora
 from mora.config import Settings
 from tests.cases import assert_registrations_equal
@@ -158,22 +159,22 @@ async def test_edit_employee_overwrite(service_client: TestClient) -> None:
 @pytest.mark.integration_test
 @pytest.mark.usefixtures("fixture_db")
 @freezegun.freeze_time("2017-01-01", tz_offset=1)
-async def test_edit_remove_seniority(service_client: TestClient) -> None:
+async def test_edit_remove_seniority(
+    service_client: TestClient, another_transaction: AnotherTransaction
+) -> None:
     # A generic example of editing an employee
 
     userid = "6ee24785-ee9a-4502-81c2-7697009c9053"
-#   # NOTE: Commenting this in, makes the check on line 203 fail
-#   #       I think it is related to some kind of database transaction control?
-#    c = lora.Connector(virkningfra="-infinity", virkningtil="infinity")
-#    actual = await c.bruger.get(userid)
-#    assert actual is not None
-#
-#    actual_seniorities = [
-#        x.get("seniority", None)
-#        for x in actual["attributter"]["brugerudvidelser"]
-#    ]
-#    assert actual_seniorities == [None]
-#
+    async with another_transaction():
+        c = lora.Connector(virkningfra="-infinity", virkningtil="infinity")
+        actual = await c.bruger.get(userid)
+        assert actual is not None
+
+        actual_seniorities = [
+            x.get("seniority", None) for x in actual["attributter"]["brugerudvidelser"]
+        ]
+        assert actual_seniorities == [None]
+
     req = [
         {
             "type": "employee",
@@ -193,15 +194,15 @@ async def test_edit_remove_seniority(service_client: TestClient) -> None:
     assert response.status_code == 200
     assert response.json() == [userid]
 
-    c = lora.Connector(virkningfra="-infinity", virkningtil="infinity")
-    actual = await c.bruger.get(userid)
-    assert actual is not None
+    async with another_transaction():
+        c = lora.Connector(virkningfra="-infinity", virkningtil="infinity")
+        actual = await c.bruger.get(userid)
+        assert actual is not None
 
-    actual_seniorities = [
-        x.get("seniority", None)
-        for x in actual["attributter"]["brugerudvidelser"]
-    ]
-    assert actual_seniorities == ["2017-01-01", None]
+        actual_seniorities = [
+            x.get("seniority", None) for x in actual["attributter"]["brugerudvidelser"]
+        ]
+        assert actual_seniorities == ["2017-01-01", None]
 
     req = [
         {
@@ -221,16 +222,19 @@ async def test_edit_remove_seniority(service_client: TestClient) -> None:
     assert response.status_code == 200
     assert response.json() == [userid]
 
-    c = lora.Connector(virkningfra="-infinity", virkningtil="infinity")
-    actual = await c.bruger.get(userid)
-    assert actual is not None
+    async with another_transaction():
+        c = lora.Connector(virkningfra="-infinity", virkningtil="infinity")
+        actual = await c.bruger.get(userid)
+        assert actual is not None
 
-    actual_seniorities = sorted([
-        x.get("seniority", None)
-        for x in actual["attributter"]["brugerudvidelser"]
-    ], key=(lambda x: ("" if (x is None) else x))
-    )
-    assert actual_seniorities == [None, None, "2017-01-01"]
+        actual_seniorities = sorted(
+            [
+                x.get("seniority", None)
+                for x in actual["attributter"]["brugerudvidelser"]
+            ],
+            key=(lambda x: ("" if (x is None) else x)),
+        )
+        assert actual_seniorities == [None, None, "2017-01-01"]
 
 
 @pytest.mark.integration_test
