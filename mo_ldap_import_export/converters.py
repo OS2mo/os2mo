d8672e5f31a786cb87ff35aa028cb04f89e0b485
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2024-04-24 14:20:18 +0200

    fix: fix finding class name by class uuid

--- mo_ldap_import_export/converters.py
+++ mo_ldap_import_export/converters.py
@@ -107,16 +107,16 @@ get_job_function_uuid = partial(
 )
 
 
-async def get_engagement_type_name(graphql_client: GraphQLClient, uuid: str) -> str:
-    result = await graphql_client.read_class_name_by_class_uuid(UUID(uuid))
+async def get_engagement_type_name(graphql_client: GraphQLClient, uuid: UUID) -> str:
+    result = await graphql_client.read_class_name_by_class_uuid(uuid)
     engagement_type = one(result.objects)
     if engagement_type.current is None:
         raise NoObjectsReturnedException(f"engagement_type not active, uuid: {uuid}")
     return engagement_type.current.name
 
 
-async def get_job_function_name(graphql_client: GraphQLClient, uuid: str) -> str:
-    result = await graphql_client.read_class_name_by_class_uuid(UUID(uuid))
+async def get_job_function_name(graphql_client: GraphQLClient, uuid: UUID) -> str:
+    result = await graphql_client.read_class_name_by_class_uuid(uuid)
     job_function = one(result.objects)
     if job_function.current is None:
         raise NoObjectsReturnedException(f"job_function not active, uuid: {uuid}")
