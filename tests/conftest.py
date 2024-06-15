80a4be3992dcb350cad213cc7f100e5b98ba4b88
Emil Madsen <sovende@gmail.com>
2024-06-15 15:41:46 +0200

    chore: [#60126] remove dead find_dn_by_engagement_uuid
    This commit removes the `find_dn_by_engagement_uuid` method and its
    corresponding test-cases, the method has become deadcode as a result of
    prior changes, but has been unintentionally left behind.

--- tests/conftest.py
+++ tests/conftest.py
@@ -213,7 +213,6 @@ def dataloader(
     dataloader.load_ldap_attribute_values = AsyncMock()
     dataloader.modify_ldap_object.return_value = [{"description": "success"}]
     dataloader.get_ldap_objectGUID = sync_dataloader
-    dataloader.find_dn_by_engagement_uuid.return_value = "CN=foo"
 
     dataloader.load_ldap_OUs = AsyncMock()
     dataloader.move_ldap_object = AsyncMock()
