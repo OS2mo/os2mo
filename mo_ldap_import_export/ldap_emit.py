c87aa3129b384db7820a3ece7ba1ffd09872a6f2
Emil Madsen <sovende@gmail.com>
2024-06-16 18:24:55 +0200

    feat: [#61188] introduce LDAP UUID handler
    This commit introduced an LDAP `UUID` handler, which functions similarly
    to the old LDAP DN handler, by calling the `import_single_user` handler
    after converting the UUID to a DN.
    
    The old LDAP DN handler has been converted to simply emit LDAP `UUID`
    events, and is planned to be removed once event emitters are all
    emitting `UUID`s instead of DNs. That change will be coming in a future
    commit, this simply lays the groundwork for it.

--- mo_ldap_import_export/ldap_emit.py
+++ mo_ldap_import_export/ldap_emit.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2019-2020 Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
 import asyncio
+from uuid import UUID
 
 import structlog
 from fastramqpi.ramqp import AMQPSystem
@@ -10,9 +11,20 @@ from .types import DN
 logger = structlog.stdlib.get_logger()
 
 
+# TODO: Eliminate this and process_dn
 async def publish_dns(
     ldap_amqpsystem: AMQPSystem,
     dns: list[DN],
 ) -> None:
     logger.info("Registered change for LDAP object(s)", dns=dns)
     await asyncio.gather(*[ldap_amqpsystem.publish_message("dn", dn) for dn in dns])
+
+
+async def publish_uuid(
+    ldap_amqpsystem: AMQPSystem,
+    uuids: list[UUID],
+) -> None:
+    logger.info("Registered change for LDAP object(s)", uuids=uuids)
+    await asyncio.gather(
+        *[ldap_amqpsystem.publish_message("uuid", uuid) for uuid in uuids]
+    )
