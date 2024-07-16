1a546145237a6a12aacae3c316b25c531119cf11
Emil Madsen <emil@magenta.dk>
2024-07-16 18:24:01 +0200

    refactor: [#59500] generalize get or creating facet classes
    This commit generalize `get_or_create_engagement_type_uuid` and
    `get_or_create_job_function_uuid` into a single
    `_get_or_create_facet_class` function.

--- mo_ldap_import_export/converters.py
+++ mo_ldap_import_export/converters.py
@@ -288,41 +288,36 @@ async def _create_facet_class(
     )
 
 
-create_engagement_type = partial(_create_facet_class, facet_user_key="engagement_type")
-create_job_function = partial(
-    _create_facet_class, facet_user_key="engagement_job_function"
-)
-
-
-async def get_or_create_engagement_type_uuid(
-    dataloader: DataLoader, engagement_type: str
+async def _get_or_create_facet_class(
+    dataloader: DataLoader,
+    class_user_key: str,
+    facet_user_key: str,
+    default: str | None = None,
 ) -> str:
-    if not engagement_type:
-        raise UUIDNotFoundException("engagement_type is empty")
+    if not class_user_key:
+        if default is None:
+            raise UUIDNotFoundException("Cannot create class without user-key")
+        logger.info("class_user_key is empty, using provided default", default=default)
+        class_user_key = default
     try:
-        return await get_engagement_type_uuid(
-            dataloader.graphql_client, engagement_type
+        return await _get_facet_class_uuid(
+            dataloader.graphql_client,
+            class_user_key=class_user_key,
+            facet_user_key=facet_user_key,
         )
     except UUIDNotFoundException:
-        uuid = await create_engagement_type(dataloader, engagement_type)
+        uuid = await _create_facet_class(
+            dataloader, class_user_key=class_user_key, facet_user_key=facet_user_key
+        )
         return str(uuid)
 
 
-async def get_or_create_job_function_uuid(
-    dataloader: DataLoader, job_function: str, default: str | None = None
-) -> str:
-    if not job_function:
-        if default is None:
-            raise UUIDNotFoundException("job_function is empty")
-
-        logger.info("job_function is empty, using provided default", default=default)
-        job_function = default
-
-    try:
-        return await get_job_function_uuid(dataloader.graphql_client, job_function)
-    except UUIDNotFoundException:
-        uuid = await create_job_function(dataloader, job_function)
-        return str(uuid)
+get_or_create_engagement_type_uuid = partial(
+    _get_or_create_facet_class, facet_user_key="engagement_type"
+)
+get_or_create_job_function_uuid = partial(
+    _get_or_create_facet_class, facet_user_key="engagement_job_function"
+)
 
 
 async def get_itsystem_user_keys(graphql_client: GraphQLClient) -> set[str]:
