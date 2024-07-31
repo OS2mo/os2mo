0de57ff3f69f808da79aef16766885e514a03e36
Emil Madsen <emil@magenta.dk>
2024-07-31 15:49:15 +0200

    refactor: [#56970] move gmodels.lora.Organisation to GraphQL
    This commit moves the `Organisation` model from `gmodels.lora` to GraphQL
    as it is the only consumer of the model. Moving it thus simplifies the
    gmodels module and makes it simpler to understand what is being used where.

--- backend/mora/graphapi/gmodels/lora/__init__.py
+++ backend/mora/graphapi/gmodels/lora/__init__.py
@@ -1,10 +1,8 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 from ._shared import LoraBase
-from .organisation import Organisation
 
 
 __all__ = [
     "LoraBase",
-    "Organisation",
 ]
--- backend/mora/graphapi/versions/latest/org.py
+++ backend/mora/graphapi/versions/latest/org.py
@@ -7,8 +7,9 @@ from uuid import UUID
 
 import strawberry
 
+from .orgmodel import Organisation
 from mora import common
-from mora.graphapi.gmodels.lora.organisation import Organisation
+
 
 logger = logging.getLogger(__name__)
 
