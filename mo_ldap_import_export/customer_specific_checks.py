289d46fd4d643c820a29a711bc06713b5dd04f69
Emil <emil@magenta.dk>
2024-08-05 17:22:50 +0200

    chore(exportchecks): [#55956] remove dead alleroed check code
    This commit removes an unused customer specific export-check for Alleroed.
    
    It is our belief that this old export check has been superseeded by
    checking for the existence of a given IT-User account instead, and thus
    that it can be removed without issue.
    
    The feature flag to enable it was defaulting to false, and no
    configuration was setting it to true.

--- mo_ldap_import_export/customer_specific_checks.py
+++ mo_ldap_import_export/customer_specific_checks.py
@@ -6,7 +6,6 @@ import structlog
 from fastramqpi.context import Context
 from fastramqpi.depends import UserContext
 
-from .converters import LdapConverter
 from .converters import get_it_system_uuid
 from .dataloaders import DataLoader
 from .exceptions import IgnoreChanges
@@ -22,44 +21,8 @@ class ExportChecks:
     """
 
     def __init__(self, context: Context):
-        self.context = context
-        self.user_context: UserContext = self.context["user_context"]
-        self.dataloader: DataLoader = self.user_context["dataloader"]
-        self.converter: LdapConverter = self.user_context["converter"]
-
-    async def check_alleroed_sd_number(self, employee_uuid: UUID, object_uuid: UUID):
-        """
-        Checks if an SD-employee number starts with 9 and aborts if so.
-
-        These numbers belong to 'timelønnede' in Allerød Kommune, and should not be
-        exported to AD.
-
-        Notes
-        -------
-        For Allerød, SD-Numbers are stored inside Engagements on the user_key attribute
-        """
-
-        error_message = (
-            f"SD-number for employee with uuid = '{employee_uuid}' starts with '9'."
-        )
-
-        engagements = await self.dataloader.load_mo_employee_engagements(employee_uuid)
-
-        sd_numbers = [e.user_key for e in engagements]
-        hourly_paid_worker = [sd_number.startswith("9") for sd_number in sd_numbers]
-        engagement_dict = {e.uuid: e.user_key for e in engagements}
-
-        if not hourly_paid_worker:
-            # Nothing to do if there are no engagements
-            return
-        elif all(hourly_paid_worker):
-            # If this is an hourly paid worker, ignore him
-            raise IgnoreChanges(error_message)
-        elif any(hourly_paid_worker):
-            # If the hourly paid worker also has other engagements;
-            # Only ignore the specific engagement which states that he is hourly paid
-            if engagement_dict.get(object_uuid, "").startswith("9"):
-                raise IgnoreChanges(error_message)
+        user_context: UserContext = context["user_context"]
+        self.dataloader: DataLoader = user_context["dataloader"]
 
     async def check_it_user(self, employee_uuid: UUID, it_system_user_key: str):
         if not it_system_user_key:
