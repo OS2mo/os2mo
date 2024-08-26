719768af42c9a7428323e75812f0f468d54954dd
Emil Madsen <emil@magenta.dk>
2024-08-26 15:46:52 +0200

    refactor: [#59372] add lazy property for converter
    This commit adds a lazy property for `LdapConverter` such that the
    methods inside of `DataLoader` do not to depend on the entire
    `user_context`.
    
    This commit refactors 4 cases where the code would extract the
    `converter` directly from the context to instead using a class
    attribute in which it is bound.

--- mo_ldap_import_export/dataloaders.py
+++ mo_ldap_import_export/dataloaders.py
@@ -179,11 +179,17 @@ class DataLoader:
         return cast(SyncTool, self.user_context["sync_tool"])
 
     @property
+    def converter(self):
+        from .converters import LdapConverter
+
+        return cast(LdapConverter, self.user_context["converter"])
+
+    @property
     def mo_to_ldap_attributes(self):
         """
         Returns a list of all LDAP attribute names which are synchronized to LDAP.
         """
-        converter = self.user_context["converter"]
+        converter = self.converter
 
         mo_to_ldap_attributes = []
         for json_dict in converter.mapping["mo_to_ldap"].values():
@@ -270,7 +276,7 @@ class DataLoader:
         search_bases = [
             combine_dn_strings([ou, search_base]) for ou in ous_to_search_in
         ]
-        converter = self.user_context["converter"]
+        converter = self.converter
 
         object_class = converter.find_ldap_object_class(json_key)
         attributes = converter.get_ldap_attributes(json_key) + additional_attributes
@@ -457,7 +463,7 @@ class DataLoader:
         _, result = await ldap_add(
             self.ldap_connection,
             dn,
-            self.user_context["converter"].find_ldap_object_class("Employee"),
+            self.converter.find_ldap_object_class("Employee"),
             attributes=attributes,
         )
         logger.info("LDAP Result", result=result, dn=dn)
@@ -597,7 +603,7 @@ class DataLoader:
         delete: bool
             Set to True to delete contents in LDAP, instead of creating/modifying them
         """
-        converter = self.user_context["converter"]
+        converter = self.converter
         if not converter._export_to_ldap_(json_key):
             logger.info("_export_to_ldap_ == False.", json_key=json_key)
             return []
