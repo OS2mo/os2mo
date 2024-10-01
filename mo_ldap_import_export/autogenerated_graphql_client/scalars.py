5f1a5699744b492d82dc4820cca4ee401113146e
Casper V. Kristensen <casper@vkristensen.dk>
2024-10-01 14:08:09 +0200

    fix: [#59851] graphql datetime timezone

--- mo_ldap_import_export/autogenerated_graphql_client/scalars.py
+++ mo_ldap_import_export/autogenerated_graphql_client/scalars.py
@@ -1,5 +1,10 @@
 from collections.abc import Callable
+from datetime import datetime
 from typing import Any
 
-SCALARS_PARSE_FUNCTIONS: dict[Any, Callable[[Any], Any]] = {}
+from fastramqpi.ariadne import parse_graphql_datetime
+
+SCALARS_PARSE_FUNCTIONS: dict[Any, Callable[[Any], Any]] = {
+    datetime: parse_graphql_datetime
+}
 SCALARS_SERIALIZE_FUNCTIONS: dict[Any, Callable[[Any], Any]] = {}
