d038ac6292487493eea7715ceb6b511767eb6e76
Emil Madsen <emil@magenta.dk>
2024-10-16 14:37:27 +0200

    test(mo2ldap): [#59372] introduce org-unit address integration test
    This commit adapts the `test_org_unit_address.py` test-file to testing
    org-unit addresses instead of employee addresses.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -138,3 +138,58 @@ async def mo_org_unit(graphql_client: GraphQLClient) -> UUID:
         )
     )
     return r.uuid
+
+
+@pytest.fixture
+async def ansat(graphql_client: GraphQLClient) -> UUID:
+    return one(
+        (
+            await graphql_client.read_class_uuid_by_facet_and_class_user_key(
+                "engagement_type", "Ansat"
+            )
+        ).objects
+    ).uuid
+
+
+@pytest.fixture
+async def jurist(graphql_client: GraphQLClient) -> UUID:
+    return one(
+        (
+            await graphql_client.read_class_uuid_by_facet_and_class_user_key(
+                "engagement_job_function", "Jurist"
+            )
+        ).objects
+    ).uuid
+
+
+@pytest.fixture
+async def primary(graphql_client: GraphQLClient) -> UUID:
+    return one(
+        (
+            await graphql_client.read_class_uuid_by_facet_and_class_user_key(
+                "primary_type", "primary"
+            )
+        ).objects
+    ).uuid
+
+
+@pytest.fixture
+async def email_unit(graphql_client: GraphQLClient) -> UUID:
+    return one(
+        (
+            await graphql_client.read_class_uuid_by_facet_and_class_user_key(
+                "org_unit_address_type", "EmailUnit"
+            )
+        ).objects
+    ).uuid
+
+
+@pytest.fixture
+async def public(graphql_client: GraphQLClient) -> UUID:
+    return one(
+        (
+            await graphql_client.read_class_uuid_by_facet_and_class_user_key(
+                "visibility", "Public"
+            )
+        ).objects
+    ).uuid
