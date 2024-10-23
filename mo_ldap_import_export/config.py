eb2adba8afff3e420cfaa2f718beaae444351c51
Casper V. Kristensen <casper@vkristensen.dk>
2024-10-23 17:15:42 +0200

    chore: [#59372] align copyright

--- mo_ldap_import_export/config.py
+++ mo_ldap_import_export/config.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2019-2020 Magenta ApS
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 # pylint: disable=too-few-public-methods
 """Settings handling."""
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2019-2020 Magenta ApS
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 # pylint: disable=redefined-outer-name
 # pylint: disable=unused-argument
