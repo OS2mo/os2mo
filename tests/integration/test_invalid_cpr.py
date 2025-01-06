ec9e861fc1cebeef5df03a493426f11bb72143b6
Emil Madsen <emil@magenta.dk>
2025-01-06 15:36:51 +0100

    test: [#63443] reproduce `dn2cpr` issue
    This commit introduces a test-case verifying a production issue related
    to extracting the CPR numbers from LDAP accounts.
    
    The issue arises when no CPR number is found on the LDAP account.
    The expected behavior in this situation would be that the `dn2cpr`
    function returns `None`, however it currently produces an exception:
    ```
    ValueError: too few items in iterable (expected 1)
    ```
    As it tries to extract the CPR number from the list value returned from
    LDAP.

--- tests/integration/test_invalid_cpr.py
+++ tests/integration/test_invalid_cpr.py
@@ -6,9 +6,13 @@ import pytest
 from fastramqpi.context import Context
 
 from mo_ldap_import_export.dataloaders import DataLoader
+from mo_ldap_import_export.depends import Connection
 from mo_ldap_import_export.exceptions import InvalidCPR
+from mo_ldap_import_export.ldap import ldap_modify
+from mo_ldap_import_export.ldapapi import LDAPAPI
 from mo_ldap_import_export.moapi import MOAPI
 from mo_ldap_import_export.types import CPRNumber
+from mo_ldap_import_export.utils import combine_dn_strings
 
 
 @pytest.mark.integration_test
@@ -35,3 +39,38 @@ async def test_cpr2uuids(
     with pytest.raises(InvalidCPR) as exc_info:
         await moapi.cpr2uuids(CPRNumber("9999999999"))
     assert "Unable to lookup invalid CPR number" in str(exc_info.value)
+
+
+@pytest.mark.xfail(reason="Reproducing production issue")
+@pytest.mark.integration_test
+@pytest.mark.envvar(
+    {
+        "LISTEN_TO_CHANGES_IN_MO": "False",
+        "LISTEN_TO_CHANGES_IN_LDAP": "False",
+    }
+)
+@pytest.mark.usefixtures("test_client")
+async def test_dn2cpr(
+    context: Context,
+    ldap_connection: Connection,
+    ldap_person: list[str],
+) -> None:
+    person_dn = combine_dn_strings(ldap_person)
+
+    dataloader: DataLoader = context["user_context"]["dataloader"]
+    ldapapi: LDAPAPI = dataloader.ldapapi
+
+    result = await ldapapi.dn2cpr(person_dn)
+    assert result == "2108613133"
+
+    # Clear the CPR field, after which we expect None to be returned
+    await ldap_modify(
+        ldap_connection,
+        dn=person_dn,
+        changes={
+            "employeeNumber": [("MODIFY_REPLACE", [])],
+        },
+    )
+
+    result = await ldapapi.dn2cpr(person_dn)
+    assert result is None
