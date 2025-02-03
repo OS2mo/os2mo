ffa76f7a5df11e34c75375929cf1740e44bd2761
Casper V. Kristensen <casper@vkristensen.dk>
2025-02-03 17:04:05 +0100

    test: [#63471] add simple graphql versioning tests

--- backend/tests/graphapi/test_versioning.py
+++ backend/tests/graphapi/test_versioning.py
@@ -1,19 +1,28 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
-from mora.graphapi.main import graphql_versions
-from mora.graphapi.main import latest_graphql_version
+import pytest
+from mora.graphapi.version import LATEST_VERSION
+from mora.graphapi.version import Version
 from starlette.testclient import TestClient
+
+
+def test_version_enum_incomparable_to_int() -> None:
+    with pytest.raises(
+        TypeError,
+        match="'>' not supported between instances of 'Version' and 'int'",
+    ):
+        assert LATEST_VERSION > 1
 
 
 def test_unversioned_get_redirects_to_latest(raw_client: TestClient) -> None:
     response = raw_client.get("/graphql", follow_redirects=False)
     assert response.is_redirect
-    assert response.headers["location"] == f"/graphql/v{latest_graphql_version}"
+    assert response.headers["location"] == f"/graphql/v{LATEST_VERSION.value}"
 
 
 def test_non_existent(raw_client: TestClient) -> None:
-    # Non-existent versions are NOT FOUND
+    # Non-existent versions are not found
     assert raw_client.get("/graphql/v1").status_code == 404
     # Active versions are found
-    for version in graphql_versions:
-        assert raw_client.get(f"/graphql/v{version}").status_code == 200
+    for version in Version:
+        assert raw_client.get(f"/graphql/v{version.value}").status_code == 200
