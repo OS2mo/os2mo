7866123af0708a64595d89c44842e46d0b6ada88
Casper V. Kristensen <casper@vkristensen.dk>
2025-02-03 16:46:23 +0100

    refactor: [#63471] associate graphql schema with version

--- backend/mora/graphapi/custom_schema.py
+++ backend/mora/graphapi/custom_schema.py
@@ -1,16 +1,23 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 import traceback
+from typing import Any
 
 from graphql import GraphQLError
 from strawberry import Schema
+from strawberry.schema import BaseSchema
 from strawberry.types import ExecutionContext
 
 from mora import config
+from mora.graphapi.version import Version
 from mora.log import canonical_gql_context
 
 
 class CustomSchema(Schema):
+    def __init__(self, version: Version, *args: Any, **kwargs: Any) -> None:
+        self.version = version
+        super().__init__(*args, **kwargs)
+
     def process_errors(
         self,
         errors: list[GraphQLError],
@@ -25,3 +32,11 @@ class CustomSchema(Schema):
             # Pretty-print exceptions in development
             for exception in exceptions:
                 print(exception, end="")
+
+
+def get_version(schema: BaseSchema) -> Version:
+    """Get Schema version."""
+    # Strawberry is not generic in its schema, so callers will have a
+    # strawberry.Schema or strawberry.BaseSchema type.
+    assert isinstance(schema, CustomSchema)
+    return schema.version
