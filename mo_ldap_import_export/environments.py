d23683ecf6d0da2361f310dc7d8f745346a37314
Emil <emil@magenta.dk>
2025-04-09 18:49:28 +0200

    feat(environment): [#64659] add get_itsystem_uuid

--- mo_ldap_import_export/environments.py
+++ mo_ldap_import_export/environments.py
@@ -495,6 +495,15 @@ async def get_ituser_uuid(
     return obj.uuid if obj else None
 
 
+async def get_itsystem_uuid(
+    graphql_client: GraphQLClient, filter: dict[str, Any]
+) -> UUID | None:
+    itsystem_filter = parse_obj_as(ITSystemFilter, filter)
+    result = await graphql_client.read_itsystem_uuid(itsystem_filter)
+    obj = only(result.objects)
+    return obj.uuid if obj else None
+
+
 async def get_engagement_uuid(
     graphql_client: GraphQLClient, filter: dict[str, Any]
 ) -> UUID | None:
@@ -649,6 +658,7 @@ def construct_globals_dict(
         "get_person_uuid": partial(get_person_uuid, graphql_client),
         "get_address_uuid": partial(get_address_uuid, graphql_client),
         "get_ituser_uuid": partial(get_ituser_uuid, graphql_client),
+        "get_itsystem_uuid": partial(get_itsystem_uuid, graphql_client),
         "get_engagement_uuid": partial(get_engagement_uuid, graphql_client),
         "get_org_unit_uuid": partial(get_org_unit_uuid, graphql_client),
         "get_employment_interval": partial(get_employment_interval, graphql_client),
