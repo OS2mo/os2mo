4fc9b553c7d57069d4f7c9af3867aa26a9794df3
Emil Madsen <emil@magenta.dk>
2025-06-17 11:35:47 +0200

    test(graphql): [#65186] extract fixtures from test_itsystem_roles
    This commit extracts various fixtures from `test_itsystem_roles` such
    that they can be reused for a test introduced in a future commit while
    keeping the current test equivalent with its state before the
    extraction.

--- backend/tests/graphapi/test_itsystem_roles.py
+++ backend/tests/graphapi/test_itsystem_roles.py
@@ -1,5 +1,7 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
+from collections.abc import Callable
+from typing import Any
 from uuid import UUID
 
 import pytest
@@ -8,78 +10,125 @@ from more_itertools import one
 from ..conftest import GraphAPIPost
 
 
-@pytest.mark.integration_test
-@pytest.mark.usefixtures("fixture_db")
-def test_itsystem_roles(graphapi_post: GraphAPIPost) -> None:
-    """Test that we can create and read itsystem roles."""
-    # Read the role facet UUID
-    facet_uuid_query = """
-        query ReadFacetUUID($user_key: String!) {
-            facets(filter: {user_keys: [$user_key]}) {
-                objects {
+@pytest.fixture
+def read_facet_uuid(graphapi_post: GraphAPIPost) -> Callable[[str], UUID]:
+    def inner(user_key: str) -> UUID:
+        facet_uuid_query = """
+            query ReadFacetUUID($user_key: String!) {
+                facets(filter: {user_keys: [$user_key]}) {
+                    objects {
+                        uuid
+                    }
+                }
+            }
+        """
+        response = graphapi_post(facet_uuid_query, {"user_key": user_key})
+        assert response.errors is None
+        assert response.data
+        facet_uuid = one(response.data["facets"]["objects"])["uuid"]
+        return facet_uuid
+
+    return inner
+
+
+@pytest.fixture
+def create_itsystem(graphapi_post: GraphAPIPost) -> Callable[[dict[str, Any]], UUID]:
+    def inner(input: dict[str, Any]) -> UUID:
+        itsystem_create_mutation = """
+            mutation CreateITSystem($input: ITSystemCreateInput!) {
+                itsystem_create(input: $input) {
                     uuid
                 }
             }
-        }
-    """
-    response = graphapi_post(facet_uuid_query, {"user_key": "role"})
-    assert response.errors is None
-    assert response.data
-    role_facet_uuid = one(response.data["facets"]["objects"])["uuid"]
+        """
+        response = graphapi_post(itsystem_create_mutation, {"input": input})
+        assert response.errors is None
+        assert response.data
+        itsystem_uuid = UUID(response.data["itsystem_create"]["uuid"])
+        return itsystem_uuid
 
-    # Create new itsystem
-    itsystem_create_mutation = """
-        mutation CreateITSystem($input: ITSystemCreateInput!) {
-            itsystem_create(input: $input) {
-                uuid
-            }
-        }
-    """
-    itsystem_uuid = UUID("624e4f57-0a36-4751-9982-51ef37457ebc")
-    itsystem_user_key = "suila"
-    itsystem_name = "Suila-tapit"
-    response = graphapi_post(
-        itsystem_create_mutation,
-        {
-            "input": {
-                "uuid": str(itsystem_uuid),
-                "user_key": itsystem_user_key,
-                "name": itsystem_name,
-                "validity": {"from": "2024-01-01"},
-            }
-        },
-    )
-    assert response.errors is None
-    assert response.data
-    new_uuid = UUID(response.data["itsystem_create"]["uuid"])
-    assert new_uuid == itsystem_uuid
+    return inner
 
-    # Read the new itsystem along with its (non-existent) roles
-    query = """
-        query ReadITSystem($uuid: UUID!) {
-            itsystems(filter: {uuids: [$uuid]}) {
-                objects {
+
+@pytest.fixture
+def create_class(graphapi_post: GraphAPIPost) -> Callable[[dict[str, Any]], UUID]:
+    def inner(input: dict[str, Any]) -> UUID:
+        class_create_mutation = """
+            mutation CreateRole($input: ClassCreateInput!) {
+                class_create(input: $input) {
                     uuid
-                    current {
-                        user_key
-                        name
-                        roles {
-                            uuid
-                            current {
-                                user_key
-                                name
+                }
+            }
+        """
+        response = graphapi_post(class_create_mutation, {"input": input})
+        assert response.errors is None
+        assert response.data
+        class_uuid = UUID(response.data["class_create"]["uuid"])
+        return class_uuid
+
+    return inner
+
+
+@pytest.fixture
+def read_itsystem(graphapi_post: GraphAPIPost) -> Callable[[UUID], dict[str, Any]]:
+    def inner(uuid: UUID) -> dict[str, Any]:
+        query = """
+            query ReadITSystem($uuid: UUID!) {
+                itsystems(filter: {uuids: [$uuid]}) {
+                    objects {
+                        uuid
+                        current {
+                            user_key
+                            name
+                            roles {
+                                uuid
+                                current {
+                                    user_key
+                                    name
+                                }
                             }
                         }
                     }
                 }
             }
+        """
+        response = graphapi_post(query, {"uuid": str(uuid)})
+        assert response.errors is None
+        assert response.data
+        itsystem = one(response.data["itsystems"]["objects"])
+        return itsystem
+
+    return inner
+
+
+@pytest.mark.integration_test
+@pytest.mark.usefixtures("fixture_db")
+def test_itsystem_roles(
+    read_facet_uuid: Callable[[str], UUID],
+    read_itsystem: Callable[[UUID], dict[str, Any]],
+    create_class: Callable[[dict[str, Any]], UUID],
+    create_itsystem: Callable[[dict[str, Any]], UUID],
+) -> None:
+    """Test that we can create and read itsystem roles."""
+    # Read the role facet UUID
+    role_facet_uuid = read_facet_uuid("role")
+
+    # Create new itsystem
+    itsystem_uuid = UUID("624e4f57-0a36-4751-9982-51ef37457ebc")
+    itsystem_user_key = "suila"
+    itsystem_name = "Suila-tapit"
+    new_uuid = create_itsystem(
+        {
+            "uuid": str(itsystem_uuid),
+            "user_key": itsystem_user_key,
+            "name": itsystem_name,
+            "validity": {"from": "2024-01-01"},
         }
-    """
-    response = graphapi_post(query, {"uuid": str(itsystem_uuid)})
-    assert response.errors is None
-    assert response.data
-    itsystem = one(response.data["itsystems"]["objects"])
-    assert itsystem == {
+    )
+    assert new_uuid == itsystem_uuid
+
+    # Read the new itsystem along with its (non-existent) roles
+    assert read_itsystem(itsystem_uuid) == {
         "uuid": str(itsystem_uuid),
         "current": {
             "user_key": itsystem_user_key,
@@ -89,60 +138,23 @@ def test_itsystem_roles(graphapi_post: GraphAPIPost) -> None:
     }
 
     # Create a rolebinding role
-    class_create_mutation = """
-        mutation CreateRole($input: ClassCreateInput!) {
-            class_create(input: $input) {
-                uuid
-            }
-        }
-    """
     class_uuid = UUID("64cf30fb-8819-4be2-9f62-39fc6cb9b169")
     class_user_key = "admin"
     class_name = "Administrator"
-    response = graphapi_post(
-        class_create_mutation,
+    new_uuid = create_class(
         {
-            "input": {
-                "uuid": str(class_uuid),
-                "user_key": class_user_key,
-                "name": class_name,
-                "facet_uuid": str(role_facet_uuid),
-                "it_system_uuid": str(itsystem_uuid),
-                "validity": {"from": "2024-01-01"},
-            }
-        },
+            "uuid": str(class_uuid),
+            "user_key": class_user_key,
+            "name": class_name,
+            "facet_uuid": str(role_facet_uuid),
+            "it_system_uuid": str(itsystem_uuid),
+            "validity": {"from": "2024-01-01"},
+        }
     )
-    assert response.errors is None
-    assert response.data
-    new_uuid = UUID(response.data["class_create"]["uuid"])
     assert new_uuid == class_uuid
 
     # Read the itsystem again along with its now existing role
-    query = """
-        query ReadITSystem($uuid: UUID!) {
-            itsystems(filter: {uuids: [$uuid]}) {
-                objects {
-                    uuid
-                    current {
-                        user_key
-                        name
-                        roles {
-                            uuid
-                            current {
-                                user_key
-                                name
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    """
-    response = graphapi_post(query, {"uuid": str(itsystem_uuid)})
-    assert response.errors is None
-    assert response.data
-    itsystem = one(response.data["itsystems"]["objects"])
-    assert itsystem == {
+    assert read_itsystem(itsystem_uuid) == {
         "uuid": str(itsystem_uuid),
         "current": {
             "user_key": itsystem_user_key,
