365119e911b2cae40f2a9ce432dec854aeb0f30f
Emil <emil@magenta.dk>
2025-07-03 17:32:09 +0200

    test(auth): [#64963] parametrize the `test_create_facet` test

--- backend/tests/graphapi/test_auth_userref.py
+++ backend/tests/graphapi/test_auth_userref.py
@@ -3,20 +3,49 @@
 from uuid import UUID
 
 import pytest
+from mora.auth.middleware import LORA_USER_UUID
 from mora.db import AsyncSession
 from mora.db import FacetRegistrering
+from mora.mapping import ADMIN
 from sqlalchemy import select
 
 from tests.conftest import GQLResponse
 from tests.conftest import GraphAPIPost
-from tests.conftest import fake_auth
+from tests.conftest import SetAuth
 
 
 @pytest.mark.integration_test
+@pytest.mark.parametrize(
+    "token_uuid, actor_uuid",
+    [
+        # Random token UUIDs -> those UUIDs
+        (
+            UUID("a2f13305-e49b-416a-b31c-8d80b24b269b"),
+            UUID("a2f13305-e49b-416a-b31c-8d80b24b269b"),
+        ),
+        (
+            UUID("6998caa6-aee7-4b03-889f-216c2b2cbc62"),
+            UUID("6998caa6-aee7-4b03-889f-216c2b2cbc62"),
+        ),
+        # No UUID -> Faceless
+        (
+            None,
+            LORA_USER_UUID,
+        ),
+    ],
+)
 async def test_create_facet(
-    empty_db: AsyncSession, root_org: UUID, graphapi_post: GraphAPIPost
+    empty_db: AsyncSession,
+    root_org: UUID,
+    graphapi_post: GraphAPIPost,
+    set_auth: SetAuth,
+    token_uuid: UUID | None,
+    actor_uuid: UUID,
 ) -> None:
     """Integrationtest for testing user references in LoRa."""
+    # Change our token user to the specified UUID
+    set_auth(ADMIN, token_uuid)
+
     payload = {
         "user_key": "TestFacet",
         "validity": {"from": "2012-03-04", "to": None},
@@ -41,5 +70,4 @@ async def test_create_facet(
         )
     )
 
-    user_ref = await fake_auth()
-    assert brugerref == user_ref.uuid
+    assert brugerref == actor_uuid
