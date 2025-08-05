ee79e9aeb4f17fcce9580d4cd63696078537bece
Emil Madsen <emil@magenta.dk>
2025-08-05 20:51:19 +0200

    docs(cli): [#65829] document authentication scheme limitations
    This commit documents the limitations with regards to authentication
    schemes when generating the ldapsearch commands based on the LDAP
    integration configuration, in particular why we can only generate the
    ldapsearch command for simple authentication and not NTLM.

--- mo_ldap_import_export/cli.py
+++ mo_ldap_import_export/cli.py
@@ -17,7 +17,47 @@ def generate_ldapsearch_command() -> list[str]:
     command = ["ldapsearch"]
     # Output in LDIF format without comments and version
     command.append("-LLL")
-    # Configure program for SIMPLE auth
+    # LDAP authentication schemes are specified by RFC4513, with Simple Bind being
+    # specified in section 5.1(.3) and SASL in section 5.2, while being further detailed
+    # in RFC4422.
+    #
+    # Currently the LDAP integration does not support SASL, and as such the only
+    # standardized way to connect to an LDAP server with the integration is Simple Bind.
+    #
+    # However the integration does support connecting using NTLM, a proprietary
+    # Microsoft authentication mechanism, not specified by any RFC or similar standard.
+    # NTLM can be used in two ways; As an authentication backend in SASL and via the
+    # standard conflicting NTLM-over-Simple-Bind scheme.
+    #
+    # The integration only supports NTLM-over-Simple-Bind, which works by doing two
+    # subsequent Simple Bind calls, sending an NTLM Negotiate message as in password
+    # field during the first Simple Bind call, expecting the Active Directory server
+    # to detect this payload and respond with an LDAP_INVALID_CREDENTIALS error
+    # containing the NTLM Challenge in the error message field, upon which the client
+    # can answer the challenge and send a new Simple Bind call with the NTLM
+    # Authenticate message as the password.
+    #
+    # Unfortunately this NTLM-over-Simple-Bind behavior is not supported by ldapsearch,
+    # as it is entirely non-standard, and as such we cannot generate a command for it.
+    #
+    # In theory we could generate a command for NTLM over SASL, however this
+    # authentication mechanism is disabled by default for being unsafe as NTLM itself is
+    # considered unsafe.
+    # Why NTLM is disabled by default, while NTLM-over-Simple-Bind is not when they are
+    # functionally equivalent is a question that only the engineers at Microsoft may
+    # answer, but it is likely for backwards compatability.
+    #
+    # An attempt was also made to do authentication via DIGEST-MD5 over SASL using
+    # ldapsearch as an alternative to NTLM as Active Directory reports DIGEST-MD5 as a
+    # valid authentication scheme, however using DIGEST-MD5 always results in an
+    # LDAP_INVALID_CREDENTIALS error even if the provided credentails are valid.
+    # Why DIGEST-MD5 is included in the list of authentication schemes when it is
+    # functionally disabled by default is a question that only the engineers at
+    # Microsoft may answer, but it is likely for backwards compatability.
+    #
+    # It might be possible to extend this code to support Kerberos over SASL in the
+    # future, however for now, we can only generate the ldapsearch command if the server
+    # allows standard compliant Simple Bind.
     assert (
         settings.ldap_auth_method == AuthBackendEnum.SIMPLE
     ), f"Can only generate script for {AuthBackendEnum.SIMPLE}"
