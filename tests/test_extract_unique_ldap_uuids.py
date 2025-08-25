f3d74117d8058a96f67e85e4275e15bc02e96d3e
Emil Madsen <emil@magenta.dk>
2025-08-25 18:38:14 +0200

    test: [#66087] split test_extract_unique_ldap_uuids in multiple
    This commit splits the `test_extract_unique_ldap_uuids` test in multiple.
    One test showing the happy-path behavior, one showing an error when a
    single bad user-key is encounter, one showing multiple errors when
    multiple bad user-keys are encounter, and two which shows the behavior
    when a mix of good and bad user-keys are provided.
    
    Additionally a few tests concerning duplicate entries were added to
    cement the behavior of duplicate user-keys.

--- tests/test_extract_unique_ldap_uuids.py
+++ tests/test_extract_unique_ldap_uuids.py
@@ -12,34 +12,123 @@ from more_itertools import one
 from mo_ldap_import_export.dataloaders import extract_unique_ldap_uuids
 from mo_ldap_import_export.models import ITUser
 from mo_ldap_import_export.types import LDAPUUID
+from tests.integration.conftest import AnyOrder
+
+
+def create_ituser(user_key: str) -> ITUser:
+    return ITUser(
+        user_key=user_key,
+        itsystem=uuid4(),
+        person=uuid4(),
+        validity={"start": datetime.datetime.today()},
+    )
 
 
 def test_extract_unique_ldap_uuids() -> None:
     ldap_uuid_1 = str(uuid4())
     ldap_uuid_2 = str(uuid4())
-    it_user_1 = ITUser(
-        user_key=ldap_uuid_1,
-        itsystem=uuid4(),
-        person=uuid4(),
-        validity={"start": datetime.datetime.today()},
-    )
-    it_user_2 = ITUser(
-        user_key=ldap_uuid_2,
-        itsystem=uuid4(),
-        person=uuid4(),
-        validity={"start": datetime.datetime.today()},
-    )
+    it_user_1 = create_ituser(ldap_uuid_1)
+    it_user_2 = create_ituser(ldap_uuid_2)
 
     ldap_uuids = extract_unique_ldap_uuids([it_user_1, it_user_2])
     assert ldap_uuids == {LDAPUUID(ldap_uuid_1), LDAPUUID(ldap_uuid_2)}
 
-    it_user_3 = ITUser(
-        user_key="not_an_uuid",
-        itsystem=uuid4(),
-        person=uuid4(),
-        validity={"start": datetime.datetime.today()},
-    )
+
+def test_extract_unique_ldap_uuids_non_uuid() -> None:
+    it_user = create_ituser("not_an_uuid")
     with pytest.raises(ExceptionGroup) as exc_info:
-        extract_unique_ldap_uuids([it_user_1, it_user_2, it_user_3])
+        extract_unique_ldap_uuids([it_user])
     exception = one(exc_info.value.exceptions)
     assert str(exception) == "Non UUID IT-user user-key: not_an_uuid"
+
+
+def test_extract_unique_ldap_uuids_multiple_non_uuids() -> None:
+    it_user_1 = create_ituser("not_an_uuid_1")
+    it_user_2 = create_ituser("not_an_uuid_2")
+    with pytest.raises(ExceptionGroup) as exc_info:
+        extract_unique_ldap_uuids([it_user_1, it_user_2])
+    exception_errors = [str(exception) for exception in exc_info.value.exceptions]
+    assert AnyOrder(exception_errors) == [
+        "Non UUID IT-user user-key: not_an_uuid_1",
+        "Non UUID IT-user user-key: not_an_uuid_2",
+    ]
+
+
+def test_extract_unique_ldap_uuids_mixed() -> None:
+    ldap_uuid_1 = str(uuid4())
+    it_user_1 = create_ituser(ldap_uuid_1)
+    it_user_2 = create_ituser("not_an_uuid")
+    with pytest.raises(ExceptionGroup) as exc_info:
+        extract_unique_ldap_uuids([it_user_1, it_user_2])
+    exception = one(exc_info.value.exceptions)
+    assert str(exception) == "Non UUID IT-user user-key: not_an_uuid"
+
+
+def test_extract_unique_ldap_uuids_multiple_mixed() -> None:
+    ldap_uuid_1 = str(uuid4())
+    ldap_uuid_2 = str(uuid4())
+    it_user_1 = create_ituser(ldap_uuid_1)
+    it_user_2 = create_ituser(ldap_uuid_2)
+    it_user_3 = create_ituser("not_an_uuid_1")
+    it_user_4 = create_ituser("not_an_uuid_2")
+    with pytest.raises(ExceptionGroup) as exc_info:
+        extract_unique_ldap_uuids([it_user_1, it_user_2, it_user_3, it_user_4])
+    exception_errors = [str(exception) for exception in exc_info.value.exceptions]
+    assert AnyOrder(exception_errors) == [
+        "Non UUID IT-user user-key: not_an_uuid_1",
+        "Non UUID IT-user user-key: not_an_uuid_2",
+    ]
+
+
+def test_extract_unique_ldap_uuids_duplicate_non_uuids() -> None:
+    it_user_1 = create_ituser("not_an_uuid")
+    it_user_2 = create_ituser("not_an_uuid")
+    with pytest.raises(ExceptionGroup) as exc_info:
+        extract_unique_ldap_uuids([it_user_1, it_user_2])
+    exception_errors = [str(exception) for exception in exc_info.value.exceptions]
+    assert exception_errors == ["Non UUID IT-user user-key: not_an_uuid"]
+
+
+def test_extract_unique_ldap_uuids_duplicate_mixed() -> None:
+    ldap_uuid = str(uuid4())
+    it_user_1 = create_ituser(ldap_uuid)
+    it_user_2 = create_ituser("not_an_uuid")
+    it_user_3 = create_ituser("not_an_uuid")
+    with pytest.raises(ExceptionGroup) as exc_info:
+        extract_unique_ldap_uuids([it_user_1, it_user_2, it_user_3])
+    exception_errors = [str(exception) for exception in exc_info.value.exceptions]
+    assert exception_errors == ["Non UUID IT-user user-key: not_an_uuid"]
+
+
+def test_extract_unique_ldap_uuids_duplicate_uuid() -> None:
+    ldap_uuid = str(uuid4())
+    it_user_1 = create_ituser(ldap_uuid)
+    it_user_2 = create_ituser(ldap_uuid)
+
+    ldap_uuids = extract_unique_ldap_uuids([it_user_1, it_user_2])
+    assert ldap_uuids == {LDAPUUID(ldap_uuid)}
+
+
+def test_extract_unique_ldap_uuids_duplicate_uuids_mixed() -> None:
+    ldap_uuid_1 = str(uuid4())
+    ldap_uuid_2 = str(uuid4())
+    it_user_11 = create_ituser(ldap_uuid_1)
+    it_user_12 = create_ituser(ldap_uuid_1)
+    it_user_2 = create_ituser(ldap_uuid_2)
+
+    ldap_uuids = extract_unique_ldap_uuids([it_user_11, it_user_12, it_user_2])
+    assert ldap_uuids == {LDAPUUID(ldap_uuid_1), LDAPUUID(ldap_uuid_2)}
+
+
+def test_extract_unique_ldap_uuids_duplicate_uuids() -> None:
+    ldap_uuid_1 = str(uuid4())
+    ldap_uuid_2 = str(uuid4())
+    it_user_11 = create_ituser(ldap_uuid_1)
+    it_user_12 = create_ituser(ldap_uuid_1)
+    it_user_21 = create_ituser(ldap_uuid_2)
+    it_user_22 = create_ituser(ldap_uuid_2)
+
+    ldap_uuids = extract_unique_ldap_uuids(
+        [it_user_11, it_user_12, it_user_21, it_user_22]
+    )
+    assert ldap_uuids == {LDAPUUID(ldap_uuid_1), LDAPUUID(ldap_uuid_2)}
