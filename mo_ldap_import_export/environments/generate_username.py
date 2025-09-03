41e35fb325907e7a671ba478f4c596f046d2fffb
Emil Madsen <emil@magenta.dk>
2025-09-03 20:50:25 +0200

    refactor(usernames): [#66058] eliminate max_iterations code
    The refactoring made in the prior commit made it so that each iteration
    of the while loop would add atleast one character per iteration.
    This means that we need to do at most 2 iterations of the while-loop
    removing the prior upper-bound set by the `max_iterations` variable at
    1000.
    
    This is a big step towards making the code more predictable and allowing
    further simplifications.

--- mo_ldap_import_export/environments/generate_username.py
+++ mo_ldap_import_export/environments/generate_username.py
@@ -408,9 +408,6 @@ async def generate_username(
 
 
 def _extract_letters(name: list[str]) -> list[str]:
-    length = 3
-    max_iterations = 1000
-
     # Convert ["Firstname", "Last Name"] -> ["Firstname", "Last", "Name"]
     # and ["First-Name", "Last-Name"] -> ["First", "Name", "Last", "Name"]
     name = flatten(map(partial(re.split, r"[\-\s+]"), name))  # type: ignore
@@ -445,8 +442,8 @@ def _extract_letters(name: list[str]) -> list[str]:
     # a consonant.)
     result = [first_ascii_letter]
 
-    iterations = 0
-    while len(result) < length:
+    length = 3
+    for _ in range(2):
         for part in consonant_name[1:]:
             for letter in part:
                 result.append(letter)
@@ -460,11 +457,7 @@ def _extract_letters(name: list[str]) -> list[str]:
             if len(result) >= length:
                 return result
 
-        iterations += 1
-        if iterations > max_iterations:
-            raise ValueError(f"cannot create username for input {name!r}")
-
-    return result
+    raise ValueError(f"cannot create username for input {name!r}")
 
 
 class UserNameGenPermutation:
