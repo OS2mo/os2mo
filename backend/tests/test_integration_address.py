11392b7ddb5bf41d93a88a666085f645f8c188ea
Emil <emil@magenta.dk>
2025-09-11 14:45:26 +0200

    test(graphql): [#65188] ensure that address relations can be cleared
    This commit expands the tests for ituser relation on addresses,
    added to MO in version 36.6.0 (!2369, b6d94c0c), to include a test for
    clearing the ituser relation.

--- backend/tests/test_integration_address.py
+++ backend/tests/test_integration_address.py
@@ -1550,7 +1550,7 @@ def test_address_ituser(graphapi_post: GraphAPIPost) -> None:
     }
     """
     EDIT_ADDRESS_ITUSER = """
-    mutation EditAddress($uuid: UUID!, $engagement: UUID!, $ituser: UUID!) {
+    mutation EditAddress($uuid: UUID!, $engagement: UUID, $ituser: UUID) {
       address_update(
         input: {
           uuid: $uuid
@@ -1666,6 +1666,32 @@ def test_address_ituser(graphapi_post: GraphAPIPost) -> None:
         "engagement": [engagement],
         "ituser_uuid": ituser2_uuid,
         "ituser": [ituser2],
+    }
+
+    # Remove ituser and engagement connections
+    response = graphapi_post(
+        EDIT_ADDRESS_ITUSER,
+        variables={
+            "uuid": address_uuid,
+            "engagement": None,
+            "ituser": None,
+        },
+    )
+    assert response.errors is None
+
+    # Verify that the edit was successful
+    response = graphapi_post(
+        GET_ADDRESS,
+        variables={
+            "uuid": address_uuid,
+        },
+    )
+    assert response.errors is None
+    assert one(response.data["addresses"]["objects"])["current"] == {
+        "uuid": address_uuid,
+        "user_key": edited_user_key,
+        "engagement": [],
+        "ituser": [],
     }
 
 
