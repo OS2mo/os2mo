fdaa3d35038a762f2c284fe8ec9e055fd9df5dff
Emil Madsen <emil@magenta.dk>
2025-11-19 13:00:35 +0100

    refactor(graphql): [#65303] add loaders.org to CustomInfo
    This commit adds a `loaders` container and its first typed loader `org`
    to the `CustomInfo` which allows for well-typed access to the strawberry
    context instead of having untyped dict access.

--- backend/mora/graphapi/info.py
+++ backend/mora/graphapi/info.py
@@ -6,9 +6,23 @@ from typing import Self
 from strawberry.types import Info
 
 from mora.auth.keycloak.models import Token
+from mora.graphapi.gmodels.mo.organisation import OrganisationRead
+
+
+class LoaderInfo:
+    def __init__(self: Self, info: Info) -> None:
+        self.info = info
+
+    @property
+    def org(self: Self) -> Awaitable[OrganisationRead]:
+        return self.info.context["org_loader"].load(0)
 
 
 class CustomInfo(Info):
+    @property
+    def loaders(self) -> LoaderInfo:
+        return LoaderInfo(self)
+
     @property
     def token(self: Self) -> Awaitable[Token]:
         return self.context["get_token"]()
