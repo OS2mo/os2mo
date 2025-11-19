2a61255cbc4b838a4c87e20af93180f1d8425ba8
Emil Madsen <emil@magenta.dk>
2025-11-19 04:56:45 +0100

    feat(graphql): [#65303] introduce rolebinding responses
    This commit deprecates all relation fields on the `RoleBinding` collection
    in GraphQL in favor of new relation fields that return `Response`
    objects instead.

--- backend/mora/graphapi/versions/latest/schema.py
+++ backend/mora/graphapi/versions/latest/schema.py
@@ -5216,6 +5216,22 @@ class RoleBinding:
     # TODO: Document this
     user_key: str = strawberry.auto
 
+    role_response: Response[LazyClass] = strawberry.field(  # type: ignore
+        resolver=lambda root: Response[ClassRead](uuid=root.role),
+        description=dedent(
+            """\
+            The role that is being fulfilled.
+
+            Examples of user-keys:
+            * `"AD Read"`
+            * `"AD Write"`
+            * `"SAP Admin"`
+            """
+        )
+        + list_to_optional_field_warning,
+        permission_classes=[IsAuthenticatedPermission, gen_read_permission("class")],
+    )
+
     role: list[LazyClass] = strawberry.field(
         resolver=to_list(
             seed_resolver(class_resolver, {"uuids": lambda root: [root.role]})
@@ -5232,6 +5248,14 @@ class RoleBinding:
         )
         + list_to_optional_field_warning,
         permission_classes=[IsAuthenticatedPermission, gen_read_permission("class")],
+        deprecation_reason="Use 'role_response' instead. Will be removed in a future version of OS2mo.",
+    )
+
+    ituser_response: Response[LazyITUser] = strawberry.field(  # type: ignore
+        resolver=lambda root: Response[ITUserRead](uuid=root.it_user_uuid),
+        description="The IT-user that should be granted this role\n"
+        + list_to_optional_field_warning,
+        permission_classes=[IsAuthenticatedPermission, gen_read_permission("ituser")],
     )
 
     ituser: list[LazyITUser] = strawberry.field(
@@ -5243,6 +5267,20 @@ class RoleBinding:
         description="The IT-user that should be granted this role\n"
         + list_to_optional_field_warning,
         permission_classes=[IsAuthenticatedPermission, gen_read_permission("ituser")],
+        deprecation_reason="Use 'ituser_response' instead. Will be removed in a future version of OS2mo.",
+    )
+
+    org_unit_response: Response[LazyOrganisationUnit] | None = strawberry.field(  # type: ignore
+        resolver=lambda root: Response[OrganisationUnitRead](uuid=root.org_unit_uuid)
+        if root.org_unit_uuid
+        else None,
+        description=dedent(
+            """\
+            The organisational unit in which the role is being fulfilled.
+            """
+        )
+        + list_to_optional_field_warning,
+        permission_classes=[IsAuthenticatedPermission, gen_read_permission("org_unit")],
     )
 
     org_unit: list[LazyOrganisationUnit] = strawberry.field(
@@ -5259,6 +5297,7 @@ class RoleBinding:
         )
         + list_to_optional_field_warning,
         permission_classes=[IsAuthenticatedPermission, gen_read_permission("org_unit")],
+        deprecation_reason="Use 'org_unit_response' instead. Will be removed in a future version of OS2mo.",
     )
 
     validity: Validity = strawberry.auto
