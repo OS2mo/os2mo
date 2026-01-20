e47aa4ffb11960148321b57c1def7a665cd85c07
Emil Madsen <githubpublicemail.goldmine822@passinbox.com>
2026-01-20 20:08:04 +0100

    refactor(graphql): [#67163] move RegistrationBase to registrationbase.py

---
+++ backend/mora/graphapi/versions/latest/registrationbase.py
@@ -0,0 +1,103 @@
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+from datetime import datetime
+from textwrap import dedent
+from uuid import UUID
+
+import strawberry
+from strawberry.types import Info
+
+from .actor import Actor
+from .actor import actor_uuid_to_actor
+
+
+@strawberry.interface(description="Common fields for registrations.")
+class RegistrationBase:
+    registration_id: int = strawberry.field(
+        description=dedent(
+            """\
+        Internal registration ID for the registration.
+        """
+        ),
+        deprecation_reason=dedent(
+            """\
+            May be removed in the future once the bitemporal scheme is finished.
+            """
+        ),
+    )
+
+    start: datetime = strawberry.field(
+        description=dedent(
+            """\
+        Start of the bitemporal interval.
+
+        Examples:
+        * `"1970-01-01T00:00:00.000000+00:00"`
+        * `"2019-12-18T12:55:15.348614+00:00"`
+        """
+        )
+    )
+    end: datetime | None = strawberry.field(
+        description=dedent(
+            """\
+        End of the bitemporal interval.
+
+        `null` indicates the open interval, aka. infinity.
+
+        Examples:
+        * `"1970-01-01T00:00:00.000000+00:00"`
+        * `"2019-12-18T12:55:15.348614+00:00"`
+        * `null`
+        """
+        )
+    )
+
+    actor: UUID = strawberry.field(
+        description=dedent(
+            """\
+        UUID of the actor (integration or user) who changed the data.
+
+        Note:
+        Currently mostly returns `"42c432e8-9c4a-11e6-9f62-873cf34a735f"`.
+        Will eventually contain for the UUID of the integration or user who mutated data, based on the JWT token.
+        """
+        ),
+        deprecation_reason="Use actor_object.",
+    )
+
+    @strawberry.field(
+        description=dedent(
+            """\
+            Object for the actor (integration or user) who changed the data.
+            """
+        )
+    )
+    def actor_object(self, root: "RegistrationBase", info: Info) -> Actor:
+        return actor_uuid_to_actor(root.actor)
+
+    # Name of the entity model
+    model: str = strawberry.field(
+        description=dedent(
+            """\
+        Model of the modified entity.
+
+        Examples:
+        * `"class"`
+        * `"employee"`
+        * `"org_unit"`
+        """
+        )
+    )
+
+    # UUID of the modified entity
+    uuid: UUID = strawberry.field(
+        description=dedent(
+            """\
+        UUID of the modified entity.
+        """
+        )
+    )
+
+    note: str | None = strawberry.field(
+        description="Note associated with the registration."
+    )
