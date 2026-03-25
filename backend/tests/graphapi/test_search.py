OS2mo/os2mo

commit f50098069520dbbc389865873431e985d462e8fa

refactor: [#67457] in-line org-unit search

backend/mora/graphapi/versions/latest/resolvers.py
@@ -26,8 +26,10 @@ from sqlalchemy import cast
 from sqlalchemy import distinct
 from sqlalchemy import exists
 from sqlalchemy import func
+from sqlalchemy import or_
 from sqlalchemy import select
 from sqlalchemy.sql.functions import now as SQLNOW
+from sqlalchemy.types import Text
 from starlette_context import context
 from strawberry import UNSET
 from strawberry.dataloader import DataLoader
@@ -57,7 +59,6 @@ from mora.graphapi.gmodels.mo.details import ManagerRead
 from mora.graphapi.gmodels.mo.details import OwnerRead
 from mora.graphapi.gmodels.mo.details import RelatedUnitRead
 from mora.service.autocomplete.employees import search_employees
-from mora.service.autocomplete.orgunits import search_orgunits_query
 
 from ...custom_schema import get_version
 from ...middleware import with_graphql_dates
@@ -1040,11 +1041,27 @@ async def organisation_unit_resolver_query(
 
     # Query search
     if filter.query:
-        # TODO: do this in-line instead...
+        search_phrase = util.query_to_search_phrase(filter.query)
         query = query.where(
-            OrganisationEnhedRegistrering.organisationenhed_id.in_(
-                search_orgunits_query(filter.query)
-            )
+            or_(
+                cast(OrganisationEnhedRegistrering.organisationenhed_id, Text)
+                == filter.query,
+                OrganisationEnhedRegistrering.id.in_(
+                    select(
+                        OrganisationEnhedAttrEgenskaber.organisationenhed_registrering_id
+                    ).where(
+                        or_(
+                            OrganisationEnhedAttrEgenskaber.brugervendtnoegle.ilike(
+                                search_phrase
+                            ),
+                            OrganisationEnhedAttrEgenskaber.enhedsnavn.ilike(
+                                search_phrase
+                            ),
+                        ),
+                        _virkning(OrganisationEnhedAttrEgenskaber),
+                    )
+                ),
+            ),
         )
 
     # Pagination. Must be done here since the generic_resolver (lora) does not support

backend/mora/service/autocomplete/orgunits.py
@@ -1,90 +1,0 @@
-# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
-# SPDX-License-Identifier: MPL-2.0
-from datetime import date
-
-from mora import util
-from mora.db import OrganisationEnhedAttrEgenskaber
-from mora.db import OrganisationEnhedRegistrering
-from mora.db._common import LivscyklusKode
-from mora.service.autocomplete.shared import UUID_SEARCH_MIN_PHRASE_LENGTH
-from mora.service.autocomplete.shared import get_at_date_sql
-from sqlalchemy import Select
-from sqlalchemy import Text
-from sqlalchemy import Tuple
-from sqlalchemy import cast
-from sqlalchemy.sql import func
-from sqlalchemy.sql import select
-from sqlalchemy.sql import union
-
-
-def search_orgunits_query(
-    query: str,
-    at: date | None = None,
-) -> Select[Tuple]:
-    at_sql, _ = get_at_date_sql(at)
-    return _sqlalchemy_generate_query(query, at_sql)
-
-
-def _sqlalchemy_generate_query(query: str, at_sql: str) -> Select[Tuple]:
-    selects = [
-        select(cte.c.uuid)
-        for cte in (
-            _get_cte_orgunit_uuid_hits(query, at_sql),
-            _get_cte_orgunit_name_hits(query, at_sql),
-        )
-    ]
-    all_hits = union(*selects).cte()
-
-    query_final = (
-        select(
-            OrganisationEnhedRegistrering.organisationenhed_id.label("uuid"),
-        )
-        .where(OrganisationEnhedRegistrering.organisationenhed_id == all_hits.c.uuid)
-        .group_by(OrganisationEnhedRegistrering.organisationenhed_id)
-    )
-
-    return query_final
-
-
-def _get_cte_orgunit_uuid_hits(query: str, at_sql: str):
-    search_phrase = util.query_to_search_phrase(query)
-    return (
-        select(OrganisationEnhedRegistrering.organisationenhed_id.label("uuid"))
-        .join(
-            OrganisationEnhedAttrEgenskaber,
-            OrganisationEnhedAttrEgenskaber.organisationenhed_registrering_id
-            == OrganisationEnhedRegistrering.id,
-        )
-        .where(
-            OrganisationEnhedRegistrering.lifecycle != cast("Slettet", LivscyklusKode),
-            OrganisationEnhedRegistrering.registrering_period.contains(func.now()),
-            func.char_length(search_phrase) > UUID_SEARCH_MIN_PHRASE_LENGTH,
-            OrganisationEnhedRegistrering.organisationenhed_id != None,  # noqa: E711
-            cast(OrganisationEnhedRegistrering.organisationenhed_id, Text).ilike(
-                search_phrase
-            ),
-        )
-        .cte()
-    )
-
-
-def _get_cte_orgunit_name_hits(query: str, at_sql: str):
-    search_phrase = util.query_to_search_phrase(query)
-    return (
-        select(OrganisationEnhedRegistrering.organisationenhed_id.label("uuid"))
-        .join(
-            OrganisationEnhedAttrEgenskaber,
-            OrganisationEnhedAttrEgenskaber.organisationenhed_registrering_id
-            == OrganisationEnhedRegistrering.id,
-        )
-        .where(
-            OrganisationEnhedRegistrering.lifecycle != cast("Slettet", LivscyklusKode),
-            OrganisationEnhedRegistrering.registrering_period.contains(func.now()),
-            OrganisationEnhedRegistrering.organisationenhed_id != None,  # noqa: E711
-            (
-                OrganisationEnhedAttrEgenskaber.enhedsnavn.ilike(search_phrase)
-                | OrganisationEnhedAttrEgenskaber.brugervendtnoegle.ilike(search_phrase)
-            ),
-        )
-        .cte()
-    )

commit 790e42731a1e62ef87db88db65aa51807e1e14e3

feat: [#69400] improve org search for uuids
For a while, it did not let you search case insensitively or by a part
of a UUID. Regressed in f50098069520d.

backend/mora/graphapi/versions/latest/resolvers.py
@@ -49,6 +49,7 @@ from mora.db import OrganisationEnhedTilsGyldighed
 from mora.graphapi.gmodels.base import tz_isodate
 from mora.graphapi.gmodels.mo.details import EngagementRead
 from mora.service.autocomplete.employees import search_employees
+from mora.service.autocomplete.shared import UUID_SEARCH_MIN_PHRASE_LENGTH
 
 from ...context import MOInfo
 from ...custom_schema import get_version
@@ -1047,8 +1048,6 @@ async def organisation_unit_resolver_query(
         search_phrase = util.query_to_search_phrase(filter.query)
 
         clauses = [
-            cast(OrganisationEnhedRegistrering.organisationenhed_id, Text)
-            == filter.query,
             OrganisationEnhedRegistrering.id.in_(
                 select(
                     OrganisationEnhedAttrEgenskaber.organisationenhed_registrering_id
@@ -1063,6 +1062,13 @@ async def organisation_unit_resolver_query(
                 )
             ),
         ]
+
+        if len(filter.query) > UUID_SEARCH_MIN_PHRASE_LENGTH:
+            clauses.append(
+                cast(OrganisationEnhedRegistrering.organisationenhed_id, Text).ilike(
+                    search_phrase
+                )
+            )
 
         query = query.where(or_(*clauses))
 

backend/tests/graphapi/test_search.py
@@ -170,3 +170,44 @@ async def test_employee_query_must_be_used_alone(graphapi_post: GraphAPIPost) ->
             "path": ["employees"],
         }
     ]
+
+
+@pytest.mark.integration_test
+@pytest.mark.usefixtures("empty_db")
+async def test_org_unit_search_uuid(
+    graphapi_post: GraphAPIPost, create_org_unit: Callable[..., UUID]
+) -> None:
+    ou = str(create_org_unit("random org unit"))
+
+    def search(q: str) -> dict:
+        query = """
+          query Search($query: String) {
+            org_units(filter: {query: $query}) {
+              objects {
+                current {
+                  uuid
+                }
+              }
+            }
+          }
+        """
+        response = graphapi_post(query, {"query": q})
+        assert response.errors is None
+        assert response.data is not None
+        return response.data["org_units"]
+
+    response = search(ou)
+    assert response["objects"] == [{"current": {"uuid": ou}}]
+
+    response = search(ou.upper())
+    assert response["objects"] == [{"current": {"uuid": ou}}]
+
+    response = search(ou[:10])
+    assert response["objects"] == [{"current": {"uuid": ou}}]
+
+    response = search(ou[-10:])
+    assert response["objects"] == [{"current": {"uuid": ou}}]
+
+    # Too short to search for UUIDs.
+    response = search(ou[:7])
+    assert response["objects"] == []