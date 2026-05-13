7d773f6ca2cd15474009fc2b2172fb7f05befc40
Casper V. Kristensen <casper@vkristensen.dk>
2026-05-13 12:29:20 +0000

    chore: [#69932] copy `engagement_resolver[_query]`

--- mora/graphapi/resolvers.py
+++ mora/graphapi/resolvers.py
@@ -60,7 +60,6 @@ from mora.graphapi.version import Version
 from mora.service.autocomplete.employees import search_employees
 from mora.service.autocomplete.shared import UUID_SEARCH_MIN_PHRASE_LENGTH
 
-from .filters import AddressFilter
 from .filters import AssociationFilter
 from .filters import BaseFilter
 from .filters import ClassFilter
@@ -319,75 +318,201 @@ async def class_resolver(
     return classes
 
 
-async def address_resolver(
+async def address_resolver_query(
     info: MOInfo,
-    filter: AddressFilter | None = None,
+    filter: EngagementFilter,
     limit: LimitType = None,
     cursor: CursorType = None,
-) -> Any:
-    """Resolve addresses."""
-
-    async def _get_address_type_uuids(
-        info: MOInfo, filter: AddressFilter
-    ) -> list[UUID]:
-        class_filter = filter.address_type or ClassFilter()
-        # Handle deprecated filter
-        extend_uuids(class_filter, filter.address_types)
-        extend_user_keys(class_filter, filter.address_type_user_keys)
-        return lora_filter(await filter2uuids_func(class_resolver, info, class_filter))
-
-    if filter is None:
-        filter = AddressFilter()
+) -> Select:
+    # TODO: this function should not be an awaitable
 
     await registration_filter(info, filter)
 
-    kwargs: dict[str, Any] = {"gyldighed": "Aktiv"}
+    def _funktionsnavn() -> ColumnElement:
+        return OrganisationFunktionRegistrering.id.in_(
+            select(
+                OrganisationFunktionAttrEgenskaber.organisationfunktion_registrering_id
+            ).where(
+                OrganisationFunktionAttrEgenskaber.funktionsnavn == "Engagement",
+                _get_virkning_clause(OrganisationFunktionAttrEgenskaber, filter),
+            )
+        )
+
+    query = (
+        select(
+            distinct(OrganisationFunktionRegistrering.organisationfunktion_id),
+        )
+        .where(
+            _get_registrering_clause(
+                OrganisationFunktionRegistrering,
+                _get_registration_time(filter, cursor),
+            ),
+            _get_gyldighed_clause(
+                OrganisationFunktionRegistrering,
+                OrganisationFunktionTilsGyldighed,
+                filter,
+            ),
+            _funktionsnavn(),
+        )
+        .order_by(
+            OrganisationFunktionRegistrering.organisationfunktion_id,
+        )
+    )
+
+    # UUIDs
+    if filter.uuids is not None:
+        query = query.where(
+            OrganisationFunktionRegistrering.organisationfunktion_id.in_(filter.uuids)
+        )
+
+    # User keys
+    if filter.user_keys is not None:
+        query = query.where(
+            OrganisationFunktionRegistrering.id.in_(
+                select(
+                    OrganisationFunktionAttrEgenskaber.organisationfunktion_registrering_id
+                ).where(
+                    OrganisationFunktionAttrEgenskaber.brugervendtnoegle.in_(
+                        filter.user_keys
+                    ),
+                    _get_virkning_clause(OrganisationFunktionAttrEgenskaber, filter),
+                )
+            )
+        )
+
+    # Employees
     if (
         filter.employee is not None and filter.employee is not UNSET
     ) or filter.employees is not None:
-        kwargs["tilknyttedebrugere"] = lora_filter(
-            await get_employee_uuids(info, filter)
+        employee_uuids = await get_employee_uuids(info, filter)
+        query = query.where(
+            OrganisationFunktionRegistrering.id.in_(
+                select(
+                    OrganisationFunktionRelation.organisationfunktion_registrering_id
+                ).where(
+                    OrganisationFunktionRelation.rel_type
+                    == OrganisationFunktionRelationKode.tilknyttedebrugere,
+                    OrganisationFunktionRelation.rel_maal_uuid.in_(employee_uuids),
+                    _get_virkning_clause(OrganisationFunktionRelation, filter),
+                )
+            )
         )
+
+    # Org units
     if filter.org_units is not None or filter.org_unit is not None:
-        kwargs["tilknyttedeenheder"] = lora_filter(
-            await get_org_unit_uuids(info, filter)
-        )
-    if (
-        filter.address_types is not None
-        or filter.address_type_user_keys is not None
-        or filter.address_type is not None
-    ):
-        kwargs["organisatoriskfunktionstype"] = await _get_address_type_uuids(
-            info, filter
-        )
-    if filter.visibility is not None:
-        class_filter = filter.visibility or ClassFilter()
-        # rel_type "opgaver" with objekt_type "synlighed" in mox
-        # TODO: Support finding entries with visibility=None
-        kwargs["opgaver"] = lora_filter(
-            await filter2uuids_func(class_resolver, info, class_filter)
+        org_unit_uuids = await get_org_unit_uuids(info, filter)
+        query = query.where(
+            OrganisationFunktionRegistrering.id.in_(
+                select(
+                    OrganisationFunktionRelation.organisationfunktion_registrering_id
+                ).where(
+                    OrganisationFunktionRelation.rel_type
+                    == OrganisationFunktionRelationKode.tilknyttedeenheder,
+                    OrganisationFunktionRelation.rel_maal_uuid.in_(org_unit_uuids),
+                    _get_virkning_clause(OrganisationFunktionRelation, filter),
+                )
+            )
         )
 
-    tilknyttedefunktioner = []
-    if filter.engagements is not None or filter.engagement is not None:
-        tilknyttedefunktioner.extend(
-            lora_filter(await get_engagement_uuids(info, filter))
+    # Job function
+    if filter.job_function is not None:
+        job_function_uuids = await filter2uuids_func(
+            class_resolver, info, filter.job_function
         )
-    if filter.ituser is not None:
-        tilknyttedefunktioner.extend(
-            lora_filter(await filter2uuids_func(it_user_resolver, info, filter.ituser))
+        query = query.where(
+            OrganisationFunktionRegistrering.id.in_(
+                select(
+                    OrganisationFunktionRelation.organisationfunktion_registrering_id
+                ).where(
+                    OrganisationFunktionRelation.rel_type
+                    == OrganisationFunktionRelationKode.opgaver,
+                    OrganisationFunktionRelation.rel_maal_uuid.in_(job_function_uuids),
+                    _get_virkning_clause(OrganisationFunktionRelation, filter),
+                )
+            )
         )
-    if tilknyttedefunktioner:
-        kwargs["tilknyttedefunktioner"] = tilknyttedefunktioner
 
-    return await generic_resolver(
-        info.context.dataloaders.address_getter,
-        info.context.dataloaders.address_loader,
+    # Engagement type
+    if filter.engagement_type is not None:
+        engagement_type_uuids = await filter2uuids_func(
+            class_resolver, info, filter.engagement_type
+        )
+        query = query.where(
+            OrganisationFunktionRegistrering.id.in_(
+                select(
+                    OrganisationFunktionRelation.organisationfunktion_registrering_id
+                ).where(
+                    OrganisationFunktionRelation.rel_type
+                    == OrganisationFunktionRelationKode.organisatoriskfunktionstype,
+                    OrganisationFunktionRelation.rel_maal_uuid.in_(
+                        engagement_type_uuids
+                    ),
+                    _get_virkning_clause(OrganisationFunktionRelation, filter),
+                )
+            )
+        )
+
+    # Pagination. Must be done here since the generic_resolver (lora) does not support
+    # filtering on UUIDs and limit/cursor at the same time.
+    if limit is not None:
+        query = query.limit(limit)
+    if cursor is not None:
+        query = query.offset(cursor.offset)
+
+    return query
+
+
+async def address_resolver(
+    info: MOInfo,
+    filter: EngagementFilter | None = None,
+    limit: LimitType = None,
+    cursor: CursorType = None,
+) -> Any:
+    """Resolve engagements."""
+    if filter is None:
+        filter = EngagementFilter()
+
+    query = await engagement_resolver_query(
         info=info,
         filter=filter,
         limit=limit,
         cursor=cursor,
-        **kwargs,
+    )
+
+    # Execute
+    session: AsyncSession = info.context.session
+    result = await session.execute(query)
+    uuids = [row[0] for row in result]
+
+    # See lora.py:fetch()'s is_paged
+    is_paged = limit != 0 and cursor is not None and cursor.offset > 0
+    if not uuids and is_paged:
+        # There may be multiple LoRa fetches in one GraphQL request, so this
+        # cannot be refactored into always overwriting the value.
+        context["lora_page_out_of_range"] = True
+
+    access_log(
+        session,
+        "filter_engagements",
+        "OrganisationFunktion",
+        {
+            "filter": filter,
+            "limit": limit,
+            "cursor": cursor,
+        },
+        uuids,
+    )
+
+    return await generic_resolver(
+        info.context.dataloaders.engagement_getter,
+        info.context.dataloaders.engagement_loader,
+        info=info,
+        filter=BaseFilter(
+            uuids=uuids,
+            from_date=filter.from_date,
+            to_date=filter.to_date,
+            registration_time=filter.registration_time,
+        ),
     )
 
 
