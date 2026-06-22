ec66ec5e704cf9238f1ef6cbb8f54d3246b91eeb
Andreas Kring <andreas@magenta-aps.dk>
2026-06-22 11:32:58 +0200

    chore: add manager terminator and Xflow integrations to actor table

---
+++ alembic/versions/cfcfa8b6102f_add_manager_terminator_to_actor_table.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+"""add manager terminator to actor table"""
+
+from collections.abc import Sequence
+
+from alembic import op
+
+revision: str = "cfcfa8b6102f"
+down_revision: str | Sequence[str] | None = "aeb818ae64be"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade() -> None:
+    op.execute(
+        """
+        INSERT INTO actor (name, actor) VALUES
+            ('Manager Terminator', '3aca9ef1-baad-c0de-6d61-6e6167657274'),
+            ('Xflow', '8f107777-baad-c0de-7866-6c6f77000000')
+        """
+    )
+
+
+def downgrade() -> None:
+    op.execute("DELETE FROM actor WHERE actor = '3aca9ef1-baad-c0de-6d61-6e6167657274'")
+    op.execute("DELETE FROM actor WHERE actor = '8f107777-baad-c0de-7866-6c6f77000000'")
