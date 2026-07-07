f4e34815d1a9afd95899a47f55ed81779dabc2c8
Emil Madsen <githubpublicemail.goldmine822@passinbox.com>
2026-07-07 23:11:13 +0000

    chore(graphql): [#58898] remove dead gen_role_permission and gen_permission
    This commit removes leftover deadcode following the removal of their
    uses in the prior commit.

--- mora/graphapi/permissions.py
+++ mora/graphapi/permissions.py
@@ -1,14 +1,11 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 from contextlib import suppress
-from functools import cache
-from functools import partial
 from typing import Any
 from typing import Literal
 from typing import get_args
 
 from graphql import OperationType
-from strawberry import BasePermission
 from strawberry.types import Info
 
 from mora.auth.exceptions import AuthorizationError
@@ -114,100 +111,3 @@ async def _check_rbac(
             return True
 
     return False
-
-
-@cache
-def gen_role_permission(
-    permission_role: str,
-    message: str | None = None,
-    force_permission_check: bool = False,
-    collection: Collections | None = None,
-    permission_type: CollectionPermissionType | None = None,
-) -> type[BasePermission]:
-    """Generator function for permission classes.
-
-    Args:
-        permission_role: The role to check existence for.
-        message: Optional message override.
-        collection: Optional collection used for owner check.
-        collection: Optional permission type used for owner check.
-
-    Returns:
-        Permission class that checks if `role_name` is in the OIDC token.
-    """
-    fail_message = message or f"User does not have required role: {permission_role}"
-
-    class CheckRolePermission(BasePermission):
-        """Permission class that checks that a given role exists on the OIDC token.
-
-        If the simple role-check fails, we additionally allow access if the operation
-        is a mutation and the user is an owner of the object. This probably should be
-        implemented in a separate permission class, but Strawberry does not support
-        combining multiple permissions with OR.
-        https://github.com/strawberry-graphql/strawberry/issues/2350
-        """
-
-        message = fail_message
-
-        # TODO: Should be typed as MOInfo, but gives cyclic import issues
-        async def has_permission(self, source: Any, info: Info, **kwargs: Any) -> bool:
-            """Returns `True` if `role_name` exists in the token's roles."""
-            return await _check_rbac(
-                info,
-                permission_role,
-                force_permission_check,
-                collection,
-                permission_type,
-                kwargs,
-            )
-
-    return CheckRolePermission
-
-
-def gen_permission(
-    collection: Collections,
-    permission_type: CollectionPermissionType,
-    force_permission_check: bool = False,
-) -> type[BasePermission]:
-    """Generator function for permission classes.
-
-    Utilizes `gen_role_permission` with a generated role-name and a custom message.
-
-    Args:
-        collection_name: Name of the collection to check access to.
-
-    Returns:
-        Permission class that checks if the `collection_name` derived role is in the
-        OIDC token.
-    """
-    assert collection in get_args(Collections), f"{collection} not a valid collection"
-    assert permission_type in get_args(CollectionPermissionType), (
-        f"{permission_type} not a valid permission type"
-    )
-
-    permission_name = f"{permission_type}_{collection}"
-    return gen_role_permission(
-        permission_name,
-        f"User does not have {permission_type}-access to {collection}",
-        force_permission_check=force_permission_check,
-        collection=collection,
-        permission_type=permission_type,
-    )
-
-
-gen_read_permission = partial(gen_permission, permission_type="read")
-gen_create_permission = partial(
-    gen_permission, permission_type="create", force_permission_check=True
-)
-gen_update_permission = partial(
-    gen_permission, permission_type="update", force_permission_check=True
-)
-gen_terminate_permission = partial(
-    gen_permission, permission_type="terminate", force_permission_check=True
-)
-gen_delete_permission = partial(
-    gen_permission, permission_type="delete", force_permission_check=True
-)
-gen_refresh_permission = partial(
-    gen_permission, permission_type="refresh", force_permission_check=True
-)
