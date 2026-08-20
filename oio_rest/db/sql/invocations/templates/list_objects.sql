85b1ac8ba20bff87d332ab0e797fcb4394aa23b3
Carl Bordum Hansen <carl@bordum.dk>
2026-08-20 15:49:24 +0200

    perf: [#71302] one json doc from pg instead of array
    Get one json doc instead of an array of json docs. This let's postgres
    do the work, rather than having psycopg do a ton of json.loads.

--- oio_rest/db/sql/invocations/templates/list_objects.sql
+++ oio_rest/db/sql/invocations/templates/list_objects.sql
@@ -1,8 +1,9 @@
 -- SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 -- SPDX-License-Identifier: MPL-2.0
-
-SELECT  as_list_{{ class_name | lower }}(
-    {{ uuid|adapt }}::uuid[],
-    {{ registrering_tstzrange|adapt }},
-    {{ virkning_tstzrange|adapt }}
-) :: json[];
+SELECT array_to_json(
+    as_list_{{ class_name | lower }}(
+        {{ uuid|adapt }}::uuid[],
+        {{ registrering_tstzrange|adapt }},
+        {{ virkning_tstzrange|adapt }}
+    ) :: json[]
+);
