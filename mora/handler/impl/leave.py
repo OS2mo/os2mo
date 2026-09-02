c838dfd1e11f78c7d13e9a1ca7bd988bd9863189
Lorenzo Panieri <lp@magenta.dk>
2026-09-02 15:17:28 +0200

    refactor(handler): [#71423] remove dead code

--- mora/handler/impl/leave.py
+++ mora/handler/impl/leave.py
@@ -2,15 +2,8 @@
 # SPDX-License-Identifier: MPL-2.0
 from structlog import get_logger
 
-from mora import lora
-from mora import util
-
 from ... import mapping
-from ...graphapi.middleware import is_graphql
-from ...service import employee
-from ...service import facet
 from .. import reading
-from .engagement import get_engagement
 
 ROLE_TYPE = "leave"
 
@@ -30,38 +23,10 @@ class LeaveReader(reading.OrgFunkReadingHandler):
         engagement_uuid = mapping.ASSOCIATED_FUNCTION_FIELD.get_uuid(effect)
 
         base_obj = await super()._get_mo_object_from_effect(effect, start, end, funcid)
-        only_primary_uuid = util.get_args_flag("only_primary_uuid")
 
-        if is_graphql():
-            return {
-                **base_obj,
-                "employee_uuid": person,
-                "leave_type_uuid": leave_type,
-                "engagement_uuid": engagement_uuid,
-            }
-
-        person_obj = await employee.request_bulked_get_one_employee(
-            person, only_primary_uuid=only_primary_uuid
-        )
-
-        leave_type_obj = await facet.request_bulked_get_one_class(
-            leave_type, only_primary_uuid=only_primary_uuid
-        )
-
-        if only_primary_uuid:
-            engagement = {mapping.UUID: engagement_uuid}
-        else:  # pragma: no cover
-            # We look up whatever engagement is active at the present time period
-            # to account for edge cases where the engagement might have changed or is
-            # no longer active during the time period
-            present_connector = lora.Connector(validity="present")
-            engagement = await get_engagement(present_connector, uuid=engagement_uuid)
-
-        r = {
+        return {
             **base_obj,
-            mapping.PERSON: person_obj,
-            mapping.LEAVE_TYPE: leave_type_obj,
-            mapping.ENGAGEMENT: engagement,
+            "employee_uuid": person,
+            "leave_type_uuid": leave_type,
+            "engagement_uuid": engagement_uuid,
         }
-
-        return r
--- mora/handler/impl/related_unit.py
+++ mora/handler/impl/related_unit.py
@@ -6,11 +6,7 @@ from typing import TypeVar
 
 from structlog import get_logger
 
-from mora import util
-
 from ... import mapping
-from ...graphapi.middleware import is_graphql
-from ...service import orgunit
 from .. import reading
 
 ROLE_TYPE = "related_unit"
@@ -31,26 +27,8 @@ class RoleReader(reading.OrgFunkReadingHandler):
         org_units_uuid = mapping.ASSOCIATED_ORG_UNIT_FIELD.get_uuids(effect)
 
         base_obj = await super()._get_mo_object_from_effect(effect, start, end, funcid)
-        if is_graphql():
-            return {
-                **base_obj,
-                "org_unit_uuids": org_units_uuid,
-            }
 
-        only_primary_uuid = util.get_args_flag("only_primary_uuid")
-
-        org_units = [
-            await orgunit.request_bulked_get_one_orgunit(
-                unitid=org_unit_uuid,
-                details=orgunit.UnitDetails.MINIMAL,
-                only_primary_uuid=only_primary_uuid,
-            )
-            for org_unit_uuid in org_units_uuid
-        ]
-
-        r = {
+        return {
             **base_obj,
-            mapping.ORG_UNIT: sorted(org_units, key=lambda x: x.get("name")),
+            "org_unit_uuids": org_units_uuid,
         }
-
-        return r
