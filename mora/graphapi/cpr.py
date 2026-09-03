ba804afa7730a2a74adcd2c961aac4c847c5c7d7
Lorenzo Panieri <lp@magenta.dk>
2026-09-03 16:35:03 +0200

    feat(graphapi/cpr): [#71318] add `cpr` query

--- mora/graphapi/cpr.py
+++ mora/graphapi/cpr.py
@@ -1,20 +1,104 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 import re
-from typing import Any
+from textwrap import dedent
 
 import httpx
+import strawberry
 import structlog
 
 from mora import config
-from mora import exceptions
-from mora import mapping
 from mora import serviceplatformen
+from mora import util
+from mora.graphapi.context import MOInfo
 
 logger = structlog.stdlib.get_logger()
 
 
-def _handle_erstatningspersonnummer(cpr: str) -> dict | None:
+@strawberry.input(description="CPR filter.")
+class CPRFilter:
+    cpr_number: str = strawberry.field(description="The CPR number to look up.")
+
+
+@strawberry.type(description="Person found by CPR lookup.")
+class CPRPerson:
+    name: str = strawberry.field(
+        description=dedent(
+            """\
+            Name of the person.
+
+            Example: "John Doe"
+            """
+        )
+    )
+    cpr_number: str = strawberry.field(
+        description=dedent(
+            """\
+            CPR number of the person.
+
+            Example: "6101501234"
+            """
+        )
+    )
+
+
+@strawberry.type(
+    description=dedent(
+        """\
+        CPR number lookup is not possible, because the "serviceplatformen" integration is disabled.
+        """
+    )
+)
+class ServiceplatformenDisabledError:
+    cpr_number: str
+
+
+@strawberry.type(
+    description=dedent(
+        """\
+        The given CPR number is not valid.
+        """
+    )
+)
+class InvalidCPRNumberError:
+    cpr_number: str
+
+
+@strawberry.type(
+    description=dedent(
+        """\
+        No person was found that matches the given CPR number.
+        """
+    )
+)
+class CPRPersonNotFoundError:
+    cpr_number: str
+
+
+CPRError = (
+    ServiceplatformenDisabledError | InvalidCPRNumberError | CPRPersonNotFoundError
+)
+
+
+async def cpr_resolver(info: MOInfo, filter: CPRFilter) -> CPRPerson | CPRError:
+    cpr = filter.cpr_number
+
+    settings = info.context.settings
+    if not settings.enable_sp:
+        logger.error("serviceplatformen not enabled")
+        return ServiceplatformenDisabledError(cpr_number=cpr)
+
+    if not util.is_cpr_number(cpr):
+        logger.error("CPR number not valid")
+        return InvalidCPRNumberError(cpr_number=cpr)
+
+    if fictitious := _handle_erstatningspersonnummer(cpr):
+        return fictitious
+
+    return _get_citizen(cpr, settings)
+
+
+def _handle_erstatningspersonnummer(cpr: str) -> CPRPerson | None:
     """Handle "erstatningspersonnummer" CPR numbers - that is, CPR numbers where the
     'day' part of the birthdate is in the range 61-91.
 
@@ -40,16 +124,17 @@ def _handle_erstatningspersonnummer(cpr: str) -> dict | None:
         day: int = parsed["day"]
         if 61 <= day <= 91:
             logger.debug(event="detected 'erstatningspersonnummer'")
-            # Return the CPR number entered by the user, along with a blank name.
-            return {mapping.NAME: "", mapping.CPR_NO: cpr}
+            return CPRPerson(name="", cpr_number=cpr)
         else:
             logger.debug(event="detected normal CPR")
     else:
         logger.warning(event="could not parse CPR", value=cpr)
+    return None
 
 
-def _get_citizen(cpr: str, settings: config.Settings) -> dict[str, Any]:
+def _get_citizen(cpr: str, settings: config.Settings) -> CPRPerson | CPRError:
     assert settings.sp_settings is not None
+
     sp_uuids = {
         "service_agreement": str(settings.sp_settings.sp_agreement_uuid),
         "user_system": str(settings.sp_settings.sp_system_uuid),
@@ -59,20 +144,38 @@ def _get_citizen(cpr: str, settings: config.Settings) -> dict[str, Any]:
     certificate = str(settings.sp_settings.sp_certificate_path)
     sp_production = settings.sp_settings.sp_production
     sp_api_version = settings.sp_settings.sp_api_version
+
     try:
-        return serviceplatformen.get_citizen(
+        sp_data = serviceplatformen.get_citizen(
             sp_uuids,
             certificate,
             cpr,
             production=sp_production,
             api_version=sp_api_version,
         )
-    except httpx.HTTPStatusError as e:  # pragma: no cover
+    except httpx.HTTPStatusError as e:
         if "PNRNotFound" in e.response.text:
-            raise KeyError("CPR not found")
-        else:
-            logger.exception(event="HTTPStatusError", exception=e)
-            raise e
-    except httpx.ConnectError as e:  # pragma: no cover
-        logger.exception(event="ConnectError", exception=e)
-        exceptions.ErrorCodes.E_SP_SSL_ERROR()
+            logger.exception("CPR person not found", exception=e)
+            return CPRPersonNotFoundError(cpr_number=cpr)
+        logger.exception("unexpected HTTP error during CPR lookup", exception=e)
+        raise e
+    except httpx.ConnectError as e:
+        logger.exception("HTTP connection error", exception=e)
+        raise e
+    except Exception as e:  # pragma: no cover
+        logger.exception("unexpected error during CPR lookup", exception=e)
+        raise e
+
+    # handle edge case where serviceplatformen.get_citizen returns an error dict.
+    # This can happen if an HTTP request succeded but returned a code other than 200
+    if err := sp_data.get("Error"):  # pragma: no cover
+        exception = RuntimeError(err)
+        logger.exception("unexpected error during CPR lookup", exception=exception)
+        raise exception
+
+    first_name = sp_data.get("fornavn")
+    middle_name = sp_data.get("mellemnavn")
+    last_name = sp_data.get("efternavn")
+    name = " ".join(filter(None, [first_name, middle_name, last_name]))
+
+    return CPRPerson(name=name, cpr_number=cpr)
