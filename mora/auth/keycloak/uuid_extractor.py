a09115d5b953f911e7e4021c6075f6430a23ff4d
Emil Madsen <githubpublicemail.goldmine822@passinbox.com>
2026-09-11 15:19:33 +0000

    refactor(pbac, owner): [#58898] lift the related-unit check into OWNER_ENTITIES
    This commit extracts the `related_units_update` mutator branch from
    `get_entities_graphql` moving it into the `OWNER_ENTITIES` map.

--- mora/auth/keycloak/uuid_extractor.py
+++ mora/auth/keycloak/uuid_extractor.py
@@ -210,13 +210,6 @@ def get_entities_graphql(
     """
 
     def rule(input: Any) -> ColumnElement | None:
-        if collection == "related_unit":
-            # Related units have a single `origin` field and a list of
-            # `destination`s. Originally we required ownership of both the
-            # origin and destinations, but that's not compatible with the old
-            # service-api owner calculation
-            return org_unit(settings, version, token, getattr(input, "origin", None))
-
         # Even though most of the remaining object types (addresses,
         # associations, engagements, IT-users, leaves, managers, owners and
         # role-bindings, at time of writing) can reference both employees and
